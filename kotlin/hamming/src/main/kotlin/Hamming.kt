81a6f779d79474a8f2fa682c8b14b2e79085a96f
Paul Moore <pdmoore@gmail.com>
2019-04-16 10:30:03 -0700

    dumb impl passes the tests where strands match

---
+++ kotlin/hamming/src/main/kotlin/Hamming.kt
@@ -0,0 +1,10 @@
+object Hamming {
+
+    fun compute(l: String, r: String): Int {
+
+        // LOOP OVER WHOLE SEQUENCE,
+        // COMPARE CHAR IN EACH SIDE, INCREMENT COUNTER IF THEY DIFFER
+
+       return 0;
+   }
+}
