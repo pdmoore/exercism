c2cfe6ccaf05249e7be9ce95e6c3ea7ad7929e93
Paul Moore <pdmoore@gmail.com>
2019-04-19 22:32:41 -0700

    inline zip/count

--- kotlin/hamming/src/main/kotlin/Hamming.kt
+++ kotlin/hamming/src/main/kotlin/Hamming.kt
@@ -3,7 +3,6 @@ object Hamming {
     fun compute(l: String, r: String): Int {
         require (l.length == r.length) { "left and right strands must be of equal length." }
 
-        val zipped = l.zip(r);
-        return zipped.count { it.first != it.second }
+        return l.zip(r).count { it.first != it.second }
    }
 }
