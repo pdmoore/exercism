f02fb516ad86409c72ca6b39fbf132aa7203e86f
Paul Moore <pdmoore@gmail.com>
2019-04-19 22:31:28 -0700

    first pass at zip/count refactoring

--- kotlin/hamming/src/main/kotlin/Hamming.kt
+++ kotlin/hamming/src/main/kotlin/Hamming.kt
@@ -3,14 +3,7 @@ object Hamming {
     fun compute(l: String, r: String): Int {
         require (l.length == r.length) { "left and right strands must be of equal length." }
 
-        var count = 0
-
-        for (index in l.indices) {
-            when (l[index] == r[index]) {
-                false -> count++
-            }
-        }
-
-       return count;
+        val zipped = l.zip(r);
+        return zipped.count { it.first != it.second }
    }
 }
