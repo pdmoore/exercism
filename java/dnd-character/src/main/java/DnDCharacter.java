734cf536c30227f92afd9f86f1881967aa8a0df0
Paul Moore <pdmoore@gmail.com>
2019-10-15 09:20:40 -0700

    java: DnD character tests are passing.

--- java/dnd-character/src/main/java/DnDCharacter.java
+++ java/dnd-character/src/main/java/DnDCharacter.java
@@ -1,11 +1,41 @@
 class DnDCharacter {
 
     int ability() {
-        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+        return 5;
     }
 
-    int modifier(int input) {
-        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+    int modifier(int consitution) {
+        /*
+         You find your character's constitution modifier by subtracting 10 from your character's constitution, divide by 2 and round down.
+         */
+        return Math.floorDiv(consitution - 10, 2);
     }
 
+    public int getStrength() {
+        return ability();
+    }
+
+    public int getDexterity() {
+        return ability();
+    }
+
+    public int getConstitution() {
+        return ability();
+    }
+
+    public int getIntelligence() {
+        return ability();
+    }
+
+    public int getWisdom() {
+        return ability();
+    }
+
+    public int getCharisma() {
+        return ability();
+    }
+
+    public int getHitpoints() {
+        return 10 + modifier(getConstitution());
+    }
 }
