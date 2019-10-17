9687a6ba3afe31be0de613e7d09f0953ddab14e9
Paul Moore <pdmoore@gmail.com>
2019-10-17 15:32:49 -0700

    java: grains refactor

--- java/grains/src/main/java/Grains.java
+++ java/grains/src/main/java/Grains.java
@@ -3,21 +3,19 @@ import java.math.BigInteger;
 class Grains {
 
     private static final String WRONG_SQUARE_MESSAGE = "square must be between 1 and 64";
+    private static final int BOARD_SIZE = 8;
+    private static final int NUMBER_OF_SQUARES = BOARD_SIZE * BOARD_SIZE;
 
-    BigInteger computeNumberOfGrainsOnSquare(final int square) {
-        if ((square < 1) || (square > 64)) {
+    BigInteger computeNumberOfGrainsOnSquare(final int squareNumber) {
+        if ((squareNumber < 1) || (squareNumber > NUMBER_OF_SQUARES)) {
             throw new IllegalArgumentException(WRONG_SQUARE_MESSAGE);
         }
 
-        return (BigInteger.valueOf(2)).pow(square - 1);
+        return (BigInteger.valueOf(2)).pow(squareNumber - 1);
     }
 
     BigInteger computeTotalNumberOfGrainsOnBoard() {
-        BigInteger totalGrains = BigInteger.ZERO;
-        for (int i = 1; i <= 64; i++) {
-            totalGrains = totalGrains.add(computeNumberOfGrainsOnSquare(i));
-        }
-
+        BigInteger totalGrains = (BigInteger.valueOf(2)).pow(NUMBER_OF_SQUARES).subtract(BigInteger.ONE);
         return totalGrains;
     }
 
