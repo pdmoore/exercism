e188bcdbaef43c8bb5477990d4b97bd424bb1e39
Paul Moore <pdmoore@gmail.com>
2019-10-22 14:18:55 -0700

    java: sum multiples - refactor to store non-0 elements of set

--- java/sum-of-multiples/src/main/java/SumOfMultiples.java
+++ java/sum-of-multiples/src/main/java/SumOfMultiples.java
@@ -1,25 +1,28 @@
 import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 
 class SumOfMultiples {
 
     private final int number;
     private final int[] set;
+    private final List<Integer> ints;
 
     SumOfMultiples(int number, int[] set) {
         this.number = number;
         this.set = set;
+
+        ints = Arrays.stream(set).filter(n -> n > 0).boxed().collect(Collectors.toList());
     }
 
     int getSum() {
         int sum = 0;
         for (int i = 1; i < number; i++) {
-            for (int j = 0; j < set.length; j++) {
-                if (set[j] != 0) {
-                    if (i % set[j] == 0) {
-                        sum += i;
-                        break;
-                    }
+            for (int j :
+                    ints) {
+                if (i % j == 0) {
+                    sum += i;
+                    break;
                 }
             }
         }
