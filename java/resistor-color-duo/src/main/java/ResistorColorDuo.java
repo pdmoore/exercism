e3e71caff42b374853cbfcc1a5935e8262e78ca7
Paul Moore <pdmoore@gmail.com>
2020-01-01 14:29:28 -0800

    java - resistor color duo - refactor to array of color names

--- java/resistor-color-duo/src/main/java/ResistorColorDuo.java
+++ java/resistor-color-duo/src/main/java/ResistorColorDuo.java
@@ -1,21 +1,22 @@
+import java.util.Arrays;
+
 class ResistorColorDuo {
-    int value(String[] colors) {
-        String number = numberForColor(colors[0]) + numberForColor(colors[1]);
-        return Integer.parseInt(number);
+
+    String[] colors() {
+        return new String[] {
+                "black", "brown", "red", "orange", "yellow", "green", "blue", "violet", "grey", "white"
+        };
     }
 
-    private String numberForColor(String color) {
-        String number = "";
-        switch (color) {
-            case "black": number += 0; break;
-            case "brown": number += 1; break;
-            case "orange": number += 3; break;
-            case "yellow": number += 4; break;
-            case "green": number += 5; break;
-            case "blue": number += 6; break;
-            case "violet": number += 7; break;
-            case "grey": number += 8; break;
-        }
-        return number;
+
+    int value(String[] colors) {
+        int firstResistor = colorCode(colors[0]);
+        int secondResistor = colorCode(colors[1]);
+
+        return firstResistor * 10 + secondResistor;
+    }
+
+    int colorCode(String color) {
+        return Arrays.asList(colors()).indexOf(color);
     }
 }
