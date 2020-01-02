5eb10aef4a8f8b8f9271da894dd33c8292a7ba92
Paul Moore <pdmoore@gmail.com>
2020-01-02 10:21:15 -0800

    java - 12 days - complete

--- java/twelve-days/src/main/java/TwelveDays.java
+++ java/twelve-days/src/main/java/TwelveDays.java
@@ -2,17 +2,17 @@ class TwelveDays {
     final static String ORDINAL[] = { "first", "second", "third", "fourth", "fifth", "sixth", "seventh", "eighth", "ninth", "tenth", "eleventh", "twelfth" };
     final static String VERSETEXT[] = {
             "a Partridge in a Pear Tree",
-            "two Turtle Doves, ",
-            "three French Hens, ",
-            "four Calling Birds, ",
-            "five Gold Rings, ",
-            "six Geese-a-Laying, ",
-            "seven Swans-a-Swimming, ",
-            "eight Maids-a-Milking, ",
-            "nine Ladies Dancing, ",
-            "ten Lords-a-Leaping, ",
-            "eleven Pipers Piping, ",
-            "twelve Drummers Drumming, ",
+            "two Turtle Doves",
+            "three French Hens",
+            "four Calling Birds",
+            "five Gold Rings",
+            "six Geese-a-Laying",
+            "seven Swans-a-Swimming",
+            "eight Maids-a-Milking",
+            "nine Ladies Dancing",
+            "ten Lords-a-Leaping",
+            "eleven Pipers Piping",
+            "twelve Drummers Drumming",
     };
 
     String verse(int verseNumber) {
@@ -21,14 +21,18 @@ class TwelveDays {
 
         for (int i = verseNumber; i > 1; i--) {
             sb.append(VERSETEXT[i - 1]);
+            sb.append(", ");
         }
 
-        if (verseNumber > 1) {
-            sb.append("and ");
-        }
-        sb.append(VERSETEXT[0]);
+        sb.append(String.format("%s%s", andIfNeeded(verseNumber), VERSETEXT[0]));
+
         sb.append(".\n");
+
         return sb.toString();
+    }
+
+    private String andIfNeeded(int verseNumber) {
+        return (verseNumber > 1) ? "and " : "";
     }
 
     String verses(int startVerse, int endVerse) {
