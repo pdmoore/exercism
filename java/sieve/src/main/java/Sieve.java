6d54e07ecb983d9d88c51b314af19fa6d5a3e206
Paul Moore <pdmoore@gmail.com>
2020-01-04 10:52:07 -0800

    java - sieve - starting point

---
+++ java/sieve/src/main/java/Sieve.java
@@ -0,0 +1,12 @@
+import java.util.Collections;
+import java.util.List;
+
+class Sieve {
+    Sieve(int maxPrime) {
+
+    }
+
+    List<Integer> getPrimes() {
+        return Collections.emptyList();
+    }
+}
