48f5429fdcfd2c22e21744fc132d292520c5b43e
Paul Moore <pdmoore@gmail.com>
2020-01-04 11:08:46 -0800

    java - sieve - refactor

--- java/sieve/src/main/java/Sieve.java
+++ java/sieve/src/main/java/Sieve.java
@@ -3,35 +3,37 @@ import java.util.Collections;
 import java.util.List;
 
 class Sieve {
-    private final Boolean[] foo;
+    private List primes;
 
     Sieve(int maxPrime) {
         if (maxPrime <= 1) {
-            foo = new Boolean[1];
+            primes = Collections.emptyList();
         } else {
+            sievePrimes(maxPrime);
+        }
+    }
 
-            // create boolean array up to maxPrime in length
-            foo = new Boolean[maxPrime + 1];
-            for (int i = 0; i < maxPrime + 1; i++) {
-                foo[i] = true;
-            }
+    private void sievePrimes(int maxPrime) {
+        Boolean[] candidates = new Boolean[maxPrime + 1];
+        for (int i = 0; i < maxPrime + 1; i++) {
+            candidates[i] = true;
+        }
+
+        primes = new ArrayList<>();
+        for (int i = 2; i < candidates.length; i++) {
+            if (candidates[i]) primes.add(i);
+
+            removeMultiplesOfCandidate(candidates, i);
+        }
+    }
+
+    private void removeMultiplesOfCandidate(Boolean[] candidates, int i) {
+        for (int j = i; j < candidates.length; j = j + i) {
+            candidates[j] = false;
         }
     }
 
     List<Integer> getPrimes() {
-        // loop from 2 length
-        // mark each multiple as prime
-        // as looping, capture whether number was marked already or not
-
-        List<Integer> primes = new ArrayList<>();
-        for (int i = 2; i < foo.length; i++) {
-            if (foo[i]) primes.add(i);
-
-            for (int j = i; j < foo.length; j = j + i) {
-                foo[j] = false;
-            }
-        }
-
         return primes;
     }
 }
