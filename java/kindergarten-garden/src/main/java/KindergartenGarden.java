92c2af58e354a3959486b65fe764822fbcb8a422
Paul Moore <pdmoore@gmail.com>
2020-01-05 14:57:44 -0800

    java - kid garden - refactor pass

--- java/kindergarten-garden/src/main/java/KindergartenGarden.java
+++ java/kindergarten-garden/src/main/java/KindergartenGarden.java
@@ -6,30 +6,33 @@ import java.util.List;
 class KindergartenGarden {
 
     private final String[] rows;
-    private final List<String> studentNames = Arrays.asList("Alice", "Bob", "Charlie", "David");
+    private final List<String> STUDENT_NAMES = Arrays.asList("Alice", "Bob", "Charlie", "David", "Eve", "Fred", "Ginny", "Harriet", "Ileana", "Joseph", "Kincaid", "Larry");
     private HashMap<String, List> plantsPerChild;
 
     KindergartenGarden(String garden) {
         rows = garden.split("\\n");
 
         plantsPerChild = new HashMap<>();
-        List<Plant> plants = new ArrayList<>();
-        plantsPerChild.put("Alice", plants);
-        plants = new ArrayList<>();
-        plantsPerChild.put("Bob", plants);
-        plants = new ArrayList<>();
-        plantsPerChild.put("Charlie", plants);
-        plants = new ArrayList<>();
-        plantsPerChild.put("David", plants);
+        for (int i = 0; i < STUDENT_NAMES.size(); i++) {
+            List<Plant> plants = new ArrayList<>();
+            plantsPerChild.put(STUDENT_NAMES.get(i), plants);
+        }
+
+        assignPlantsByStudent();
     }
 
     List<Plant> getPlantsOfStudent(String student) {
+
+        return plantsPerChild.get(student);
+    }
+
+    private void assignPlantsByStudent() {
         for (String row :
                 rows) {
             for (int i = 0; i < row.length(); i += 2) {
 
                 int studentNameIndex = i / 2;
-                String thisStudent = studentNames.get(studentNameIndex);
+                String thisStudent = STUDENT_NAMES.get(studentNameIndex);
                 List<Plant> plants = plantsPerChild.get(thisStudent);
 
                 int studentOffset = i;
@@ -40,8 +43,6 @@ class KindergartenGarden {
             }
 
         }
-
-        return plantsPerChild.get(student);
     }
 
     private Plant convertCupContentToPlantName(char cupContains) {
