53d59cdf8ca7a6d0bd69bb9a8aaa2d1fc3a879e1
Paul Moore <pdmoore@gmail.com>
2020-01-08 15:11:01 -0800

    java - wordy - iter 1, addition

--- java/wordy/src/main/java/WordProblemSolver.java
+++ java/wordy/src/main/java/WordProblemSolver.java
@@ -5,7 +5,15 @@ public class WordProblemSolver {
         problem = problem.replace("?", "");
         String[] words = problem.split(" ");
 
-        int answer = Integer.parseInt(words[2]);
+        int answer = 0;
+        if (words.length == 3) {
+            answer = Integer.parseInt(words[2]);
+        } else {
+            int operand1 = Integer.parseInt(words[2]);
+            int operand2 = Integer.parseInt(words[4]);
+
+            answer = operand1 + operand2;
+        }
 
         return answer;
     }
