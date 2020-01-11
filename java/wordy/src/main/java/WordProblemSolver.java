3316786621ad8fb194879c235994340758612798
Paul Moore <pdmoore@gmail.com>
2020-01-11 11:43:53 -0800

    java - wordy - refactor to factory to create a math action. Open/Closed on 'solve'

--- java/wordy/src/main/java/WordProblemSolver.java
+++ java/wordy/src/main/java/WordProblemSolver.java
@@ -3,9 +3,6 @@ import java.util.function.IntBinaryOperator;
 public class WordProblemSolver {
     public static final String INVALID_QUESTION_MESSAGE = "I'm sorry, I don't understand the question!";
 
-
-    //TODO - refactor
-
     public int solve(String problem) {
         validateProblemStatement(problem);
 
@@ -22,34 +19,14 @@ public class WordProblemSolver {
         int operand1 = getNumber(words, 2);
 
         int answer = 0;
-        int i = 3;
-        while (i < words.length) {
-            int operand2 = 0;
-            IntBinaryOperator operator;
+        int indexOfOperation = 3;
+        while (indexOfOperation < words.length) {
+            MathActions action = createAction(words[indexOfOperation]);
+            int operand2 = getNumber(words, indexOfOperation + action.operand2Offset);
+            answer = action.operator.applyAsInt(operand1, operand2);
 
-            String operation = words[i];
-            if ("plus".equals(operation)) {
-                operator = Math::addExact;
-                operand2 = getNumber(words, i + 1);
-                i += 2;
-            } else if ("minus".equals(operation)) {
-                operator = Math::subtractExact;
-                operand2 = getNumber(words, i + 1);
-                i += 2;
-            } else if ("multiplied".equals(operation)) {
-                operator = Math::multiplyExact;
-                operand2 = getNumber(words, i + 2);
-                i += 3;
-            } else if ("divided".equals(operation)) {
-                operator = Math::floorDiv;
-                operand2 = getNumber(words, i + 2);
-                i += 3;
-            } else {
-                throw new IllegalArgumentException(INVALID_QUESTION_MESSAGE);
-            }
-
-            answer = operator.applyAsInt(operand1, operand2);
             operand1 = answer;
+            indexOfOperation += action.incrementToNextOperation;
         }
 
         return answer;
@@ -68,6 +45,7 @@ public class WordProblemSolver {
     }
 
     private boolean askingForNumber(String[] words) {
+        // could check words[1,2] are "what is" instead
         return words.length == 3;
     }
 
@@ -81,6 +59,60 @@ public class WordProblemSolver {
 
         if (!"What".equals(words[0]) || (problem.contains("cubed"))) {
             throw new IllegalArgumentException(INVALID_QUESTION_MESSAGE);
+        }
+    }
+
+    private MathActions createAction(String operation) {
+        switch (operation) {
+            case "plus":
+                return new Addition();
+            case "minus":
+                return new Subtraction();
+            case "multiplied":
+                return new Multiplication();
+            case "divided":
+                return new Division();
+        }
+
+        throw new IllegalArgumentException(INVALID_QUESTION_MESSAGE);
+    }
+
+
+    private abstract class MathActions {
+        public IntBinaryOperator operator;
+        public int operand2Offset;
+        public int incrementToNextOperation;
+    }
+
+    private class Addition extends MathActions {
+        public Addition() {
+            operator = Math::addExact;
+            operand2Offset = 1;
+            incrementToNextOperation = 2;
+        }
+    }
+
+    private class Subtraction extends MathActions {
+        public Subtraction() {
+            operator = Math::subtractExact;
+            operand2Offset = 1;
+            incrementToNextOperation = 2;
+        }
+    }
+
+    private class Multiplication extends MathActions {
+        public Multiplication() {
+            operator = Math::multiplyExact;
+            operand2Offset = 2;
+            incrementToNextOperation = 3;
+        }
+    }
+
+    private class Division extends MathActions {
+        public Division() {
+            operator = Math::floorDiv;
+            operand2Offset = 2;
+            incrementToNextOperation = 3;
         }
     }
 }
