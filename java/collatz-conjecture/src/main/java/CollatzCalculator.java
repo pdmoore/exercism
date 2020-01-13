9f9cb27c7bf18bfde1e459a12147e244611c1bc8
Paul Moore <pdmoore@gmail.com>
2020-01-12 19:24:21 -0800

    java - collatz - refactor because the pom ain't done

--- java/collatz-conjecture/src/main/java/CollatzCalculator.java
+++ java/collatz-conjecture/src/main/java/CollatzCalculator.java
@@ -1,23 +1,32 @@
 class CollatzCalculator {
 
+    private int step;
+
+    public CollatzCalculator() {
+        step = 0;
+    }
+
     int computeStepCount(int start) {
         if (start <= 0) {
             throw new IllegalArgumentException("Only natural numbers are allowed");
         }
 
-        int step = 0;
         int i = start;
         while (i > 1) {
-            step++;
-
-            if (i % 2 == 0) {
-                i = i / 2;
-            } else {
-                i = (i * 3) + 1;
-            }
+            i = nextStep(i);
         }
 
         return step;
+    }
+
+    private int nextStep(int i) {
+        step++;
+
+        if (i % 2 == 0) {
+            return i / 2;
+        } else {
+            return (i * 3) + 1;
+        }
     }
 
 }
