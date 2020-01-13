ab4debf2a343f4fe11215610563329ed386987d0
Paul Moore <pdmoore@gmail.com>
2020-01-12 19:16:19 -0800

    java - collatz - done...zzzz

--- java/collatz-conjecture/src/main/java/CollatzCalculator.java
+++ java/collatz-conjecture/src/main/java/CollatzCalculator.java
@@ -1,7 +1,23 @@
 class CollatzCalculator {
 
     int computeStepCount(int start) {
-        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+        if (start <= 0) {
+            throw new IllegalArgumentException("Only natural numbers are allowed");
+        }
+
+        int count = 0;
+        int i = start;
+        while (i > 1) {
+            count++;
+
+            if (i % 2 == 0) {
+                i = i / 2;
+            } else {
+                i = (i * 3) + 1;
+            }
+        }
+
+        return count;
     }
 
 }
--- java/collatz-conjecture/src/test/java/CollatzCalculatorTest.java
+++ java/collatz-conjecture/src/test/java/CollatzCalculatorTest.java
@@ -23,25 +23,21 @@ public class CollatzCalculatorTest {
         assertEquals(0, collatzCalculator.computeStepCount(1));
     }
 
-    @Ignore("Remove to run test")
     @Test
     public void testCorrectNumberOfStepsWhenAllStepsAreDivisions() {
         assertEquals(4, collatzCalculator.computeStepCount(16));
     }
 
-    @Ignore("Remove to run test")
     @Test
     public void testCorrectNumberOfStepsWhenBothStepTypesAreNeeded() {
         assertEquals(9, collatzCalculator.computeStepCount(12));
     }
 
-    @Ignore("Remove to run test")
     @Test
     public void testAVeryLargeInput() {
         assertEquals(152, collatzCalculator.computeStepCount(1000000));
     }
 
-    @Ignore("Remove to run test")
     @Test
     public void testZeroIsConsideredInvalidInput() {
         expectedException.expect(IllegalArgumentException.class);
@@ -50,7 +46,6 @@ public class CollatzCalculatorTest {
         collatzCalculator.computeStepCount(0);
     }
 
-    @Ignore("Remove to run test")
     @Test
     public void testNegativeIntegerIsConsideredInvalidInput() {
         expectedException.expect(IllegalArgumentException.class);
