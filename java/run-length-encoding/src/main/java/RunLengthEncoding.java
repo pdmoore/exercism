6d5c590a96aa36747d923579e89f5438d211a629
Paul Moore <pdmoore@gmail.com>
2020-01-21 17:34:54 -0800

    java - run-length-encoding - starting point

---
+++ java/run-length-encoding/src/main/java/RunLengthEncoding.java
@@ -0,0 +1,11 @@
+public class RunLengthEncoding {
+
+
+    public String encode(String s) {
+        return null;
+    }
+
+    public String decode(String s) {
+        throw new UnsupportedOperationException("decode not implemented yet");
+    }
+}
