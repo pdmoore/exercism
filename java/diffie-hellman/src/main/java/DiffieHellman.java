4533325093e562e22b7db5ebb7738b06794225ef
Paul Moore <pdmoore@gmail.com>
2020-01-24 21:54:57 -0800

    java - diffie-hellman - starting point

---
+++ java/diffie-hellman/src/main/java/DiffieHellman.java
@@ -0,0 +1,16 @@
+import java.math.BigInteger;
+
+public class DiffieHellman {
+
+    public BigInteger privateKey(BigInteger prime) {
+        return null;
+    }
+
+    public BigInteger publicKey(BigInteger primeA, BigInteger primeB, BigInteger privateKey) {
+        return null;
+    }
+
+    public BigInteger secret(BigInteger prime, BigInteger publicKey, BigInteger privateKey) {
+        return null;
+    }
+}
