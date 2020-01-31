af9ce626a31ed5b88bf8befd4a30b9b46390cdf3
Paul Moore <pdmoore@gmail.com>
2020-01-31 09:53:10 -0800

    java - phone number - introduce field

--- java/phone-number/src/main/java/PhoneNumber.java
+++ java/phone-number/src/main/java/PhoneNumber.java
@@ -1,10 +1,14 @@
 public class PhoneNumber {
 
+    private final String phoneNumber;
+
     public PhoneNumber(String rawInput) {
+
+        phoneNumber = "2234567890";
 
     }
 
     public String getNumber() {
-        return "2234567890";
+        return phoneNumber;
     }
 }
