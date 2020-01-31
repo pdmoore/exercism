684efd8a4192533d9d883e335697720dde4a2ec8
Paul Moore <pdmoore@gmail.com>
2020-01-31 09:50:01 -0800

    java - phone number - compiles, test fails

--- java/phone-number/src/main/java/PhoneNumber.java
+++ java/phone-number/src/main/java/PhoneNumber.java
@@ -1,10 +1,10 @@
-/*
+public class PhoneNumber {
 
-Since this exercise has a difficulty of > 4 it doesn't come
-with any starter implementation.
-This is so that you get to practice creating classes and methods
-which is an important part of programming in Java.
+    public PhoneNumber(String rawInput) {
 
-Please remove this comment when submitting your solution.
+    }
 
-*/
+    public String getNumber() {
+        return null;
+    }
+}
