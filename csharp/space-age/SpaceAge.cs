359542ab31626f2f0e938fa34a55b3de3ac15a05
Paul Moore <pdmoore@gmail.com>
2020-02-01 18:58:14 -0800

    c# - space age - Venus passing, need to refactor non-EArth common code

--- csharp/space-age/SpaceAge.cs
+++ csharp/space-age/SpaceAge.cs
@@ -13,9 +13,6 @@ public class SpaceAge {
         return ToTwoDecimals(seconds / SECONDS);
     }
 
-    private double ToTwoDecimals(double age) {
-        return Math.Round(age, 2);
-    }
 
     public double OnMercury() {
         var MercuryOfEarth = 0.2408467;
@@ -24,7 +21,9 @@ public class SpaceAge {
     }
 
     public double OnVenus() {
-        throw new NotImplementedException("You need to implement this function.");
+        var VenusOfEarth = 0.61519726;
+
+        return ToTwoDecimals(seconds / (SECONDS * VenusOfEarth));
     }
 
     public double OnMars() {
@@ -45,5 +44,9 @@ public class SpaceAge {
 
     public double OnNeptune() {
         throw new NotImplementedException("You need to implement this function.");
+    }
+
+    private double ToTwoDecimals(double age) {
+        return Math.Round(age, 2);
     }
 }
