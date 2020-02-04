83781d1314514709c7c82a4ba59c7df457454618
Paul Moore <pdmoore@gmail.com>
2020-02-03 21:37:31 -0800

    java - phone number - refactor names

--- java/phone-number/src/main/java/PhoneNumber.java
+++ java/phone-number/src/main/java/PhoneNumber.java
@@ -10,58 +10,57 @@ public class PhoneNumber {
     private static final String exchangeCodeStartsWithZeroExceptionMessage = "exchange code cannot start with zero";
     private static final String exchangeCodeStartsWithOneExceptionMessage = "exchange code cannot start with one";
 
-
     private final String phoneNumber;
 
     public PhoneNumber(String rawInput) {
         //TODO clean and cleanUp don't mean anything now
-        String clean = cleanUp(rawInput);
+        String candidateNumber = removePunctuation(rawInput);
 
-        if (hasAlphabeticCharacters(clean)) {
+        if (hasAlphabeticCharacters(candidateNumber)) {
             throw new IllegalArgumentException(illegalCharacterExceptionMessage);
         }
 
-        if (hasPunctuation(clean)) {
+        if (hasPunctuation(candidateNumber)) {
             throw new IllegalArgumentException(illegalPunctuationExceptionMessage);
         }
 
-        if (clean.length() < 10) {
+        if (candidateNumber.length() < 10) {
             throw new IllegalArgumentException(wrongLengthExceptionMessage);
-        } else if (clean.length() > 11) {
+        } else if (candidateNumber.length() > 11) {
             throw new IllegalArgumentException(moreThan11DigitsExceptionMessage);
-        } else if (clean.length() > 10) {
-            if (clean.charAt(0) != '1') {
+        } else if (candidateNumber.length() > 10) {
+            if (candidateNumber.charAt(0) != '1') {
                 throw new IllegalArgumentException(numberIs11DigitsButDoesNotStartWith1ExceptionMessage);
             }
-            clean = clean.substring(1);
+            candidateNumber = candidateNumber.substring(1);
         }
 
         // TODO refactor duplicate check and duplicate string contents
-        if (clean.charAt(0) == '0') {
+        if (candidateNumber.charAt(0) == '0') {
             throw new IllegalArgumentException(areaCodeStartsWithZeroExceptionMessage);
-        } else if (clean.charAt(0) == '1') {
+        } else if (candidateNumber.charAt(0) == '1') {
             throw new IllegalArgumentException(areaCodeStartsWithOneExceptionMessage);
         }
 
         // TODO refactor duplicate check and duplicate string contents
-        if (clean.charAt(3) == '0') {
+        if (candidateNumber.charAt(3) == '0') {
             throw new IllegalArgumentException(exchangeCodeStartsWithZeroExceptionMessage);
-        } else if (clean.charAt(3) == '1') {
+        } else if (candidateNumber.charAt(3) == '1') {
             throw new IllegalArgumentException(exchangeCodeStartsWithOneExceptionMessage);
         }
 
-        phoneNumber = clean;
+        phoneNumber = candidateNumber;
     }
 
-    private boolean hasPunctuation(String phoneNumber) {
-        return phoneNumber.chars().anyMatch(c -> !Character.isDigit(c));
+    private boolean hasPunctuation(String candidateNumber) {
+        return candidateNumber.chars().anyMatch(c -> !Character.isDigit(c));
     }
 
-    private boolean hasAlphabeticCharacters(String phoneNumber) {
-        return phoneNumber.chars().anyMatch(c -> Character.isAlphabetic(c));
+    private boolean hasAlphabeticCharacters(String candidateNumber) {
+        return candidateNumber.chars().anyMatch(c -> Character.isAlphabetic(c));
     }
 
-    private String cleanUp(String rawInput) {
+    private String removePunctuation(String rawInput) {
         return rawInput.replaceAll("[+( )\\.()-]", "");
     }
 
