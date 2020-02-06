eb15734eacffbc669c6501cdf04d9a9c124aeebc
Paul Moore <pdmoore@gmail.com>
2020-02-05 20:35:54 -0800

    c# - resistor-color - refactor IDE suggestion to remove method body

--- csharp/resistor-color/ResistorColor.cs
+++ csharp/resistor-color/ResistorColor.cs
@@ -3,20 +3,7 @@ using System.Linq;
 
 public static class ResistorColor
 {
-    public static int ColorCode(string color)
-    {
-        // switch (color)
-        // {
-        //     case "black"  : return 0;
-        //     case "orange" : return 3;
-        // }
-        //
-        // return 9;
-
-        return Array.IndexOf(Colors(), color);
-        
-        
-    }
+    public static int ColorCode(string color) => Array.IndexOf(Colors(), color);
 
     public static string[] Colors() => new[] {"black", "brown", "red", "orange", "yellow", "green", "blue", "violet", "grey", "white"};
 }
