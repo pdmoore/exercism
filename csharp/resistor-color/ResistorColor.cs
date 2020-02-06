733d03c659bfbd996ce9f1a993da712d0f24ece0
Paul Moore <pdmoore@gmail.com>
2020-02-05 20:35:08 -0800

    c# - resistor-color - refactor look up to search Colors elements

--- csharp/resistor-color/ResistorColor.cs
+++ csharp/resistor-color/ResistorColor.cs
@@ -1,16 +1,21 @@
 ﻿using System;
+using System.Linq;
 
 public static class ResistorColor
 {
     public static int ColorCode(string color)
     {
-        switch (color)
-        {
-            case "black"  : return 0;
-            case "orange" : return 3;
-        }
+        // switch (color)
+        // {
+        //     case "black"  : return 0;
+        //     case "orange" : return 3;
+        // }
+        //
+        // return 9;
+
+        return Array.IndexOf(Colors(), color);
         
-        return 9;
+        
     }
 
     public static string[] Colors() => new[] {"black", "brown", "red", "orange", "yellow", "green", "blue", "violet", "grey", "white"};
