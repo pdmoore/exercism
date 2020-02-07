1bba3f1c38601b985981f9aa77a0901f93c8988c
Paul Moore <pdmoore@gmail.com>
2020-02-07 15:44:17 -0800

    c# - nucleotide count - aborted attempt on linq

--- csharp/nucleotide-count/NucleotideCount.cs
+++ csharp/nucleotide-count/NucleotideCount.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.Linq;
 
 public class NucleotideCount {
     private readonly Dictionary<char, int> countByNucleotide = new Dictionary<char, int> {
