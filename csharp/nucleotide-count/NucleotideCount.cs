1cfbd859c4c395f78a2c6e72a92a9c3f2731698a
Paul Moore <pdmoore@gmail.com>
2020-02-07 15:29:08 -0800

    c# - nucleotide count - empty strand passing

--- csharp/nucleotide-count/NucleotideCount.cs
+++ csharp/nucleotide-count/NucleotideCount.cs
@@ -1,17 +1,18 @@
 using System;
 using System.Collections.Generic;
 
-public class NucleotideCount
-{
-    public NucleotideCount(string sequence)
-    {
+public class NucleotideCount {
+    private Dictionary<char, int> empty = new Dictionary<char, int> {
+        ['A'] = 0,
+        ['C'] = 0,
+        ['G'] = 0,
+        ['T'] = 0
+    };
+
+    public NucleotideCount(string sequence) {
     }
 
-    public IDictionary<char, int> NucleotideCounts
-    {
-        get
-        {
-            throw new NotImplementedException("You need to implement this function.");
-        }
+    public IDictionary<char, int> NucleotideCounts {
+        get { return empty; }
     }
 }
