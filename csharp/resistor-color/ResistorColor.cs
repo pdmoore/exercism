e34b71e7df4d13d9482dea295d3382c28efcc9fe
Paul Moore <pdmoore@gmail.com>
2020-02-06 23:24:11 -0800

    c# - resistor color - refactor to create Colors once, not on each call

--- csharp/resistor-color/ResistorColor.cs
+++ csharp/resistor-color/ResistorColor.cs
@@ -3,7 +3,9 @@ using System.Linq;
 
 public static class ResistorColor
 {
+    private static readonly string[] COLOR_NAMES = new[] {"black", "brown", "red", "orange", "yellow", "green", "blue", "violet", "grey", "white"};
+
     public static int ColorCode(string color) => Array.IndexOf(Colors(), color);
 
-    public static string[] Colors() => new[] {"black", "brown", "red", "orange", "yellow", "green", "blue", "violet", "grey", "white"};
+    public static string[] Colors() => COLOR_NAMES;
 }
