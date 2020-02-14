ec67e2c2a4748f499d3d15e6fe1013be3563727e
Paul Moore <pdmoore@gmail.com>
2020-02-13 16:24:35 -0800

    java - spiral matrix - compiles and empty matrix works

--- java/spiral-matrix/src/main/java/SpiralMatrixBuilder.java
+++ java/spiral-matrix/src/main/java/SpiralMatrixBuilder.java
@@ -1,10 +1,6 @@
-/*
+public class SpiralMatrixBuilder {
 
-Since this exercise has a difficulty of > 4 it doesn't come
-with any starter implementation.
-This is so that you get to practice creating classes and methods
-which is an important part of programming in Java.
-
-Please remove this comment when submitting your solution.
-
-*/
+    public Integer[][] buildMatrixOfSize(int i) {
+        return new Integer[0][];
+    }
+}
