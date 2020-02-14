d8675413ce1373a205f3882d98ce1bfd04bb86f6
Paul Moore <pdmoore@gmail.com>
2020-02-13 16:44:19 -0800

    java - spiral matrix - too big a step, back up

--- java/spiral-matrix/src/main/java/SpiralMatrixBuilder.java
+++ java/spiral-matrix/src/main/java/SpiralMatrixBuilder.java
@@ -1,19 +1,21 @@
 public class SpiralMatrixBuilder {
 
-    public Integer[][] buildMatrixOfSize(int i) {
-        if (i == 0) {
+    public Integer[][] buildMatrixOfSize(int size) {
+        if (size == 0) {
             return new Integer[0][];
-        } else if (i == 1) {
+        } else if (size == 1) {
             return new Integer[][] { {1} };
         }
 
         Integer[][] matrix;
-        matrix = new Integer[i][i];
+        matrix = new Integer[size][size];
         matrix[0][0] = 1;
         matrix[0][1] = 2;
         matrix[1][0] = 4;
         matrix[1][1] = 3;
-        return matrix;
 
+
+
+        return matrix;
     }
 }
