0be6cf1a97c1915b5b213a61eff8491cca160b93
Paul Moore <pdmoore@gmail.com>
2020-02-15 19:49:42 -0800

    java - spiral matrix - thinking through 4 for loop approach

--- java/spiral-matrix/src/main/java/SpiralMatrixBuilder.java
+++ java/spiral-matrix/src/main/java/SpiralMatrixBuilder.java
@@ -1,6 +1,7 @@
 public class SpiralMatrixBuilder {
 
     public Integer[][] buildMatrixOfSize(int size) {
+        //TODO - refactor away these special cases
         if (size == 0) {
             return new Integer[0][];
         } else if (size == 1) {
@@ -10,15 +11,24 @@ public class SpiralMatrixBuilder {
         Integer[][] matrix;
         matrix = new Integer[size][size];
 
-        //TODO - started building a sprial approach of for loops
+        //TODO - started building a spiral approach of for loops
         // Only works for size 2 right now
         // need to consider squeezing the boundaries each pass
+        // following this thinking....across to right, down to bottom, across to left, up to top
+
+
+        // ACROSS TO RIGHT
         int counter = 1;
         for (int i = 0; i < size; i++) {
             matrix[0][i] = counter++;
         }
 
+        // DOWN TO BOTTOM
+        for (int i = 1; i < size; i++) {
+            matrix[i][size - 1] = counter ++;
+        }
 
+        // ACROSS TO LEFT
         for (int i = size - 1; i >= 0; i--) {
             matrix[1][i] = counter++;
         }
