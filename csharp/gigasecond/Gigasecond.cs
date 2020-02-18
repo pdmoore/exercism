c756782f64c80e6da71ea04aca2149b09c1d72ef
Paul Moore <pdmoore@gmail.com>
2020-02-17 19:46:31 -0800

    c# - gigasecond - consistent typing for constants

--- csharp/gigasecond/Gigasecond.cs
+++ csharp/gigasecond/Gigasecond.cs
@@ -2,10 +2,10 @@ using System;
 
 public static class Gigasecond
 {
-    private const int NO_HOURS   = 0;
-    private const int NO_MINUTES = 0;
-    private static int GIGA_SECONDS = Convert.ToInt32(Math.Pow(10, 9));
-    private static TimeSpan ONE_GIGASECOND = new TimeSpan(NO_HOURS, NO_MINUTES, GIGA_SECONDS);
+    private static readonly int NO_HOURS   = 0;
+    private static readonly int NO_MINUTES = 0;
+    private static readonly int GIGA_SECONDS = Convert.ToInt32(Math.Pow(10, 9));
+    private static readonly TimeSpan ONE_GIGASECOND = new TimeSpan(NO_HOURS, NO_MINUTES, GIGA_SECONDS);
 
     public static DateTime Add(DateTime birthDate)
     {
