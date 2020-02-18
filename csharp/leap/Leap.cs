47bdf9076b5d9a4c1b98a9f7860a6c4c943fe6dc
Paul Moore <pdmoore@gmail.com>
2020-02-17 19:32:02 -0800

    c# - leap - refactor check not divisible by 100

--- csharp/leap/Leap.cs
+++ csharp/leap/Leap.cs
@@ -5,6 +5,6 @@ public static class Leap
     public static bool IsLeapYear(int year)
     {
         return (year % 400 == 0) ||
-               (((year % 4) == 0) && !((year % 100) == 0));
+               (((year % 4) == 0) && (year % 100) != 0);
     }
 }
