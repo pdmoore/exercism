934b5f142ab6002eb818ef5ef8462841e1556809
Paul Moore <pdmoore@gmail.com>
2020-02-17 19:19:14 -0800

    c# - two-fer - tests pass with existing solution

--- csharp/two-fer/TwoFer.cs
+++ csharp/two-fer/TwoFer.cs
@@ -2,7 +2,7 @@
 
   public static class TwoFer
   {
-      public static string Name(string input = null)
+      public static string Speak(string input = null)
       {
           string name = input ?? "you";
           return $"One for {name}, one for me.";
