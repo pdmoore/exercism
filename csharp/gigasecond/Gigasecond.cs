c9f026679ecaa430042371ba4583eaf50241742c
Paul Moore <pdmoore@gmail.com>
2020-02-17 19:47:08 -0800

    c# - gigasecond - refactor to expression body

--- csharp/gigasecond/Gigasecond.cs
+++ csharp/gigasecond/Gigasecond.cs
@@ -7,9 +7,5 @@ public static class Gigasecond
     private static readonly int GIGA_SECONDS = Convert.ToInt32(Math.Pow(10, 9));
     private static readonly TimeSpan ONE_GIGASECOND = new TimeSpan(NO_HOURS, NO_MINUTES, GIGA_SECONDS);
 
-    public static DateTime Add(DateTime birthDate)
-    {
-        return birthDate.Add(ONE_GIGASECOND);
-    }
-    
+    public static DateTime Add(DateTime birthDate) => birthDate.Add(ONE_GIGASECOND);
 }
