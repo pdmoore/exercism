4c3960d0bb352c9615e650b71f8ba2062617ad83
Paul Moore <pdmoore@gmail.com>
2020-02-17 19:52:43 -0800

    c# - gigasecond - apparently PascalCasing is convention for C# constants

--- csharp/gigasecond/Gigasecond.cs
+++ csharp/gigasecond/Gigasecond.cs
@@ -2,10 +2,10 @@ using System;
 
 public static class Gigasecond
 {
-    private const int NO_HOURS = 0;
-    private const int NO_MINUTES = 0;
-    private static readonly int GIGA_SECONDS = Convert.ToInt32(Math.Pow(10, 9));
-    private static readonly TimeSpan ONE_GIGASECOND = new TimeSpan(NO_HOURS, NO_MINUTES, GIGA_SECONDS);
+    private const int NoHours = 0;
+    private const int NoMinutes = 0;
+    private static readonly int GigaSeconds = Convert.ToInt32(Math.Pow(10, 9));
+    private static readonly TimeSpan OneGigasecond = new TimeSpan(NoHours, NoMinutes, GigaSeconds);
 
-    public static DateTime Add(DateTime birthDate) => birthDate.Add(ONE_GIGASECOND);
+    public static DateTime Add(DateTime birthDate) => birthDate.Add(OneGigasecond);
 }
