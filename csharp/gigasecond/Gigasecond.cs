d2f0c58afd7e13f0d1b372ec2193719eb114c0d9
Paul Moore <pdmoore@gmail.com>
2020-02-17 19:54:03 -0800

    c# - gigasecond - rename const for consistency

--- csharp/gigasecond/Gigasecond.cs
+++ csharp/gigasecond/Gigasecond.cs
@@ -5,7 +5,7 @@ public static class Gigasecond
     private const int NoHours = 0;
     private const int NoMinutes = 0;
     private static readonly int GigaSeconds = Convert.ToInt32(Math.Pow(10, 9));
-    private static readonly TimeSpan OneGigasecond = new TimeSpan(NoHours, NoMinutes, GigaSeconds);
+    private static readonly TimeSpan OneGigaSecond = new TimeSpan(NoHours, NoMinutes, GigaSeconds);
 
-    public static DateTime Add(DateTime birthDate) => birthDate.Add(OneGigasecond);
+    public static DateTime Add(DateTime birthDate) => birthDate.Add(OneGigaSecond);
 }
