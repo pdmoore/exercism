a1d2c70d3a89df6dfb8c75be1928cffe8e902e96
Paul Moore <pdmoore@gmail.com>
2020-02-19 14:01:02 -0800

    java - spiral matrix - refactor - split movement and calculation of next cell out of single switch

--- java/spiral-matrix/src/main/java/SpiralMatrixBuilder.java
+++ java/spiral-matrix/src/main/java/SpiralMatrixBuilder.java
@@ -14,57 +14,52 @@ public class SpiralMatrixBuilder {
         POPULATE_NEXT_CELL direction = POPULATE_NEXT_CELL.GO_RIGHT;
         Point cellCoordinate = new Point(0, 0);
 
+        //TODO can this be further reduced? Duplication in the moveOneCell after switching direction?
         int counter = 1;
         while (counter <= (size * size)) {
             matrix[cellCoordinate.x][cellCoordinate.y] = counter++;
 
-//            if (keepGoing(direction, cellCoordinate, matrix, size)) {
-//                cellCoordinate = moveOneCell(direction, cellCoordinate);
-//            } else {
-//                direction = changeDirection(direction);
-//                cellCoordinate = moveOneCell(direction, cellCoordinate);
-//            }
-
-            switch (direction) {
-                case GO_RIGHT: {
-                    if (keepGoing(direction, cellCoordinate, matrix, size)) {
-                        ++cellCoordinate.y;
-                    } else {
-                        ++cellCoordinate.x;
-                        direction = POPULATE_NEXT_CELL.GO_DOWN;
-                    }
-                    break;
-                }
-                case GO_DOWN: {
-                    if (keepGoing(direction, cellCoordinate, matrix, size)) {
-                        ++cellCoordinate.x;
-                    } else {
-                        --cellCoordinate.y;
-                        direction = POPULATE_NEXT_CELL.GO_LEFT;
-                    }
-                    break;
-                }
-                case GO_LEFT: {
-                    if (keepGoing(direction, cellCoordinate, matrix, size)) {
-                        --cellCoordinate.y;
-                    } else {
-                        --cellCoordinate.x;
-                        direction = POPULATE_NEXT_CELL.GO_UP;
-                    }
-                    break;
-                }
-                case GO_UP:
-                    if (keepGoing(direction, cellCoordinate, matrix, size)) {
-                        --cellCoordinate.x;
-                    } else {
-                        ++cellCoordinate.y;
-                        direction = POPULATE_NEXT_CELL.GO_RIGHT;
-                    }
-                    break;
+            if (keepGoing(direction, cellCoordinate, matrix, size)) {
+                moveOneCell(direction, cellCoordinate);
+            } else {
+                direction = changeDirection(direction);
+                moveOneCell(direction, cellCoordinate);
             }
         }
 
         return matrix;
+    }
+
+    private void moveOneCell(POPULATE_NEXT_CELL direction, Point cellCoordinate) {
+        switch (direction) {
+            case GO_RIGHT:
+                ++cellCoordinate.y;
+                break;
+            case GO_DOWN:
+                ++cellCoordinate.x;
+                break;
+            case GO_LEFT:
+                --cellCoordinate.y;
+                break;
+            case GO_UP:
+                --cellCoordinate.x;
+                break;
+        }
+
+    }
+
+    private POPULATE_NEXT_CELL changeDirection(POPULATE_NEXT_CELL direction) {
+        switch (direction) {
+            case GO_RIGHT:
+                return POPULATE_NEXT_CELL.GO_DOWN;
+            case GO_DOWN:
+                return POPULATE_NEXT_CELL.GO_LEFT;
+            case GO_LEFT:
+                return POPULATE_NEXT_CELL.GO_UP;
+            case GO_UP:
+                return POPULATE_NEXT_CELL.GO_RIGHT;
+        }
+        return null;
     }
 
     private boolean keepGoing(POPULATE_NEXT_CELL direction, Point cellCoordinate, Integer[][] matrix, int size) {
@@ -74,7 +69,7 @@ public class SpiralMatrixBuilder {
         if (direction == POPULATE_NEXT_CELL.GO_RIGHT) {
             return (((y + 1) <= size - 1) && matrix[x][y + 1] == null);
         } else if (direction == POPULATE_NEXT_CELL.GO_DOWN) {
-            return (x + 1 <= size - 1 && matrix[x+1][y] == null);
+            return (x + 1 <= size - 1 && matrix[x + 1][y] == null);
         } else if (direction == POPULATE_NEXT_CELL.GO_LEFT) {
             return (y - 1 >= 0 && matrix[x][y - 1] == null);
         } else if (direction == POPULATE_NEXT_CELL.GO_UP) {
