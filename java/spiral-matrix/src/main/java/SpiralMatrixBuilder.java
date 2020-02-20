ad45646c84aa508c4a9988743af07aaedf91e018
Paul Moore <pdmoore@gmail.com>
2020-02-19 16:01:02 -0800

    java - spiral matrix - refactor, clean up IDE warnings

--- java/spiral-matrix/src/main/java/SpiralMatrixBuilder.java
+++ java/spiral-matrix/src/main/java/SpiralMatrixBuilder.java
@@ -1,18 +1,13 @@
-import javax.swing.*;
 import java.awt.*;
-import java.awt.geom.Point2D;
 
 public class SpiralMatrixBuilder {
 
-    private static enum POPULATE_NEXT_CELL {GO_RIGHT, GO_DOWN, GO_LEFT, GO_UP}
-
-    ;
+    private enum POPULATE_NEXT_CELL { GO_RIGHT, GO_DOWN, GO_LEFT, GO_UP }
 
     public Integer[][] buildMatrixOfSize(int size) {
-        Integer[][] matrix;
-        matrix = new Integer[size][size];
-        POPULATE_NEXT_CELL direction = POPULATE_NEXT_CELL.GO_RIGHT;
+        Integer[][] matrix = new Integer[size][size];
         Point cellCoordinate = new Point(0, 0);
+        POPULATE_NEXT_CELL direction = POPULATE_NEXT_CELL.GO_RIGHT;
 
         //TODO can this be further reduced? Duplication in the moveOneCell after switching direction?
         int counter = 1;
