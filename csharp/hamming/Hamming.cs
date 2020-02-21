41e8dcf1005317e7052cedfa001fb28d27691ab9
Paul Moore <pdmoore@gmail.com>
2020-02-21 14:52:52 -0800

    c# - hamming - refactor out intermediate var

--- csharp/hamming/Hamming.cs
+++ csharp/hamming/Hamming.cs
@@ -5,9 +5,8 @@ public static class Hamming
 {
     public static int Distance(string firstStrand, string secondStrand)
     {
-
-        var result = firstStrand.Zip(secondStrand);
-        // return result.Select(x => x.First.Equals(x.Second)).Count();
-        return result.Count(x => !x.First.Equals(x.Second));
+        return firstStrand.
+            Zip(secondStrand).
+            Count(x => !x.First.Equals(x.Second));
     }
 }
