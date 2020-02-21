6cd6180df3bac914647198e41b6fa0f1ef07b3fe
Paul Moore <pdmoore@gmail.com>
2020-02-21 14:51:04 -0800

    c# - hamming - linq to count differences in Zip'ped strands

--- csharp/hamming/Hamming.cs
+++ csharp/hamming/Hamming.cs
@@ -1,9 +1,13 @@
 using System;
+using System.Linq;
 
 public static class Hamming
 {
     public static int Distance(string firstStrand, string secondStrand)
     {
-        return 0;
+
+        var result = firstStrand.Zip(secondStrand);
+        // return result.Select(x => x.First.Equals(x.Second)).Count();
+        return result.Where(x => !x.First.Equals(x.Second)).Count();
     }
 }
