b94c0f9dfc0fdc56bbd74e9c9fc3351f5f4354a3
Paul Moore <pdmoore@gmail.com>
2020-02-22 09:38:20 -0800

    java - grade school - starting point

---
+++ java/grade-school/src/main/java/School.java
@@ -0,0 +1,16 @@
+import java.util.List;
+
+public class School {
+
+    public void add(String name, int grade) {
+
+    }
+
+    public List<String> roster() {
+        return null;
+    }
+
+    public List<String> grade(int grade) {
+        return null;
+    }
+}
