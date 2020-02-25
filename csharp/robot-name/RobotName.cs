70ca019c885c506701d882583102904d895406ad
Paul Moore <pdmoore@gmail.com>
2020-02-24 18:13:26 -0800

    c# - robot name - bunch of TODOs to meet spirit of 'random generated' name

--- csharp/robot-name/RobotName.cs
+++ csharp/robot-name/RobotName.cs
@@ -7,6 +7,15 @@ public class Robot
 
     public Robot() => GenerateNewName();
 
+    // TODO - generate New Name works fine for the tests
+    // but isn't in the spirit of the assignment
+    // "they should not follow a predictable sequence"
+    // so - 
+    // - Need to generate a truly random name RandomLetter + RandomLetter + RandomDigit...
+    // - check if rando name is in the list of names
+    // - add if it isn't
+    // - generate new name if it is
+    // - reset removes name from list of names
     private void GenerateNewName() {
         Name = _prefix + _counter.ToString("D3");
         ++_counter;
