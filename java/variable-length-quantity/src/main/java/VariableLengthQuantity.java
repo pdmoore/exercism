55d0a92e662529df84426627daefa808f4461ea8
Paul Moore <pdmoore@gmail.com>
2020-02-27 16:13:40 -0800

    java - variable length quantity - refactor towards encoding large number

--- java/variable-length-quantity/src/main/java/VariableLengthQuantity.java
+++ java/variable-length-quantity/src/main/java/VariableLengthQuantity.java
@@ -5,27 +5,31 @@ class VariableLengthQuantity {
 
     List<String> encode(List<Long> numbers) {
 
-        //TODO - loop through list of numbers and perform translation operation, appending to answer
 
+
+        List<String> encoding = new ArrayList<>();
+
+        //TODO - loop through list of numbers and perform translation operation, appending to answer
+        Long encodeThis = numbers.get(0);
 
         // TODO - instead of numbers get(0), translate number and return List of strings
 
-        Long encodeThis = numbers.get(0);
-
         String answer;
-        List<String> answer2 = new ArrayList<>();
         if (encodeThis < 128) {
             answer = String.format("0x%01x", encodeThis);
-            answer2.add(answer);
+            encoding.add(answer);
         } else {
-            answer = "0x81";
-            answer2.add(answer);
-            answer = "0x0";
-            answer2.add(answer);
+            List<String> largeNumberEncoding = new ArrayList<>();
+            largeNumberEncoding.add("0x81");
+            largeNumberEncoding.add("0x0");
+
+            encoding.addAll(largeNumberEncoding);
         }
 
 
-        return answer2;
+
+
+        return encoding;
     }
 
     List<String> decode(List<Long> bytes) {
