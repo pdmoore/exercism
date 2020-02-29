b8a5296917f10b620b919e6906da114f78573f27
Paul Moore <pdmoore@gmail.com>
2020-02-28 18:03:28 -0800

    java - variable length quantity - algorithm in progress, goes right to left

--- java/variable-length-quantity/src/main/java/VariableLengthQuantity.java
+++ java/variable-length-quantity/src/main/java/VariableLengthQuantity.java
@@ -1,5 +1,6 @@
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Stack;
 
 class VariableLengthQuantity {
 
@@ -32,9 +33,29 @@ class VariableLengthQuantity {
             //10000000
             //10000001 000000
 
+            String numberAsBits = Long.toBinaryString(number);
+            String rightSide = numberAsBits.substring(numberAsBits.length() - 7, numberAsBits.length());
+            System.out.println("just ganked " + rightSide);
+            Stack<String> vlqNumbers = new Stack<>();
+            vlqNumbers.push(String.format("0x%01x", Integer.parseInt(rightSide, 2)));
+
+            numberAsBits = numberAsBits.substring(0, numberAsBits.length() - 7);
+            System.out.println("remainder is now: " + numberAsBits);
+
+            if (numberAsBits.length() < 7) {
+                // it's less than 7, so set left most bit and pad remainder with 0s
+
+                //push it on stack
+                vlqNumbers.push(String.format("0x%01x", Integer.parseInt("10000001", 2)));
+            }
+
+            while (!vlqNumbers.empty()) {
+                numberEncoding.add(vlqNumbers.pop());
+            }
+
             // baby step towards impl
-            numberEncoding.add(String.format("0x%01x", Integer.parseInt("10000001", 2)));
-            numberEncoding.add(String.format("0x%01x", Integer.parseInt("00000000", 2)));
+//            numberEncoding.add(String.format("0x%01x", Integer.parseInt("10000001", 2)));
+//            numberEncoding.add(String.format("0x%01x", Integer.parseInt("00000000", 2)));
 
 
             // hard coded answer
