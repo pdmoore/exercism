4125e998971a4a1de1a9d9a84a3493e9fb902838
Paul Moore <pdmoore@gmail.com>
2020-03-01 15:31:14 -0800

    java - variable length quantity - WIP on general solution for double byte that should solve remaining encode tests

--- java/variable-length-quantity/src/main/java/VariableLengthQuantity.java
+++ java/variable-length-quantity/src/main/java/VariableLengthQuantity.java
@@ -7,7 +7,6 @@ class VariableLengthQuantity {
     List<String> encode(List<Long> numbers) {
 
 
-
         List<String> encoding = new ArrayList<>();
 
         //TODO - loop through list of numbers and perform translation operation, appending to answer
@@ -28,29 +27,52 @@ class VariableLengthQuantity {
 
             System.out.println(number + ": " + Long.toBinaryString(number));
             String binaryLong = Long.toBinaryString(number);
-
-            //       1 0000000
-            //10000001 000000
-
             Stack<String> vlqNumbers = new Stack<>();
 
+            // Going from right to left
+            // if there are 7 or fewer digits left, pad and set left most bit
+            // otherwise push the number as is
+
             String numberAsBits = Long.toBinaryString(number);
+
             String rightSide = numberAsBits.substring(numberAsBits.length() - 7, numberAsBits.length());
             System.out.println("just ganked " + rightSide);
             vlqNumbers.push(String.format("0x%01x", Integer.parseInt(rightSide, 2)));
 
+/** Works for double bytes only **/
             numberAsBits = numberAsBits.substring(0, numberAsBits.length() - 7);
             System.out.println("remainder is now: " + numberAsBits);
 
-//            if (numberAsBits.length() < 7) {
-                // it's less than 7, so set left most bit and pad remainder with 0s
-                numberAsBits = "1" + String.format("%1$7s", numberAsBits).replace(' ', '0');
+            // logic works for double-bytes, not for triple, need a loop around the ganking
+            // it's less than 7, so set left most bit and pad remainder with 0s
+            numberAsBits = "1" + String.format("%1$7s", numberAsBits).replace(' ', '0');
 
-                //push it on stack
-//                vlqNumbers.push(String.format("0x%01x", Integer.parseInt("10000001", 2)));
-                String format = String.format("0x%01x", Integer.parseInt(numberAsBits, 2));
-                vlqNumbers.push(format);
+            String format = String.format("0x%01x", Integer.parseInt(numberAsBits, 2));
+            vlqNumbers.push(format);
+/** end working **/
+
+// CONVERT THE ABOVE INTO A LOOP - process each chunk from right to left, deciding when to add the high bit
+//            while (!numberAsBits.isEmpty()) {
+//                if (numberAsBits.length() < 7) {
+////                    numberAsBits = numberAsBits.substring(0, numberAsBits.length() - 7);
+////                    System.out.println("remainder is now: " + numberAsBits);
+//
+//                    // logic works for double-bytes, not for triple, need a loop around the ganking
+//                    // it's less than 7, so set left most bit and pad remainder with 0s
+//                    numberAsBits = "1" + String.format("%1$7s", numberAsBits).replace(' ', '0');
+//
+//                    String format = String.format("0x%01x", Integer.parseInt(numberAsBits, 2));
+//                    vlqNumbers.push(format);
+//                    numberAsBits = null;
+//                } else {
+//                    String rightSide = numberAsBits.substring(numberAsBits.length() - 7, numberAsBits.length());
+//                    System.out.println("just ganked " + rightSide);
+//                    vlqNumbers.push(String.format("0x%01x", Integer.parseInt(rightSide, 2)));
+//                    numberAsBits = numberAsBits.substring(0, numberAsBits.length() - 7);
+//                }
 //            }
+
+
 
             while (!vlqNumbers.empty()) {
                 numberEncoding.add(vlqNumbers.pop());
