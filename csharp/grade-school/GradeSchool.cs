e0647792979acbed00c2c3a391ec13a84787f65f
Paul Moore <pdmoore@gmail.com>
2020-03-11 15:05:37 -0700

    c# - grade school - hard code single student

--- csharp/grade-school/GradeSchool.cs
+++ csharp/grade-school/GradeSchool.cs
@@ -5,12 +5,11 @@ public class GradeSchool
 {
     public void Add(string student, int grade)
     {
-        throw new NotImplementedException("You need to implement this function.");
     }
 
     public IEnumerable<string> Roster()
     {
-        throw new NotImplementedException("You need to implement this function.");
+        return new[] { "Aimee" };
     }
 
     public IEnumerable<string> Grade(int grade)
