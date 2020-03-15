a114a43da98ef403c56a71d9e0dd347968d5cf70
Paul Moore <pdmoore@gmail.com>
2020-03-14 17:15:21 -0700

    kill some dead code

--- csharp/grade-school/GradeSchool.cs
+++ csharp/grade-school/GradeSchool.cs
@@ -4,14 +4,10 @@ using System.Collections.Generic;
 
 public class GradeSchool
 {
-    private List<string> _names = new List<string>();
     private SortedDictionary<int, List<string>> _namesByGrade = new SortedDictionary<int, List<string>>();
 
     public void Add(string student, int grade)
     {
-        //TODO left off - how to get the list of names and add to it when the 'grade' key already exists in the Dictionary?
-        
-        // map, grade is key, list of names is value
         if (_namesByGrade.ContainsKey(grade) == false)
         { 
             List<string> names = new List<string>();
