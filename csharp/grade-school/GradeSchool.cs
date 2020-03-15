89af909107c35fe25c79f1f496f4752c0b8449b1
Paul Moore <pdmoore@gmail.com>
2020-03-14 17:06:29 -0700

    c# - grade school - store names by grade, in progress

--- csharp/grade-school/GradeSchool.cs
+++ csharp/grade-school/GradeSchool.cs
@@ -5,15 +5,36 @@ using System.Collections.Generic;
 public class GradeSchool
 {
     private List<string> _names = new List<string>();
+    private Dictionary<int, List<string>> _namesByGrade = new Dictionary<int, List<string>>();
 
     public void Add(string student, int grade)
     {
-        _names.Add(student);
+        //TODO left off - how to get the list of names and add to it when the 'grade' key already exists in the Dictionary?
+        
+        // map, grade is key, list of names is value
+        if (_namesByGrade.ContainsKey(grade) == false)
+        { 
+            List<string> names = new List<string>();
+            names.Add(student);
+            _namesByGrade.Add(grade, names);
+        }
+        else
+        {
+           var lazy = _namesByGrade[grade];
+           lazy.Add(student);
+        }
     }
 
     public IEnumerable<string> Roster()
     {
-        return _names;
+        List<string> allNames = new List<string>();
+        foreach (var grade
+            in _namesByGrade.Keys)
+        {
+            allNames.AddRange(_namesByGrade[grade]);
+        }
+
+        return allNames;
     }
 
     public IEnumerable<string> Grade(int grade)
