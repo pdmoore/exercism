b39b984848375b35e834cb65a3701acfac33ff4d
Paul Moore <pdmoore@gmail.com>
2020-03-15 19:45:57 -0700

    c# - grade school - refactor

--- csharp/grade-school/GradeSchool.cs
+++ csharp/grade-school/GradeSchool.cs
@@ -9,27 +9,19 @@ public class GradeSchool
 
     public void Add(string student, int grade)
     {
-        if (_namesByGrade.ContainsKey(grade) == false)
+        if (_namesByGrade.ContainsKey(grade))
         {
-            _namesByGrade.Add(grade, new List<string>() { student });
+            _namesByGrade[grade].Add(student);
+            _namesByGrade[grade].Sort();
         }
         else
         {
-           _namesByGrade[grade].Add(student);
-           _namesByGrade[grade].Sort();
+            _namesByGrade.Add(grade, new List<string>() { student });
         }
     }
 
     public IEnumerable<string> Roster() => _namesByGrade.SelectMany(x => x.Value);
 
-    public IEnumerable<string> Grade(int grade)
-    {
-        //TODO Left off here - need to provide a default if the key is not present
-        // TryGetValue is the right approach, just figuring our the magic incantation 
-        // IEnumerable<string> value = null;
-        // return _namesByGrade.TryGetValue(grade, out value);
-        if (_namesByGrade.ContainsKey(grade)) return _namesByGrade[grade];
-
-        return new List<string>();
-    }
+    //TODO TryGetValue?
+    public IEnumerable<string> Grade(int grade) => _namesByGrade.ContainsKey(grade) ? _namesByGrade[grade] : new List<string>();
 }
