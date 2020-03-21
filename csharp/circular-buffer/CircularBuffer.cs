8dd0607a56a3bbae46c8799a301ac5604ab7602a
Paul Moore <pdmoore@gmail.com>
2020-03-21 16:31:20 -0700

    c# - circular buffer - read from empty buffer throws exception

--- csharp/circular-buffer/CircularBuffer.cs
+++ csharp/circular-buffer/CircularBuffer.cs
@@ -4,12 +4,11 @@ public class CircularBuffer<T>
 {
     public CircularBuffer(int capacity)
     {
-        throw new NotImplementedException("You need to implement this function.");
     }
 
     public T Read()
     {
-        throw new NotImplementedException("You need to implement this function.");
+        throw new InvalidOperationException();
     }
 
     public void Write(T value)
