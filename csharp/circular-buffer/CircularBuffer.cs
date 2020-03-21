57e4dfbd49e66bae6b165d82544215129d6e6f0a
Paul Moore <pdmoore@gmail.com>
2020-03-21 16:36:13 -0700

    c# - circular buffer - simple impl for single item in buffer

--- csharp/circular-buffer/CircularBuffer.cs
+++ csharp/circular-buffer/CircularBuffer.cs
@@ -2,18 +2,28 @@
 
 public class CircularBuffer<T>
 {
+    private int _capacity;
+    private T _value;
+
     public CircularBuffer(int capacity)
     {
+        _capacity = capacity;
+        _value = default(T);
     }
 
     public T Read()
     {
-        throw new InvalidOperationException();
+        if (_value.Equals(default(T)))
+        {
+            throw new InvalidOperationException();
+        }
+
+        return _value;
     }
 
     public void Write(T value)
     {
-        throw new NotImplementedException("You need to implement this function.");
+        _value = value;
     }
 
     public void Overwrite(T value)
