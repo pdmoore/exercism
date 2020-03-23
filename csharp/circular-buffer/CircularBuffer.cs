70fff06bc4fc1701c74928d8d0581c478f179ec2
Paul Moore <pdmoore@gmail.com>
2020-03-22 21:11:33 -0700

    c# - circular buffer - more real impl, probably too much code

--- csharp/circular-buffer/CircularBuffer.cs
+++ csharp/circular-buffer/CircularBuffer.cs
@@ -1,33 +1,48 @@
 ﻿using System;
+using System.Collections.Generic;
 
 public class CircularBuffer<T>
 {
-    private T _value;
+    private T[] _buffer2;
+    private int _head;
+    private int _tail;
+    private int _capacity;
+    private int _size;
     
-    // Need real impl
-    // dynamic LL? need to maintain a size value and compare against capacity
-    // dynamic LL - track head and tail
-
     public CircularBuffer(int capacity)
     {
-        _value = default(T);
+        _buffer2 = new T[capacity];
+        _head = 0;
+        _tail = 0;
+        _capacity = capacity;
+        _size = 0;
     }
 
     public T Read()
     {
-        if (_value.Equals(default(T)))
+        if (_size == 0)
         {
             throw new InvalidOperationException();
         }
 
-        T result = _value;
-        _value = default(T);
+        T result = _buffer2[_tail];
+        _buffer2[_tail] = default(T);
+
+        _tail++;
+        _size--;
+        
         return result;
     }
 
     public void Write(T value)
     {
-        _value = value;
+        _buffer2[_head] = value;
+        _head++;
+        _size++;
+        if (_head == _capacity)
+        {
+            _head = 0;
+        }
     }
 
     public void Overwrite(T value)
