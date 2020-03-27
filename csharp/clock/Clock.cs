ae3f845a8d7e24337b0fd3d9c754b2daa753cd0c
Paul Moore <pdmoore@gmail.com>
2020-03-26 20:40:15 -0700

    c# - clock - remove _hours field

--- csharp/clock/Clock.cs
+++ csharp/clock/Clock.cs
@@ -3,14 +3,12 @@ using System.Text;
 
 public class Clock
 {
-    private readonly int _hours;
     private readonly int _minutes;
     private readonly string TwoDecimalPlaces = "D2";
 
     public Clock(int hours, int minutes)
     {
-        _hours = ReduceHours(hours);
-        _minutes = minutes + (_hours * 60);
+        _minutes = minutes + (ReduceHours(hours) * 60);
     }
 
     private int ReduceHours(in int hours) => hours % 24;
@@ -27,13 +25,10 @@ public class Clock
 
     public override string ToString()
     {
-        StringBuilder sb = new StringBuilder();
-
         int hours = _minutes / 60;
         int minutes = _minutes % 60;
 
-
-
+        StringBuilder sb = new StringBuilder();
         sb.Append(hours.ToString(TwoDecimalPlaces));
         sb.Append(":");
         sb.Append(minutes.ToString(TwoDecimalPlaces));
