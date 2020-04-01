a5d0a9636f31c7e0b5d1c280dfdc77b3a3bf78de
Paul Moore <pdmoore@gmail.com>
2020-04-01 13:48:33 -0700

    php - hamming - check strand lengths match, throw exception when they don't

--- php/hamming/hamming.php
+++ php/hamming/hamming.php
@@ -9,6 +9,11 @@ Remove this comment before submitting your exercise.
 
 function distance(string $strandA, string $strandB) : int
 {
+    if (strlen($strandA) != strlen($strandB)) {
+        throw new InvalidArgumentException('DNA strands must be of equal length.');
+    }
+
+
     $count = 0;
 
     for ($i=0; $i<strlen($strandA); $i++) {
