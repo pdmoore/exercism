272897b4e090751a1f8eb84e77f36ba4bb86d2e6
Paul Moore <pdmoore@gmail.com>
2020-04-01 13:48:57 -0700

    php - hamming - refactor, remove whitespace

--- php/hamming/hamming.php
+++ php/hamming/hamming.php
@@ -13,14 +13,11 @@ function distance(string $strandA, string $strandB) : int
         throw new InvalidArgumentException('DNA strands must be of equal length.');
     }
 
-
     $count = 0;
-
     for ($i=0; $i<strlen($strandA); $i++) {
        if ($strandA[$i] != $strandB[$i]) {
            $count++;
        }
     }
-
     return $count;
 }
