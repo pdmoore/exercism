2b88c8a9cc706a89f7f3122edae31e8487fcf539
Paul Moore <pdmoore@gmail.com>
2020-04-01 14:56:21 -0700

    php - hamming - remove comment - PhpStorm elided it...

--- php/hamming/hamming.php
+++ php/hamming/hamming.php
@@ -1,12 +1,5 @@
 <?php
 
-/*
-This is only a SKELETON file for the "Hamming" exercise. It's been provided as a
-convenience to get you started writing code faster.
-
-Remove this comment before submitting your exercise.
-*/
-
 function distance(string $strandA, string $strandB) : int
 {
     if (strlen($strandA) != strlen($strandB)) {
