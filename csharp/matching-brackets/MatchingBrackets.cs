b3f20ef6491891dbb432143a49a050df5ab118b3
Paul Moore <pdmoore@gmail.com>
2020-04-04 16:43:25 -0700

    c# - matching brackets - start using stack to track state of seen open items

--- csharp/matching-brackets/MatchingBrackets.cs
+++ csharp/matching-brackets/MatchingBrackets.cs
@@ -1,12 +1,16 @@
 using System;
+using System.Collections;
 
 public static class MatchingBrackets
 {
     public static bool IsPaired(string input)
     {
+        // TODO - switch to stack - push on any open character, pop on any close, closed item must match what is popped
+        // after exhausting string chars, return stack isEmpty
         //TODO duplication
         // TODO next test will require checking that open/close pair match before any other character detected
         // maybe it's state based? initial -> any open -> [any open | specific close] -> all close
+        Stack openPair = new Stack();
         
         int bracketCount = 0;
         int curlyCount = 0;
@@ -16,24 +20,30 @@ public static class MatchingBrackets
             if (c.Equals('['))
             {
                 bracketCount++;
+                openPair.Push(c);
             } else if (c.Equals(']'))
             {
+                if (openPair.Pop() != "]") return false;
                 bracketCount--;
             }
             
             if (c.Equals('{'))
             {
                 curlyCount++;
+                openPair.Push(c);
             } else if (c.Equals('}'))
             {
+                if (openPair.Pop() != "}") return false;
                 curlyCount--;
             }
 
             if (c.Equals('('))
             {
                 parenCount++;
+                openPair.Push(c);
             } else if (c.Equals(')'))
             {
+                if (openPair.Pop() != "(") return false;
                 parenCount--;
             }
 
