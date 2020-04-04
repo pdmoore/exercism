5f079f9557ea42bdad8e1a0a08d831d8e55c8176
Paul Moore <pdmoore@gmail.com>
2020-04-04 16:50:04 -0700

    c# - matching brackets - unneeded code has a bug

--- csharp/matching-brackets/MatchingBrackets.cs
+++ csharp/matching-brackets/MatchingBrackets.cs
@@ -21,8 +21,8 @@ public static class MatchingBrackets
             }
             
             switch (c) {
-                case ']' when openPair.Pop() != "]":
-                case '}' when openPair.Pop() != "}":
+                case ']' when openPair.Pop() != "x":
+                case '}' when openPair.Pop() != "y":
                 case ')' when openPair.Pop() != "(":
                     return false;
             }
