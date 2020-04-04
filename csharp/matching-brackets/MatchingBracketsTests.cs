777db82f2e531a4f231efc9daf290326f9aac435
Paul Moore <pdmoore@gmail.com>
2020-04-03 21:15:26 -0700

    c# - matching brackets - side by side works (by mistake)

--- csharp/matching-brackets/MatchingBracketsTests.cs
+++ csharp/matching-brackets/MatchingBracketsTests.cs
@@ -60,7 +60,7 @@ public class MatchingBracketsTests
         Assert.True(MatchingBrackets.IsPaired(value));
     }
 
-    [Fact(Skip = "Remove to run test")]
+    [Fact]
     public void Several_paired_brackets()
     {
         var value = "{}[]";
