4cad52588966f98c2d2a68161cb24ad119a13568
Paul Moore <pdmoore@gmail.com>
2020-04-05 14:12:19 -0700

    c# - matching brackets - add underscore to field name

--- csharp/matching-brackets/MatchingBrackets.cs
+++ csharp/matching-brackets/MatchingBrackets.cs
@@ -3,27 +3,25 @@ using System.Collections.Generic;
 
 public static class MatchingBrackets
 {
-    private static readonly Dictionary<string, string> pairs = new Dictionary<string, string>();
+    private static readonly Dictionary<string, string> _pairs = new Dictionary<string, string>();
 
     static MatchingBrackets() {
-        pairs.Add("(", ")");
-        pairs.Add("[", "]");
-        pairs.Add("{", "}");
+        _pairs.Add("(", ")");
+        _pairs.Add("[", "]");
+        _pairs.Add("{", "}");
     }
 
     public static bool IsPaired(string input)
     {
-        //TODO - key/value mapping opener to closer?
-        
         Stack openPair = new Stack();
 
         foreach (char c in input)
         {
-            if (pairs.ContainsKey(c.ToString()))
+            if (_pairs.ContainsKey(c.ToString()))
             {
                 openPair.Push(c);
             }
-            else if (pairs.ContainsValue(c.ToString()))
+            else if (_pairs.ContainsValue(c.ToString()))
             {
                 if (openPair.Count == 0)
                     return false;
