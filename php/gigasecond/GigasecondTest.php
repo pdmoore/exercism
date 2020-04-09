7f73ac3c13cae1623f6f676c79bdf86917fcffde
Paul Moore <pdmoore@gmail.com>
2020-04-08 22:26:34 -0700

    php - gigasecond - able to run test

--- php/gigasecond/GigasecondTest.php
+++ php/gigasecond/GigasecondTest.php
@@ -7,6 +7,11 @@ class GigasecondTest extends PHPUnit\Framework\TestCase
         require_once 'gigasecond.php';
     }
 
+    public function testNoDifferenceBetweenIdenticalStrands() : void
+    {
+        $this->assertEquals(0, 1);
+    }
+
     public function dateSetup($date) : DateTimeImmutable
     {
         $UTC = new DateTimeZone('UTC');
