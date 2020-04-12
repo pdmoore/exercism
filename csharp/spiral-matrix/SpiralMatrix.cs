42511e6cd837c8cd4335015e602e602509184b31
Paul Moore <pdmoore@gmail.com>
2020-04-12 12:52:09 -0700

    c# - spiral matrix - fill in the first row via for loop

--- csharp/spiral-matrix/SpiralMatrix.cs
+++ csharp/spiral-matrix/SpiralMatrix.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Threading;
 
 public class SpiralMatrix
 {
@@ -11,23 +12,23 @@ public class SpiralMatrix
 
 
         int[,] matrix = new int[size, size];
+
+        int count = 1;
+        for (int col = 0; col < size; col++)
+        {
+            matrix[0, col] = count++;
+        }
+        
         if (size == 1)
         {
-            matrix[0, 0] = 1;
         }
         else if (size == 2)
         {
-            matrix[0, 0] = 1;
-            matrix[0, 1] = 2;
             matrix[1, 0] = 4;
             matrix[1, 1] = 3;
         }
         else
         {
-            matrix[0, 0] = 1;
-            matrix[0, 1] = 2;
-            matrix[0, 2] = 3;
-            
             matrix[1, 0] = 8;
             matrix[1, 1] = 9;
             matrix[1, 2] = 4;
