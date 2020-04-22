67c68effd5fc043d096afb828bf9a0f894dfb094
Paul Moore <pdmoore@gmail.com>
2020-04-21 20:29:16 -0700

    java - vlq - another named constant

--- java/variable-length-quantity/src/main/java/VariableLengthQuantity.java
+++ java/variable-length-quantity/src/main/java/VariableLengthQuantity.java
@@ -5,7 +5,8 @@ import java.util.stream.Collectors;
 
 class VariableLengthQuantity {
 
-    private static final String LAST_BYTE_OF_SERIES = "0";
+    private static final String LAST_BYTE_IN_SERIES = "0";
+    private static final String PRECEDING_BYTE_IN_SERIES = "1";
 
     List<String> encode(List<Long> numbers) {
 
@@ -23,9 +24,9 @@ class VariableLengthQuantity {
         while (!remainingBits.isEmpty()) {
             String SevenBitByte;
 
-            String bit7 = "1";
+            String bit7 = PRECEDING_BYTE_IN_SERIES;
             if (lastByte) {
-                bit7 = LAST_BYTE_OF_SERIES;
+                bit7 = LAST_BYTE_IN_SERIES;
                 lastByte = false;
             }
             String bits0to6;
