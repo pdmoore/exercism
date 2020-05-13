abe808620305f8f2d67530636c64c314878a2a2a
Paul Moore <pdmoore@gmail.com>
2020-05-13 13:27:26 -0700

    spiral matrix - generic solution works for 1/2/3

--- csharp/spiral-matrix/SpiralMatrix.cs
+++ csharp/spiral-matrix/SpiralMatrix.cs
@@ -11,57 +11,46 @@
         int[,] matrix = new int[size, size];
 
         int count = 1;
-        int row = 0;
-        for (int col = 0; col < size; col++)
+        int limitLeft = 0;
+        int limitRight = size;
+        int limitTop = 0;
+        int limitBottom = size;
+
+        while (count <= size * size)
         {
-            matrix[row, col] = count++;
-        }
-        
-        if (size >= 2)
-        {
-            count = 1;
-
-            int limitLeft = 0;
-            int limitRight = size;
-            int limitTop = 0;
-            int limitBottom = size;
-
-
-            while (count <= size * size)
+            // go right
+            for (int fillColumn = limitLeft; fillColumn < limitRight; fillColumn++)
             {
-                // go right
-                for (int fillColumn = limitLeft; fillColumn < limitRight; fillColumn++)
-                {
-                    matrix[limitTop, fillColumn] = count++;
-                }
-
-                limitTop++;
-
-                // go down
-                for (int fillRow = limitTop; fillRow < limitBottom; fillRow++)
-                {
-                    matrix[fillRow, limitRight - 1] = count++;
-                }
-
-                limitRight--;
-
-                // go left
-                for (int fillColumn = limitRight - 1; fillColumn >= limitLeft; fillColumn--)
-                {
-                    matrix[limitBottom - 1, fillColumn] = count++;
-                }
-
-                limitBottom--;
-                // go up
-                for (int fillRow = limitBottom - 1; fillRow >= limitTop; fillRow--)
-                {
-                    matrix[fillRow, limitLeft] = count++;
-                }
-
-                limitLeft++;
+                matrix[limitTop, fillColumn] = count++;
             }
+
+            limitTop++;
+
+            // go down
+            for (int fillRow = limitTop; fillRow < limitBottom; fillRow++)
+            {
+                matrix[fillRow, limitRight - 1] = count++;
+            }
+
+            limitRight--;
+
+            // go left
+            for (int fillColumn = limitRight - 1; fillColumn >= limitLeft; fillColumn--)
+            {
+                matrix[limitBottom - 1, fillColumn] = count++;
+            }
+
+            limitBottom--;
+            // go up
+            for (int fillRow = limitBottom - 1; fillRow >= limitTop; fillRow--)
+            {
+                matrix[fillRow, limitLeft] = count++;
+            }
+
+            limitLeft++;
         }
-        
+
+
         return matrix;
     }
 }
