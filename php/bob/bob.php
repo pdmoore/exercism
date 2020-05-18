a4971ca05c25f85f9a0a93ee6ab13c5a7950bca3
Paul Moore <pdmoore@gmail.com>
2020-05-17 21:33:24 -0700

    php - bob - extract yelling method

--- php/bob/bob.php
+++ php/bob/bob.php
@@ -4,10 +4,15 @@ class Bob
 {
     public function respondTo(string $string)
     {
-        if (strpos($string, "!") !== false) {
+        if ($this->isYelling($string)) {
             return "Whoa, chill out!";
         }
 
         return "Whatever.";
+    }
+
+    public function isYelling(string $string): bool
+    {
+        return strpos($string, "!") !== false;
     }
 }
