67ef56cba8efc1f8422cb24ca4d48e2aef95099a
Paul Moore <pdmoore@gmail.com>
2020-05-17 21:32:16 -0700

    php - bob - handle yelling

--- php/bob/bob.php
+++ php/bob/bob.php
@@ -1,8 +1,13 @@
 <?php
 
-class Bob {
+class Bob
+{
     public function respondTo(string $string)
     {
+        if (strpos($string, "!") !== false) {
+            return "Whoa, chill out!";
+        }
+
         return "Whatever.";
     }
 }
