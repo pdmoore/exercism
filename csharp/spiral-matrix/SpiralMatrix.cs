b3b29cd638fa744db9fca2aa5231b5cb7f0e6b3b
Paul Moore <pdmoore@gmail.com>
2020-05-24 23:08:57 -0700

    c# - spiral matrix - steps towards cleaner impl

--- csharp/spiral-matrix/SpiralMatrix.cs
+++ csharp/spiral-matrix/SpiralMatrix.cs
@@ -1,46 +1,49 @@
 ﻿public class SpiralMatrix
 {
+    static int _count;
+    static int[,] _matrix;
+    
     public static int[,] GetMatrix(int size)
     {
-        int[,] matrix = new int[size, size];
-
-        int count = 1;
+        _matrix = new int[size, size];
+        _count = 1;
+        
         int limitLeft = 0;
         int limitRight = size - 1;
         int limitTop = 0;
         int limitBottom = size - 1;
 
-        while (count <= size * size)
+        while (_count <= size * size)
         {
             // fill across right
             for (int fillColumn = limitLeft; fillColumn <= limitRight; fillColumn++)
             {
-                matrix[limitTop, fillColumn] = count++;
+                _matrix[limitTop, fillColumn] = _count++;
             }
             limitTop++;
-
+            
             // fill down rightmost unfilled edge
             for (int fillRow = limitTop; fillRow <= limitBottom; fillRow++)
             {
-                matrix[fillRow, limitRight] = count++;
+                _matrix[fillRow, limitRight] = _count++;
             }
             limitRight--;
 
             // fill across bottom, to the left
             for (int fillColumn = limitRight; fillColumn >= limitLeft; fillColumn--)
             {
-                matrix[limitBottom, fillColumn] = count++;
+                _matrix[limitBottom, fillColumn] = _count++;
             }
             limitBottom--;
             
             // fill up leftmost unfilled edge
             for (int fillRow = limitBottom; fillRow >= limitTop; fillRow--)
             {
-                matrix[fillRow, limitLeft] = count++;
+                _matrix[fillRow, limitLeft] = _count++;
             }
             limitLeft++;
         }
 
-        return matrix;
+        return _matrix;
     }
 }
