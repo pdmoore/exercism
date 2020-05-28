9bdb8019768f4664684da85c3babed0da80faba0
Paul Moore <pdmoore@gmail.com>
2020-05-27 22:42:27 -0700

    php - rna - single letter rna transciption in place

--- php/rna-transcription/rna-transcription.php
+++ php/rna-transcription/rna-transcription.php
@@ -3,7 +3,17 @@
 class rnatranscription {
 
     public static function toRna(string $rna): string {
-        return 'Foo';
+        switch ($rna) {
+            case 'A':
+                return 'U';
+            case 'C':
+                return 'G';
+            case 'G':
+                return 'C';
+            case 'T':
+                return 'A';
+        }
+        return 'bad input';
     }
 
 
