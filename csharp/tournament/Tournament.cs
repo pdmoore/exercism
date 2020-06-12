e8f556fea02509c50065908742c4850480ae49cc
Paul Moore <pdmoore@gmail.com>
2020-06-11 21:17:45 -0700

    csharp - tournament - TODO for next time

--- csharp/tournament/Tournament.cs
+++ csharp/tournament/Tournament.cs
@@ -5,6 +5,7 @@ public static class Tournament
 {   
     public static void Tally(Stream inStream, Stream outStream)
     {
+        //TODO - split the header into it's own thing, format to ensure spacing is correct    
         StreamWriter sw = new StreamWriter(outStream);
         sw.Write("Team                           | MP |  W |  D |  L |  P");
         sw.Flush();
