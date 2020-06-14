02f13b28b08049bc5b19a0c72f9218b6e1a80749
Paul Moore <pdmoore@gmail.com>
2020-06-13 21:10:18 -0700

    csharp - tournament - center column heading

--- csharp/tournament/Tournament.cs
+++ csharp/tournament/Tournament.cs
@@ -1,9 +1,11 @@
 ﻿using System;
 using System.IO;
+using System.Runtime.CompilerServices;
 
 public static class Tournament
 {
     private const string ColumnSeparator = "|";
+
     public static void Tally(Stream inStream, Stream outStream)
     {
         //TODO - split the header into it's own thing, format to ensure spacing is correct    
@@ -11,8 +13,21 @@ public static class Tournament
         // sw.Write("Team                           ");
         sw.Write("Team".PadRight(31, ' '));
         sw.Write(ColumnSeparator);
-        sw.Write(" MP |  W |  D |  L |  P");
-        
+        sw.Write("MP".CenterTitle(4));
+        sw.Write("|  W |  D |  L |  P");
+
         sw.Flush();
+    }
+
+
+}
+
+public static class StringExtensions
+{
+    public static string CenterTitle(this string str, int totalWidth, char paddingChar = ' ')
+    {
+        int padding = totalWidth - str.Length;
+        int padLeft = padding / 2 + str.Length;
+        return str.PadLeft(padLeft, paddingChar).PadRight(totalWidth, paddingChar);
     }
 }
