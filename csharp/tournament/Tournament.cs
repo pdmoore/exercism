75ab37b052d8e1998bd5438124e6cb16e594655e
Paul Moore <pdmoore@gmail.com>
2020-06-13 21:05:16 -0700

    csharp - tournament - introduce constant for column separator

--- csharp/tournament/Tournament.cs
+++ csharp/tournament/Tournament.cs
@@ -2,14 +2,16 @@
 using System.IO;
 
 public static class Tournament
-{   
+{
+    private const string ColumnSeparator = "|";
     public static void Tally(Stream inStream, Stream outStream)
     {
         //TODO - split the header into it's own thing, format to ensure spacing is correct    
         StreamWriter sw = new StreamWriter(outStream);
         // sw.Write("Team                           ");
         sw.Write("Team".PadRight(31, ' '));
-        sw.Write("| MP |  W |  D |  L |  P");
+        sw.Write(ColumnSeparator);
+        sw.Write(" MP |  W |  D |  L |  P");
         
         sw.Flush();
     }
