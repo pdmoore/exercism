a9f739e039c9556cce40d02c5bcda19f4c8268c6
Paul Moore <pdmoore@gmail.com>
2020-06-20 18:37:30 -0700

    csharp - tournament - many lines of input handled

--- csharp/tournament/TournamentTests.cs
+++ csharp/tournament/TournamentTests.cs
@@ -100,7 +100,7 @@ public class TournamentTests
         Assert.Equal(expected, RunTally(rows));
     }
 
-    [Fact(Skip = "Remove this Skip property to run this test")]
+    [Fact]
     public void Typical_input()
     {
         var rows = 
