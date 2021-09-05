3c8599fea7ae0c52d57b6a3665e808d66c965f5e
Paul Moore <pdmoore@gmail.com>
2021-09-05 14:24:09 -0700

    java - lasagna - starting

---
+++ java/lasagna/src/main/java/Lasagna.java
@@ -0,0 +1,17 @@
+public class Lasagna {
+    public int expectedMinutesInOven() {
+        return 40;
+    }
+
+    public boolean remainingMinutesInOven(int i) {
+        return false;
+    }
+
+    public boolean preparationTimeInMinutes(int i) {
+        return false;
+    }
+
+    public boolean totalTimeInMinutes(int i, int i1) {
+        return false;
+    }
+}
