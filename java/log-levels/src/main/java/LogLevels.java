f72ec4182785d5d82788e7851cef64b8060cf3bd
Paul Moore <pdmoore@gmail.com>
2021-10-08 20:35:58 -0700

    java - log-levels - refactor for readability

--- java/log-levels/src/main/java/LogLevels.java
+++ java/log-levels/src/main/java/LogLevels.java
@@ -3,11 +3,16 @@ import java.util.Locale;
 public class LogLevels {
     
     public static String message(String logLine) {
-        return logLine.split(":")[1].trim();
+        return logLine
+                .split(":")[1]
+                .trim();
     }
 
     public static String logLevel(String logLine) {
-        return logLine.split("]")[0].substring(1).toLowerCase(Locale.ROOT);
+        return logLine
+                .split("]")[0]
+                .substring(1)
+                .toLowerCase(Locale.ROOT);
     }
 
     public static String reformat(String logLine) {
