96c4c85a1de4775f44090474b8563e225725dac3
Paul Moore <pdmoore@gmail.com>
2021-10-08 20:19:00 -0700

    java - log-levels - starting

---
+++ java/log-levels/src/main/java/LogLevels.java
@@ -0,0 +1,14 @@
+public class LogLevels {
+    
+    public static String message(String logLine) {
+        return "Stack overflow";
+    }
+
+    public static String logLevel(String logLine) {
+        throw new UnsupportedOperationException("Please implement the (static) LogLine.logLevel() method");
+    }
+
+    public static String reformat(String logLine) {
+        throw new UnsupportedOperationException("Please implement the (static) LogLine.reformat() method");
+    }
+}
