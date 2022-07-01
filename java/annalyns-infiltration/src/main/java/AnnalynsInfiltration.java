c64df90fd734f4583cfe22d759a2e2a519146f48
Paul Moore <pdmoore@gmail.com>
2022-06-30 22:44:20 -0700

    java - annalyn infiltration canSpy all cases

--- java/annalyns-infiltration/src/main/java/AnnalynsInfiltration.java
+++ java/annalyns-infiltration/src/main/java/AnnalynsInfiltration.java
@@ -5,12 +5,21 @@ class AnnalynsInfiltration {
 
     public static boolean canSpy(boolean knightIsAwake, boolean archerIsAwake, boolean prisonerIsAwake) {
         boolean everyoneIsAwake = (knightIsAwake && archerIsAwake && prisonerIsAwake);
-        boolean eitherFoeIsAsleep = !knightIsAwake || !archerIsAwake;
-        return everyoneIsAwake || eitherFoeIsAsleep;
+        boolean bothFoesAreSleeping = !knightIsAwake && !archerIsAwake;
+
+        if ((prisonerIsAwake && bothFoesAreSleeping) ||
+        (prisonerIsAwake && !knightIsAwake) ||
+        (prisonerIsAwake && !archerIsAwake) ||
+                (!bothFoesAreSleeping && !prisonerIsAwake) ||
+        everyoneIsAwake) {
+            return true;
+        }
+
+        return false;
     }
 
     public static boolean canSignalPrisoner(boolean archerIsAwake, boolean prisonerIsAwake) {
-        throw new UnsupportedOperationException("Please implement the (static) AnnalynsInfiltration.canSignalPrisoner() method");
+        return prisonerIsAwake;
     }
 
     public static boolean canFreePrisoner(boolean knightIsAwake, boolean archerIsAwake, boolean prisonerIsAwake, boolean petDogIsPresent) {
