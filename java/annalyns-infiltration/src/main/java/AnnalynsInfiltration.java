5b3d6272141bc4f6e1e6a96b90af5ef1e995760f
Paul Moore <pdmoore@gmail.com>
2022-07-01 14:11:35 -0700

    java - annalyn infiltration finished

--- java/annalyns-infiltration/src/main/java/AnnalynsInfiltration.java
+++ java/annalyns-infiltration/src/main/java/AnnalynsInfiltration.java
@@ -4,18 +4,8 @@ class AnnalynsInfiltration {
     }
 
     public static boolean canSpy(boolean knightIsAwake, boolean archerIsAwake, boolean prisonerIsAwake) {
-        boolean everyoneIsAwake = (knightIsAwake && archerIsAwake && prisonerIsAwake);
-        boolean bothFoesAreSleeping = !knightIsAwake && !archerIsAwake;
-
-        if ((prisonerIsAwake && bothFoesAreSleeping) ||
-        (prisonerIsAwake && !knightIsAwake) ||
-        (prisonerIsAwake && !archerIsAwake) ||
-                (!bothFoesAreSleeping && !prisonerIsAwake) ||
-        everyoneIsAwake) {
-            return true;
-        }
-
-        return false;
+        boolean everyoneIsAsleep = !knightIsAwake && !archerIsAwake && !prisonerIsAwake;
+        return  !everyoneIsAsleep;
     }
 
     public static boolean canSignalPrisoner(boolean archerIsAwake, boolean prisonerIsAwake) {
@@ -24,16 +14,8 @@ class AnnalynsInfiltration {
 
     public static boolean canFreePrisoner(boolean knightIsAwake, boolean archerIsAwake, boolean prisonerIsAwake, boolean petDogIsPresent) {
         boolean foesAreAsleep = !knightIsAwake && !archerIsAwake;
+        if (foesAreAsleep && prisonerIsAwake) return true;
 
-        if (petDogIsPresent) {
-            if (knightIsAwake && !archerIsAwake && !prisonerIsAwake) return true;
-            if (!foesAreAsleep && !prisonerIsAwake) return false;
-            if (!archerIsAwake) return true;
-            if (prisonerIsAwake && !foesAreAsleep) return false;
-        }
-
-        return petDogIsPresent || (prisonerIsAwake && foesAreAsleep);
-
-
+        return (petDogIsPresent && !archerIsAwake);
     }
 }
