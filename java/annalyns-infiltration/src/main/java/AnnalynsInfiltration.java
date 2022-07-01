3d374dd140afb32bc6b7ef82e289b8b3cff3dd8c
Paul Moore <pdmoore@gmail.com>
2022-06-30 22:36:24 -0700

    java - annalyn infiltration canFastAttack & canSpy impl

--- java/annalyns-infiltration/src/main/java/AnnalynsInfiltration.java
+++ java/annalyns-infiltration/src/main/java/AnnalynsInfiltration.java
@@ -1,10 +1,12 @@
 class AnnalynsInfiltration {
     public static boolean canFastAttack(boolean knightIsAwake) {
-        throw new UnsupportedOperationException("Please implement the (static) AnnalynsInfiltration.canFastAttack() method");
+        return !knightIsAwake;
     }
 
     public static boolean canSpy(boolean knightIsAwake, boolean archerIsAwake, boolean prisonerIsAwake) {
-        throw new UnsupportedOperationException("Please implement the (static) AnnalynsInfiltration.canSpy() method");
+        boolean everyoneIsAwake = (knightIsAwake && archerIsAwake && prisonerIsAwake);
+        boolean eitherFoeIsAsleep = !knightIsAwake || !archerIsAwake;
+        return everyoneIsAwake || eitherFoeIsAsleep;
     }
 
     public static boolean canSignalPrisoner(boolean archerIsAwake, boolean prisonerIsAwake) {
