30f8cb89123e82a02b69437641d96b4c5b418161
Paul Moore <pdmoore@gmail.com>
2022-06-30 22:51:42 -0700

    java - annalyn infiltration all Tests pass, need to refactor

--- java/annalyns-infiltration/src/main/java/AnnalynsInfiltration.java
+++ java/annalyns-infiltration/src/main/java/AnnalynsInfiltration.java
@@ -24,6 +24,16 @@ class AnnalynsInfiltration {
 
     public static boolean canFreePrisoner(boolean knightIsAwake, boolean archerIsAwake, boolean prisonerIsAwake, boolean petDogIsPresent) {
         boolean foesAreAsleep = !knightIsAwake && !archerIsAwake;
+
+        if (petDogIsPresent) {
+            if (knightIsAwake && !archerIsAwake && !prisonerIsAwake) return true;
+            if (!foesAreAsleep && !prisonerIsAwake) return false;
+            if (!archerIsAwake) return true;
+            if (prisonerIsAwake && !foesAreAsleep) return false;
+        }
+
         return petDogIsPresent || (prisonerIsAwake && foesAreAsleep);
+
+
     }
 }
