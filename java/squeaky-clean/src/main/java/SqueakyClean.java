54132f4c53c2c102a9ff62f83d7858cce6b02f23
Paul Moore <pdmoore@gmail.com>
2022-07-03 10:40:59 -0700

    java squeaky clean - tests all pass

--- java/squeaky-clean/src/main/java/SqueakyClean.java
+++ java/squeaky-clean/src/main/java/SqueakyClean.java
@@ -10,22 +10,27 @@ class SqueakyClean {
         // !isCharacter -> nothing
 
         StringBuffer sb = new StringBuffer();
+        boolean convertToCamel = false;
         for (int c :
                 identifier.codePoints().toArray()) {
 
-
-            if (c >= 'α'&& c <= 'ω') {
+            if (c == '-') {
+                convertToCamel = true;
+            } else if (c >= 'α' && c <= 'ω') {
                 // ignore Greek lower case
             } else if (Character.isLetter(c)) {
-                sb.append(Character.toChars(c));
-            }
-            else if (Character.isSpaceChar(c)) {
+                if (convertToCamel) {
+                    sb.append(Character.toChars(Character.toUpperCase(c)));
+                    convertToCamel = false;
+                } else {
+                    sb.append(Character.toChars(c));
+                }
+            } else if (Character.isSpaceChar(c)) {
                 sb.append("_");
             } else if (Character.isISOControl(c)) {
                 sb.append("CTRL");
             }
         }
-
 
 
         return sb.toString();
