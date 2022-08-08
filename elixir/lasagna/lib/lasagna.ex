0f05e2b763665f9c1f0f196ae04641a9c0a6ffba
Paul Moore <pdmoore@gmail.com>
2022-08-08 12:56:25 -0700

    elixir - lasagna - first test passes

--- elixir/lasagna/lib/lasagna.ex
+++ elixir/lasagna/lib/lasagna.ex
@@ -9,7 +9,8 @@ defmodule Lasagna do
 
   # Please define the 'preparation_time_in_minutes/1' function
   def preparation_time_in_minutes(number_of_layers) do
-    2 * number_of_layers
+    minutes_per_layer = 2;
+    minutes_per_layer * number_of_layers
   end
 
   # Please define the 'total_time_in_minutes/2' function
