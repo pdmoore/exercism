783984baa09909fd0816a14de342cd476b688f9a
Paul Moore <pdmoore@gmail.com>
2022-08-13 17:55:20 -0700

    elixir - darts

--- elixir/darts/lib/darts.ex
+++ elixir/darts/lib/darts.ex
@@ -1,17 +1,25 @@
 defmodule Darts do
   @type position :: {number, number}
+  @inner_circle_score  10
+  @middle_circle_score  5
+  @outer_circle_score   1
+  @outside_target_score 0
 
   @doc """
   Calculate the score of a single dart hitting a target
   """
   @spec score(position) :: integer
   def score({x, y}) do
-    distance = :math.sqrt(:math.pow(x, 2) + :math.pow(y, 2))
+    distance_from_center = :math.sqrt(square(x) + square(y))
     cond do
-      distance <= 1 -> 10
-      distance <= 5 -> 5
-      distance <= 10 -> 1
-      true -> 0
+      distance_from_center <= 1  -> @inner_circle_score
+      distance_from_center <= 5  -> @middle_circle_score
+      distance_from_center <= 10 -> @outer_circle_score
+      true -> @outside_target_score
     end
+  end
+
+  def square(scalar) do
+    :math.pow(scalar, 2)
   end
 end
