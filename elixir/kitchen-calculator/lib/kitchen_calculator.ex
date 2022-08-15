f204382382cebd81405ee36c74e5826d466684a9
Paul Moore <pdmoore@gmail.com>
2022-08-15 14:12:53 -0700

    elixir - kitchen calculator

--- elixir/kitchen-calculator/lib/kitchen_calculator.ex
+++ elixir/kitchen-calculator/lib/kitchen_calculator.ex
@@ -21,15 +21,11 @@ defmodule KitchenCalculator do
     end
   end
 
-  def from_milliliter(volume_pair, unit) do
-    cond do
-      unit == :milliliter -> volume_pair
-      unit == :cup -> {unit, elem(volume_pair, 1) / @ml_per_cup}
-      unit == :fluid_ounce -> {unit, elem(volume_pair, 1) / @ml_per_fluid_ounce}
-      unit == :tablespoon -> {unit, elem(volume_pair, 1) / @ml_per_tablespoon}
-      unit == :teaspoon -> {unit, elem(volume_pair, 1) / @ml_per_teaspoon}
-    end
-  end
+  def from_milliliter(volume_pair, unit) when unit == :milliliter, do: volume_pair
+  def from_milliliter(volume_pair, unit) when unit == :cup, do: {unit, elem(volume_pair, 1) / @ml_per_cup}
+  def from_milliliter(volume_pair, unit) when unit == :fluid_ounce, do: {unit, elem(volume_pair, 1) / @ml_per_fluid_ounce}
+  def from_milliliter(volume_pair, unit) when unit == :tablespoon, do: {unit, elem(volume_pair, 1) / @ml_per_tablespoon}
+  def from_milliliter(volume_pair, unit) when unit == :teaspoon, do: {unit, elem(volume_pair, 1) / @ml_per_teaspoon}
 
   def convert(volume_pair, unit) do
     to_milliliter(volume_pair) |> from_milliliter(unit)
