f604c3a2e3425b7053cecdd039f2776047debf23
Paul Moore <pdmoore@gmail.com>
2022-08-15 13:51:52 -0700

    elixir - kitchen calculator

--- elixir/kitchen-calculator/lib/kitchen_calculator.ex
+++ elixir/kitchen-calculator/lib/kitchen_calculator.ex
@@ -1,8 +1,8 @@
 defmodule KitchenCalculator do
-  @ml_per_cup 240
-  @ml_per_fluid_ounce 30
   @ml_per_teaspoon 5
   @ml_per_tablespoon 15
+  @ml_per_fluid_ounce 30
+  @ml_per_cup 240
 
   def get_volume(volume_pair) do
     elem(volume_pair, 1)
@@ -12,7 +12,6 @@ defmodule KitchenCalculator do
     # Please implement the to_milliliter/1 functions
     unit = elem(volume_pair, 0)
 
-    # TODO - next step is to create a new tuple with :milliliter on the left and the conversion on the right
     cond do
       unit == :milliliter -> {:milliliter, elem(volume_pair, 1)}
       unit == :cup -> {:milliliter, @ml_per_cup * elem(volume_pair, 1)}
@@ -21,7 +20,6 @@ defmodule KitchenCalculator do
       unit == :tablespoon -> {:milliliter, @ml_per_tablespoon * elem(volume_pair, 1)}
       true -> -99
     end
-
   end
 
   def from_milliliter(volume_pair, unit) do
