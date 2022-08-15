2f6ff02f3fe63c91eb0ea467364890247676f5fc
Paul Moore <pdmoore@gmail.com>
2022-08-14 21:18:59 -0700

    elixir - guessing game

--- elixir/guessing-game/lib/guessing_game.ex
+++ elixir/guessing-game/lib/guessing_game.ex
@@ -3,11 +3,11 @@ defmodule GuessingGame do
 
   def compare(secret_number, guess) when Kernel.abs(guess - secret_number) == 1, do: "So close"
 
-  def compare(secret_number, secret) when secret == :no_guess, do: "Make a guess"
+  def compare(_secret_number, guess) when guess == :no_guess, do: "Make a guess"
 
   def compare(secret_number, guess) when guess > secret_number, do: "Too high"
 
   def compare(secret_number, guess) when guess < secret_number, do: "Too low"
 
-  def compare(secret_number), do: "Make a guess"
+  def compare(_secret_number), do: "Make a guess"
 end
