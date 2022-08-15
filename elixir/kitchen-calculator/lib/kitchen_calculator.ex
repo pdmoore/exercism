87b47229ec2236c002106edf3275bd3004038008
Paul Moore <pdmoore@gmail.com>
2022-08-15 14:06:07 -0700

    elixir - kitchen calculator

--- elixir/kitchen-calculator/lib/kitchen_calculator.ex
+++ elixir/kitchen-calculator/lib/kitchen_calculator.ex
@@ -9,30 +9,29 @@ defmodule KitchenCalculator do
   end
 
   def to_milliliter(volume_pair) do
-    # Please implement the to_milliliter/1 functions
     unit = elem(volume_pair, 0)
 
     cond do
-      unit == :milliliter -> {:milliliter, elem(volume_pair, 1)}
+      unit == :milliliter -> volume_pair
       unit == :cup -> {:milliliter, @ml_per_cup * elem(volume_pair, 1)}
       unit == :fluid_ounce -> {:milliliter, @ml_per_fluid_ounce * elem(volume_pair, 1)}
-      unit == :teaspoon -> {:milliliter, @ml_per_teaspoon * elem(volume_pair, 1)}
       unit == :tablespoon -> {:milliliter, @ml_per_tablespoon * elem(volume_pair, 1)}
+      unit == :teaspoon -> {:milliliter, @ml_per_teaspoon * elem(volume_pair, 1)}
       true -> -99
     end
   end
 
   def from_milliliter(volume_pair, unit) do
     cond do
-      unit == :milliliter -> {unit, elem(volume_pair, 1)}
+      unit == :milliliter -> volume_pair
       unit == :cup -> {unit, elem(volume_pair, 1) / @ml_per_cup}
-      unit == :teaspoon -> {unit, elem(volume_pair, 1) / @ml_per_teaspoon}
-      unit == :tablespoon -> {unit, elem(volume_pair, 1) / @ml_per_tablespoon}
       unit == :fluid_ounce -> {unit, elem(volume_pair, 1) / @ml_per_fluid_ounce}
+      unit == :tablespoon -> {unit, elem(volume_pair, 1) / @ml_per_tablespoon}
+      unit == :teaspoon -> {unit, elem(volume_pair, 1) / @ml_per_teaspoon}
     end
   end
 
   def convert(volume_pair, unit) do
-    from_milliliter(to_milliliter(volume_pair), unit)
+    to_milliliter(volume_pair) |> from_milliliter(unit)
   end
 end
