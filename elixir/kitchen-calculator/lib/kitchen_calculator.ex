5c333c073b129bd99585a45a26c873bdb9270dde
Paul Moore <pdmoore@gmail.com>
2022-08-15 21:22:36 -0700

    elixir - kitchen calculator

--- elixir/kitchen-calculator/lib/kitchen_calculator.ex
+++ elixir/kitchen-calculator/lib/kitchen_calculator.ex
@@ -15,11 +15,7 @@ defmodule KitchenCalculator do
     {:milliliter, @conversions[elem(volume_pair, 0)] * get_volume(volume_pair)}
   end
 
-  def from_milliliter(volume_pair, unit) when unit == :milliliter, do: volume_pair
-  def from_milliliter(volume_pair, unit) when unit == :cup, do: {unit, get_volume(volume_pair) / @conversions[unit]}
-  def from_milliliter(volume_pair, unit) when unit == :fluid_ounce, do: {unit, get_volume(volume_pair) / @conversions[unit]}
-  def from_milliliter(volume_pair, unit) when unit == :tablespoon, do: {unit, get_volume(volume_pair) / @conversions[unit]}
-  def from_milliliter(volume_pair, unit) when unit == :teaspoon, do: {unit, get_volume(volume_pair) / @conversions[unit]}
+  def from_milliliter(volume_pair, unit), do: {unit, get_volume(volume_pair) / @conversions[unit]}
 
   def convert(volume_pair, unit) do
     volume_pair |> to_milliliter() |> from_milliliter(unit)
