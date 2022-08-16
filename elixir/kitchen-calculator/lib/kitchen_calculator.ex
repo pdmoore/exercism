a6c71cf785b189ba81160395c3da3e11a64e7975
Paul Moore <pdmoore@gmail.com>
2022-08-15 17:16:25 -0700

    elixir - kitchen calculator - completely missed the obvious get_volume reuse

--- elixir/kitchen-calculator/lib/kitchen_calculator.ex
+++ elixir/kitchen-calculator/lib/kitchen_calculator.ex
@@ -13,18 +13,18 @@ defmodule KitchenCalculator do
 
     cond do
       unit == :milliliter -> volume_pair
-      unit == :cup -> {:milliliter, @ml_per_cup * elem(volume_pair, 1)}
-      unit == :fluid_ounce -> {:milliliter, @ml_per_fluid_ounce * elem(volume_pair, 1)}
-      unit == :tablespoon -> {:milliliter, @ml_per_tablespoon * elem(volume_pair, 1)}
-      unit == :teaspoon -> {:milliliter, @ml_per_teaspoon * elem(volume_pair, 1)}
+      unit == :cup -> {:milliliter, @ml_per_cup * get_volume(volume_pair)}
+      unit == :fluid_ounce -> {:milliliter, @ml_per_fluid_ounce * get_volume(volume_pair)}
+      unit == :tablespoon -> {:milliliter, @ml_per_tablespoon * get_volume(volume_pair)}
+      unit == :teaspoon -> {:milliliter, @ml_per_teaspoon * get_volume(volume_pair)}
     end
   end
 
   def from_milliliter(volume_pair, unit) when unit == :milliliter, do: volume_pair
-  def from_milliliter(volume_pair, unit) when unit == :cup, do: {unit, elem(volume_pair, 1) / @ml_per_cup}
-  def from_milliliter(volume_pair, unit) when unit == :fluid_ounce, do: {unit, elem(volume_pair, 1) / @ml_per_fluid_ounce}
-  def from_milliliter(volume_pair, unit) when unit == :tablespoon, do: {unit, elem(volume_pair, 1) / @ml_per_tablespoon}
-  def from_milliliter(volume_pair, unit) when unit == :teaspoon, do: {unit, elem(volume_pair, 1) / @ml_per_teaspoon}
+  def from_milliliter(volume_pair, unit) when unit == :cup, do: {unit, get_volume(volume_pair) / @ml_per_cup}
+  def from_milliliter(volume_pair, unit) when unit == :fluid_ounce, do: {unit, get_volume(volume_pair) / @ml_per_fluid_ounce}
+  def from_milliliter(volume_pair, unit) when unit == :tablespoon, do: {unit, get_volume(volume_pair) / @ml_per_tablespoon}
+  def from_milliliter(volume_pair, unit) when unit == :teaspoon, do: {unit, get_volume(volume_pair) / @ml_per_teaspoon}
 
   def convert(volume_pair, unit) do
     volume_pair |> to_milliliter() |> from_milliliter(unit)
