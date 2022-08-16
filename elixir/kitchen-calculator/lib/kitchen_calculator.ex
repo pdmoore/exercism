be50495e6e5e20c1e47a3ba090845838cd3fd84e
Paul Moore <pdmoore@gmail.com>
2022-08-15 21:21:28 -0700

    elixir - kitchen calculator - use map to generalize conversion to mL

--- elixir/kitchen-calculator/lib/kitchen_calculator.ex
+++ elixir/kitchen-calculator/lib/kitchen_calculator.ex
@@ -1,9 +1,4 @@
 defmodule KitchenCalculator do
-  @ml_per_teaspoon 5
-  @ml_per_tablespoon 15
-  @ml_per_fluid_ounce 30
-  @ml_per_cup 240
-
   @conversions %{
     :milliliter  =>   1,
     :teaspoon    =>   5,
@@ -21,10 +16,10 @@ defmodule KitchenCalculator do
   end
 
   def from_milliliter(volume_pair, unit) when unit == :milliliter, do: volume_pair
-  def from_milliliter(volume_pair, unit) when unit == :cup, do: {unit, get_volume(volume_pair) / @ml_per_cup}
-  def from_milliliter(volume_pair, unit) when unit == :fluid_ounce, do: {unit, get_volume(volume_pair) / @ml_per_fluid_ounce}
-  def from_milliliter(volume_pair, unit) when unit == :tablespoon, do: {unit, get_volume(volume_pair) / @ml_per_tablespoon}
-  def from_milliliter(volume_pair, unit) when unit == :teaspoon, do: {unit, get_volume(volume_pair) / @ml_per_teaspoon}
+  def from_milliliter(volume_pair, unit) when unit == :cup, do: {unit, get_volume(volume_pair) / @conversions[unit]}
+  def from_milliliter(volume_pair, unit) when unit == :fluid_ounce, do: {unit, get_volume(volume_pair) / @conversions[unit]}
+  def from_milliliter(volume_pair, unit) when unit == :tablespoon, do: {unit, get_volume(volume_pair) / @conversions[unit]}
+  def from_milliliter(volume_pair, unit) when unit == :teaspoon, do: {unit, get_volume(volume_pair) / @conversions[unit]}
 
   def convert(volume_pair, unit) do
     volume_pair |> to_milliliter() |> from_milliliter(unit)
