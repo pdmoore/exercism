c2af17ddf5ec3f9e4554cfef083ee45f36994dbc
Paul Moore <pdmoore@gmail.com>
2022-08-17 17:21:55 -0700

    elixir - rational numbers

--- elixir/rational-numbers/lib/rational_numbers.ex
+++ elixir/rational-numbers/lib/rational_numbers.ex
@@ -8,8 +8,8 @@ defmodule RationalNumbers do
   def add(a, b) do
     #r₁ + r₂ = a₁/b₁ + a₂/b₂ = (a₁ * b₂ + a₂ * b₁) / (b₁ * b₂)
     a1 = elem(a, 0)
-    a2 = elem(a, 1)
-    b1 = elem(b, 0)
+    a2 = elem(b, 0)
+    b1 = elem(a, 1)
     b2 = elem(b, 1)
 
     numerator   = (a1 * b2) + (a2 * b1)
