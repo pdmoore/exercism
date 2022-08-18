40206e318f9877a8d49270c94a00b5740b9fc796
Paul Moore <pdmoore@gmail.com>
2022-08-18 13:50:06 -0700

    elixir - high school swetehearts

--- elixir/high-school-sweetheart/lib/high_school_sweetheart.ex
+++ elixir/high-school-sweetheart/lib/high_school_sweetheart.ex
@@ -11,7 +11,7 @@ defmodule HighSchoolSweetheart do
     tokens = full_name |> String.split
     first_initial = Enum.at(tokens, 0) |> initial
     last_initial = Enum.at(tokens, 1) |> initial
-    first_initial <> " " <> last_initial
+    "#{first_initial} #{last_initial}"
   end
 
   def pair(full_name1, full_name2) do
