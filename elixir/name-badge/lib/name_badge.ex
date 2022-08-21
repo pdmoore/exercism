40045cf74842cafa4b75239546e28ae5a1b1aae4
Paul Moore <pdmoore@gmail.com>
2022-08-20 17:47:39 -0700

    elixir name badge

--- elixir/name-badge/lib/name_badge.ex
+++ elixir/name-badge/lib/name_badge.ex
@@ -1,10 +1,14 @@
 defmodule NameBadge do
   def print(id, name, department) do
+    if department == nil do
+      "[#{id}] - " <> name <> " - OWNER"
+    else
     upcased_department = department |> String.upcase
     if id == nil do
       name <> " - " <> upcased_department
     else
       "[#{id}] - " <> name <> " - " <> upcased_department
+    end
     end
   end
 end
