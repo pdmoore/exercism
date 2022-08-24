7f597b6fab736bb7d25c27b282c44795ca11f125
Paul Moore <pdmoore@gmail.com>
2022-08-23 20:32:29 -0700

    elixir - take a number

--- elixir/take-a-number/lib/take_a_number.ex
+++ elixir/take-a-number/lib/take_a_number.ex
@@ -1,16 +1,17 @@
 defmodule TakeANumber do
   def start() do
-    spawn(fn -> machine() end)
+    spawn(fn -> machine(0) end)
   end
 
-  def machine() do
-    0
+  def machine(value) do
+    count = value
 
     receive do
-      {:report_state, sender_pid} -> send(sender_pid, 0)
+      {:report_state, sender_pid}  -> send(sender_pid, count)
+      {:take_a_number, sender_pid} -> send(sender_pid, count + 1)
     end
 
-    machine()
+    machine(count)
   end
 
 
