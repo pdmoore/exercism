04a2b4a95e8bcd40b0c4084f2a74425961a1e2a5
Paul Moore <pdmoore@gmail.com>
2022-08-25 09:23:11 -0700

    elixir - wine cellar

--- elixir/wine-cellar/lib/wine_cellar.ex
+++ elixir/wine-cellar/lib/wine_cellar.ex
@@ -9,8 +9,8 @@ defmodule WineCellar do
 
   def filter(cellar, color, opts \\ []) do
     Keyword.get_values(cellar, color)
-      |> filter_by_year(Keyword.get(opts, :year))
-      |> filter_by_country(Keyword.get(opts, :country))
+      |> filter_by_year(opts[:year])
+      |> filter_by_country(opts[:country])
   end
 
   # The functions below do not need to be modified.
