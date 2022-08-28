1281e4f82c0a96de4d39c2dcacbffbd1efe99ce8
Paul Moore <pdmoore@gmail.com>
2022-08-28 14:25:09 -0700

    elixir - library fees

--- elixir/library-fees/lib/library_fees.ex
+++ elixir/library-fees/lib/library_fees.ex
@@ -2,32 +2,35 @@ defmodule LibraryFees do
   @monday 1
   @noon ~T[12:00:00.000]
 
+  @spec datetime_from_string(string :: String.t()) :: NaiveDateTime
   def datetime_from_string(string), do: NaiveDateTime.from_iso8601!(string)
 
+  @spec before_noon?(datetime :: NaiveDateTime) :: boolean
   def before_noon?(datetime) do
     Time.compare(NaiveDateTime.to_time(datetime), @noon) == :lt
   end
 
+  @spec return_date(checkout_datetime :: NaiveDateTime) :: integer
   def return_date(checkout_datetime) do
     checkout_date = NaiveDateTime.to_date(checkout_datetime)
-    if before_noon?(checkout_datetime), do: Date.add(checkout_date, 28),
-    else: Date.add(checkout_date, 29)
+    if before_noon?(checkout_datetime), do:   Date.add(checkout_date, 28),
+                                        else: Date.add(checkout_date, 29)
   end
 
-  @spec days_late(planned_return_date :: Date, actual_return_datetime :: NaiveDateTime) :: integer()
+  @spec days_late(planned_return_date :: Date, actual_return_datetime :: NaiveDateTime) :: integer
   def days_late(planned_return_date, actual_return_datetime) do
     actual_return_date = NaiveDateTime.to_date(actual_return_datetime)
     returned_before_due_date = Date.compare(planned_return_date, actual_return_date) == :gt
     if returned_before_due_date, do: 0, else: Date.diff(actual_return_date, planned_return_date)
   end
 
-  @spec monday?(datetime :: NaiveDateTime) :: :atom
+  @spec monday?(datetime :: NaiveDateTime) :: boolean
   def monday?(datetime) do
     actual_return_date = NaiveDateTime.to_date(datetime)
     Date.day_of_week(actual_return_date) == @monday
   end
 
-  @spec calculate_late_fee(checkout :: String.t(), return :: String.t(), return :: integer()) :: integer()
+  @spec calculate_late_fee(checkout :: String.t(), return :: String.t(), return :: integer) :: integer
   def calculate_late_fee(checkout, return, rate) do
     expected_return_date = return_date(datetime_from_string(checkout))
     actual_return_date = datetime_from_string(return)
