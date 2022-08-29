a48ac6c8755e84aa55b35ffe42e8c69668129d1d
Paul Moore <pdmoore@gmail.com>
2022-08-29 10:20:46 -0700

    elixir basketball website

--- elixir/basketball-website/lib/basketball_website.ex
+++ elixir/basketball-website/lib/basketball_website.ex
@@ -1,13 +1,9 @@
 defmodule BasketballWebsite do
+  def extract_from_path(data, []), do: data
+  def extract_from_path(data, [h | t]), do: extract_from_path(data[h], t)
   def extract_from_path(data, path) do
-
-    paths = String.split(path, ".", parts: 2)
-    if length(paths) == 1 do
-      data[path]
-    else
-      next = data[List.first(paths)]
-      extract_from_path(next, List.last(paths))
-    end
+    paths = String.split(path, ".")
+    extract_from_path(data, paths)
   end
 
   def get_in_path(data, path) do
