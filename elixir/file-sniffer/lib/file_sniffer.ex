c1865d9c05eec17db5f387cbbc416f7aaf8010da
Paul Moore <pdmoore@gmail.com>
2022-08-30 15:34:55 -0700

    elixir - file sniffer

--- elixir/file-sniffer/lib/file_sniffer.ex
+++ elixir/file-sniffer/lib/file_sniffer.ex
@@ -9,9 +9,14 @@ defmodule FileSniffer do
 
   def type_from_binary(<<0x42, 0x4D, _rest::binary>>), do: "image/bmp"
   def type_from_binary(<<0x47, 0x49, 0x46, _rest::binary>>), do: "image/gif"
+  def type_from_binary(<<0xFF, 0xD8, 0xFF, _rest::binary>>), do: "image/jpg"
+  def type_from_binary(<<0x89, 0x50, 0x4E, 0x47, 0x0D, 0x0A, 0x1A, 0x0A, _rest::binary>>), do: "image/png"
   def type_from_binary(file_binary) do
     "not done yet"
   end
+
+
+  #
 
   def verify(file_binary, extension) do
     # Please implement the verify/2 function
