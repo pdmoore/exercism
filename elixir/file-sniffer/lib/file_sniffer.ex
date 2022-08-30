8fa0c70765c128ae04f787190ed78a747364a9b3
Paul Moore <pdmoore@gmail.com>
2022-08-30 15:51:24 -0700

    elixir - file sniffer

--- elixir/file-sniffer/lib/file_sniffer.ex
+++ elixir/file-sniffer/lib/file_sniffer.ex
@@ -4,7 +4,7 @@ defmodule FileSniffer do
   def type_from_extension(extension) when extension == "jpg", do: "image/jpg"
   def type_from_extension(extension) when extension == "png", do: "image/png"
   def type_from_extension(extension) when extension == "exe", do: "application/octet-stream"
-  def type_from_extension(extension) do
+  def type_from_extension(_) do
   end
 
   def type_from_binary(<<0x42, 0x4D, _::binary>>), do: "image/bmp"
@@ -12,7 +12,7 @@ defmodule FileSniffer do
   def type_from_binary(<<0xFF, 0xD8, 0xFF, _::binary>>), do: "image/jpg"
   def type_from_binary(<<0x89, 0x50, 0x4E, 0x47, 0x0D, 0x0A, 0x1A, 0x0A, _::binary>>), do: "image/png"
   def type_from_binary(<<0x7F, 0x45, 0x4C, 0x46, _::binary>>), do: "application/octet-stream"
-  def type_from_binary(file_binary) do
+  def type_from_binary(_) do
   end
 
   def verify(file_binary, extension) do
