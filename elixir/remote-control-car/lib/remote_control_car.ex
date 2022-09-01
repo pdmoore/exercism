8ef7da89b200ab375f80cda02415b94bf3f6e49a
Paul Moore <pdmoore@gmail.com>
2022-09-01 14:59:45 -0700

    elixir - remote control car

--- elixir/remote-control-car/lib/remote_control_car.ex
+++ elixir/remote-control-car/lib/remote_control_car.ex
@@ -1,21 +1,18 @@
 defmodule RemoteControlCar do
-#  @enforce_keys [:nickname]
-  defstruct [:nickname,
-             battery_percentage: 100,
-             distance_driven_in_meters: 0]
+  @enforce_keys [:nickname]
+  defstruct [:nickname, battery_percentage: 100, distance_driven_in_meters: 0]
 
   def new(), do: new("none")
   def new(nickname) do
-    rc = %RemoteControlCar{}
-    %{rc | nickname: nickname}
+    rc = %RemoteControlCar{nickname: nickname}
   end
 
-  def display_distance(remote_car) do
+  def display_distance(remote_car = %RemoteControlCar{}) do
     distance = remote_car.distance_driven_in_meters
     "#{distance} meters"
   end
 
-  def display_battery(remote_car) do
+  def display_battery(remote_car = %RemoteControlCar{}) do
     percentage = remote_car.battery_percentage
     cond do
       percentage == 0 -> "Battery empty"
@@ -23,8 +20,8 @@ defmodule RemoteControlCar do
     end
   end
 
-  def drive(remote_car) when remote_car.battery_percentage == 0, do: remote_car
-  def drive(remote_car) do
+  def drive(remote_car = %RemoteControlCar{}) when remote_car.battery_percentage == 0, do: remote_car
+  def drive(remote_car = %RemoteControlCar{}) do
     percentage = remote_car.battery_percentage
     distance = remote_car.distance_driven_in_meters
     %RemoteControlCar{battery_percentage: percentage - 1,
