de29ad866e458c81a8e1d03662daad21a8e0b1ca
Paul Moore <pdmoore@gmail.com>
2022-09-02 14:42:45 -0700

    elixir boutique suggestions

--- elixir/boutique-suggestions/lib/boutique_suggestions.ex
+++ elixir/boutique-suggestions/lib/boutique_suggestions.ex
@@ -1,5 +1,8 @@
 defmodule BoutiqueSuggestions do
   def get_combinations(tops, bottoms, options \\ []) do
-    []
+    for top <- tops,
+        bottom <- bottoms do
+      {top, bottom}
+    end
   end
 end
