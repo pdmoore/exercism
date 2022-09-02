6bee49c446d179260e020c7b96e92db170cd630b
Paul Moore <pdmoore@gmail.com>
2022-09-02 14:47:18 -0700

    elixir boutique suggestions

--- elixir/boutique-suggestions/lib/boutique_suggestions.ex
+++ elixir/boutique-suggestions/lib/boutique_suggestions.ex
@@ -1,9 +1,12 @@
 defmodule BoutiqueSuggestions do
   def get_combinations(tops, bottoms, options \\ []) do
+    maximum_price = options[:maximum_price]
     for top <- tops,
         bottom <- bottoms,
-          top.base_color != bottom.base_color  do
-      {top, bottom}
+          top.base_color != bottom.base_color and
+          top.price + bottom.price <= maximum_price do
+      combinations = {top, bottom}
     end
   end
+
 end
