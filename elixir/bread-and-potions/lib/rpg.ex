1aed0393ee52380c155ce085c1ff0db7c270c39d
Paul Moore <pdmoore@gmail.com>
2022-09-04 13:37:53 -0700

    elixir - bread and potions

---
+++ elixir/bread-and-potions/lib/rpg.ex
@@ -0,0 +1,28 @@
+defmodule RPG do
+  defprotocol Edible do
+    #TODO? returns a by-product and a character.
+    def eat(item, character)
+  end
+
+  defmodule Character do
+    defstruct health: 100, mana: 0
+  end
+
+  defmodule LoafOfBread do
+    defstruct []
+  end
+
+  defmodule ManaPotion do
+    defstruct strength: 10
+  end
+
+  defmodule Poison do
+    defstruct []
+  end
+
+  defmodule EmptyBottle do
+    defstruct []
+  end
+
+  # Add code to define the protocol and its implementations below here...
+end
