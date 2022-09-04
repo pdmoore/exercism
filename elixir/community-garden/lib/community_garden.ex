aa9a1ca53a0fe3b0986e667b19063bddc7c7fcee
Paul Moore <pdmoore@gmail.com>
2022-09-03 17:22:14 -0700

    elixir - community garden

--- elixir/community-garden/lib/community_garden.ex
+++ elixir/community-garden/lib/community_garden.ex
@@ -15,7 +15,6 @@ defmodule CommunityGarden do
 
   def register(pid, register_to) do
     #TODO random is wrong, but passes tests
-    # really want to track an incrementable last_id, or read the last plot_id and add one to it
     plot = %Plot{plot_id: :rand.uniform(999999), registered_to: register_to}
     Agent.update(pid, fn plots -> [plot | plots] end)
     plot
@@ -26,8 +25,8 @@ defmodule CommunityGarden do
   end
 
   def get_registration(pid, plot_id) do
-    Enum.find(list_registrations(pid),
-      {:not_found, "plot is unregistered"},
-      fn p -> p.plot_id == plot_id end)
+    list_registrations(pid)
+      |> Enum.find({:not_found, "plot is unregistered"},
+           fn p -> p.plot_id == plot_id end)
   end
 end
