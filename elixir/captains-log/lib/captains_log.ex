ed7388699317c6f065d84cf2f9806fee8434a0ab
Paul Moore <pdmoore@gmail.com>
2022-09-04 19:28:26 -0700

    elixir - captain's log

---
+++ elixir/captains-log/lib/captains_log.ex
@@ -0,0 +1,19 @@
+defmodule CaptainsLog do
+  @planetary_classes ["D", "H", "J", "K", "L", "M", "N", "R", "T", "Y"]
+
+  def random_planet_class() do
+    Enum.random(@planetary_classes)
+  end
+
+  def random_ship_registry_number() do
+    "NCC-"
+  end
+
+  def random_stardate() do
+    # Please implement the random_stardate/0 function
+  end
+
+  def format_stardate(stardate) do
+    # Please implement the format_stardate/1 function
+  end
+end
