9d75c484885c2195b8b44cd82f754eae0ffda47d
Paul Moore <pdmoore@gmail.com>
2022-09-05 12:42:48 -0700

    elixir - rpn calculator

--- elixir/rpn-calculator/lib/rpn_calculator.ex
+++ elixir/rpn-calculator/lib/rpn_calculator.ex
@@ -1,14 +1,14 @@
 defmodule RPNCalculator do
   def calculate!(stack, operation) do
-    try do
-      operation.(stack)
-    rescue
-      _ -> :ok
-    end
+    operation.(stack)
   end
 
   def calculate(stack, operation) do
-    # Please implement the calculate/2 function
+    try do
+      {:ok, operation.(stack)}
+    rescue
+      _ -> :error
+    end
   end
 
   def calculate_verbose(stack, operation) do
