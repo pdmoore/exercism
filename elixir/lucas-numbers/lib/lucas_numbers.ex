f802fd9179645406acd42f9b2c9d61e65e740a50
Paul Moore <pdmoore@gmail.com>
2022-09-08 16:40:00 -0700

    elixir - lucas numbers

--- elixir/lucas-numbers/lib/lucas_numbers.ex
+++ elixir/lucas-numbers/lib/lucas_numbers.ex
@@ -7,11 +7,16 @@ defmodule LucasNumbers do
   """
   def generate(1), do: [2]
   def generate(2), do: [2, 1]
+  def generate(count) when is_binary(count), do: generate(0)
   def generate(count) when count <= 0, do: raise ArgumentError, message: "count must be specified as an integer >= 1"
   def generate(count) do
-    # count goes at the end
-    # subtract one from count and call generate on that
-    # combine the count -1 on the left to the number itself on the right
+
+    # starting with 2, while size of list is less than count
+    # [list | second, last]
+    # list, second+last
+    # Visualize Stream usage?
+    # map the algo out on paper and see what parts are at play
+
     Enum.concat(generate(count - 1), [count])
 
 
