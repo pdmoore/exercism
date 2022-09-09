9b9885a31a60cdac01b88c862830a0d988b8bec0
Paul Moore <pdmoore@gmail.com>
2022-09-08 20:46:31 -0700

    elixir - lucas numbers

--- elixir/lucas-numbers/lib/lucas_numbers.ex
+++ elixir/lucas-numbers/lib/lucas_numbers.ex
@@ -10,18 +10,12 @@ defmodule LucasNumbers do
   def generate(count) when is_binary(count), do: generate(0)
   def generate(count) when count <= 0, do: raise ArgumentError, message: "count must be specified as an integer >= 1"
   def generate(count) do
-
-    # starting with 2, while size of list is less than count
-    # [list | second, last]
-    # list, second+last
-    # Visualize Stream usage?
-    # map the algo out on paper and see what parts are at play
-
-    Enum.concat(generate(count - 1), [count])
-
-
-
-
-
+    generate(2)
+    |> Enum.chunk_every(2)
+    |> Enum.map(fn [a, b] -> {a, b} end)
+    |> List.first()
+    |> Stream.iterate(fn {y, z} -> {z, y + z} end)
+    |> Stream.map(fn {n,_} -> n end)
+    |> Enum.take(count)
   end
 end
