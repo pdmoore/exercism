4682f12648fc575b67720f5623c6c34e1753c035
Paul Moore <pdmoore@gmail.com>
2022-09-30 13:55:37 -0700

    elixir - allergies

--- elixir/allergies/lib/allergies.ex
+++ elixir/allergies/lib/allergies.ex
@@ -5,26 +5,23 @@ defmodule Allergies do
   @spec list(non_neg_integer) :: [String.t()]
   def list(flags) do
     []
-      |> check_for_allergy(Bitwise.band(flags, 0b00001))
-      |> check_for_allergy(Bitwise.band(flags, 0b00010))
-      |> check_for_allergy(Bitwise.band(flags, 0b00100))
-      |> check_for_allergy(Bitwise.band(flags, 0b01000))
-      |> check_for_allergy(Bitwise.band(flags, 0b10000))
-      |> check_for_allergy(Bitwise.band(flags, 0b100000))
-      |> check_for_allergy(Bitwise.band(flags, 0b1000000))
+      |> check_for_allergy(Bitwise.band(flags, 0b00000001))
+      |> check_for_allergy(Bitwise.band(flags, 0b00000010))
+      |> check_for_allergy(Bitwise.band(flags, 0b00000100))
+      |> check_for_allergy(Bitwise.band(flags, 0b00001000))
+      |> check_for_allergy(Bitwise.band(flags, 0b00010000))
+      |> check_for_allergy(Bitwise.band(flags, 0b00100000))
+      |> check_for_allergy(Bitwise.band(flags, 0b01000000))
       |> check_for_allergy(Bitwise.band(flags, 0b10000000))
-#    chocolate (32)
-#    pollen (64)
-#    cats (128)
   end
 
-  defp check_for_allergy(allergies, 0b00001), do: allergies ++ ["eggs"]
-  defp check_for_allergy(allergies, 0b00010), do: allergies ++ ["peanuts"]
-  defp check_for_allergy(allergies, 0b00100), do: allergies ++ ["shellfish"]
-  defp check_for_allergy(allergies, 0b01000), do: allergies ++ ["strawberries"]
-  defp check_for_allergy(allergies, 0b10000), do: allergies ++ ["tomatoes"]
-  defp check_for_allergy(allergies, 0b100000), do: allergies ++ ["chocolate"]
-  defp check_for_allergy(allergies, 0b1000000), do: allergies ++ ["pollen"]
+  defp check_for_allergy(allergies, 0b00000001), do: allergies ++ ["eggs"]
+  defp check_for_allergy(allergies, 0b00000010), do: allergies ++ ["peanuts"]
+  defp check_for_allergy(allergies, 0b00000100), do: allergies ++ ["shellfish"]
+  defp check_for_allergy(allergies, 0b00001000), do: allergies ++ ["strawberries"]
+  defp check_for_allergy(allergies, 0b00010000), do: allergies ++ ["tomatoes"]
+  defp check_for_allergy(allergies, 0b00100000), do: allergies ++ ["chocolate"]
+  defp check_for_allergy(allergies, 0b01000000), do: allergies ++ ["pollen"]
   defp check_for_allergy(allergies, 0b10000000), do: allergies ++ ["cats"]
   defp check_for_allergy(allergies, _), do: allergies
 
