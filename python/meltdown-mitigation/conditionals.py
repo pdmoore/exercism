6cc06730013c9b77386bee6b9e45a42e9455b25d
Paul Moore <pdmoore@gmail.com>
2023-01-11 12:59:13 -0800

    python - meltdown mitigation

--- python/meltdown-mitigation/conditionals.py
+++ python/meltdown-mitigation/conditionals.py
@@ -45,12 +45,14 @@ def reactor_efficiency(voltage, current, theoretical_max_power):
 
     if efficiency >= 80:
         return 'green'
-    elif efficiency >= 60:
+
+    if efficiency >= 60:
         return 'orange'
-    elif efficiency >= 30:
+
+    if efficiency >= 30:
         return 'red'
-    else:
-        return 'black'
+
+    return 'black'
 
 
 def fail_safe(temperature, neutrons_produced_per_second, threshold):
