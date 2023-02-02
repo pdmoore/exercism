3ca141ab64bee9096e4f2b86c3a237f6e41c2406
Paul Moore <pdmoore@gmail.com>
2023-02-02 13:46:08 -0800

    elixir - hamming

--- elixir/hamming/lib/hamming.ex
+++ elixir/hamming/lib/hamming.ex
@@ -9,6 +9,12 @@ defmodule Hamming do
   """
   @spec hamming_distance([char], [char]) :: {:ok, non_neg_integer} | {:error, String.t()}
   def hamming_distance(strand1, strand2) do
+
+    # need a counter, then recurse over each character in the strand
+    # if chars do not match, increment counter
+
+    #strands of different length, return {:error, "strands must be of equal length"}
+
     {:ok, 0}
   end
 end
