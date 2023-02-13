9ed1255f50eebb2197fd9802bea95fa7bcb2e6f1
Paul Moore <pdmoore@gmail.com>
2023-02-13 13:38:46 -0800

    elixir - hamming

--- elixir/hamming/lib/hamming.ex
+++ elixir/hamming/lib/hamming.ex
@@ -12,11 +12,7 @@ defmodule Hamming do
     hamming_distance(strand1, strand2, 0)
   end
 
-  # TODO - Suspect a lot of the 'cond' code will be covered by the other impls below
-  # start pulling code, runningt tests, to see if that's true
-
-  @spec hamming_distance([char], [char], integer) :: {:ok, non_neg_integer} | {:error, String.t()}
-  def hamming_distance([strand1_head | strand1_tail], [strand2_head | strand2_tail], difference_count) do
+  defp hamming_distance([strand1_head | strand1_tail], [strand2_head | strand2_tail], difference_count) do
     cond do
       strand1_head != strand2_head ->
         hamming_distance(strand1_tail, strand2_tail, difference_count + 1)
@@ -25,7 +21,7 @@ defmodule Hamming do
     end
   end
 
-  def hamming_distance(strand1_head, [strand2_head | _], count) do
+  defp hamming_distance(strand1_head, [strand2_head | _], count) do
     if strand1_head == '' and strand2_head != '' do
       {:error, "strands must be of equal length"}
     else
@@ -33,7 +29,7 @@ defmodule Hamming do
     end
   end
 
-  def hamming_distance([strand1_head | _], strand2_head, count) do
+  defp hamming_distance([strand1_head | _], strand2_head, count) do
     if strand2_head == '' and strand1_head != '' do
       {:error, "strands must be of equal length"}
     else
@@ -41,7 +37,7 @@ defmodule Hamming do
     end
   end
 
-  def hamming_distance(strand1_head, strand2_head, count) do
+  defp hamming_distance(strand1_head, strand2_head, count) do
     if strand1_head != strand2_head do
       {:ok, count + 1}
     else
