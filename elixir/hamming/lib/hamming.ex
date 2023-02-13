8c76dfd64b1ceaac163d46d85fd934c39b7f61e2
Paul Moore <pdmoore@gmail.com>
2023-02-13 13:27:47 -0800

    elixir - hamming

--- elixir/hamming/lib/hamming.ex
+++ elixir/hamming/lib/hamming.ex
@@ -7,46 +7,31 @@ defmodule Hamming do
   iex> Hamming.hamming_distance('AAGTCATA', 'TAGCGATC')
   {:ok, 4}
   """
-#  @spec hamming_distance([], [char]) :: {:error, String.t()}
-#  def hamming_distance([], [strand2]) do
-#    {:error, "strands must be of equal length"}
-#  end
-
-#  @spec hamming_distance([char], []) :: {:error, String.t()}
-#  def hamming_distance([strand1], []) do
-#    {:error, "strands must be of equal length"}
-#  end
-
   @spec hamming_distance([char], [char]) :: {:ok, non_neg_integer} | {:error, String.t()}
   def hamming_distance(strand1, strand2) do
-
     hamming_distance(strand1, strand2, 0)
-    # need a counter, then recurse over each character in the strand
-    # if chars do not match, increment counter
-    # if lengths don't match return error
-
-    #strands of different length, return {:error, "strands must be of equal length"}
-    # handled by the functions watching for one side empty when other is not
   end
 
 
-  def hamming_distance([strand1_head | strand1_tail], [strand2_head | strand2_tail], count) do
+  # TODO - Suspect a lot of the 'cond' code will be covered by the other impls below
+  # start pulling code, runningt tests, to see if that's true
+
+  @spec hamming_distance([char], [char], integer) :: {:ok, non_neg_integer} | {:error, String.t()}
+  def hamming_distance([strand1_head | strand1_tail], [strand2_head | strand2_tail], difference_count) do
 
     cond do
       strand1_head == '' and strand2_head == '' ->
-        {:ok, count}
+        {:ok, difference_count}
       strand1_head == '' and strand2_head != '' ->
         {:error, "strands must be of equal length"}
       strand1_head != '' and strand2_head == '' ->
         {:error, "strands must be of equal length"}
       strand1_head != strand2_head ->
-        hamming_distance(strand1_tail, strand2_tail, count + 1)
+        hamming_distance(strand1_tail, strand2_tail, difference_count + 1)
       true ->
-        hamming_distance(strand1_tail, strand2_tail, count)
+        hamming_distance(strand1_tail, strand2_tail, difference_count)
     end
   end
-
-
 
   def hamming_distance(strand1_head, [strand2_head | strand2_tail], count) do
     if strand1_head == '' and strand2_head != '' do
