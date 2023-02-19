ca8cdb781d66f640e98cff8503858fbaa3b660f6
Paul Moore <pdmoore@gmail.com>
2023-02-18 16:29:35 -0800

    elixir - resisitor color

--- elixir/resistor-color/lib/resistor_color.ex
+++ elixir/resistor-color/lib/resistor_color.ex
@@ -3,19 +3,18 @@ defmodule ResistorColor do
   Return the value of a color band
   """
   @spec code(atom) :: integer()
-  def code(:orange), do: 3
-  def code(:yellow), do: 4
-  def code(:green),  do: 5
-  def code(:blue),   do: 6
-  def code(:violet), do: 7
-  def code(:grey),   do: 8
-  def code(:white),  do: 9
-
   def code(color) do
     color_value = %{
       black:  0,
       brown:  1,
-      red:    2
+      red:    2,
+      orange: 3,
+      yellow: 4,
+      green:  5,
+      blue:   6,
+      violet: 7,
+      grey:   8,
+      white:  9
     }
     color_value[color]
   end
