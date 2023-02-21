759f16a13d91cc4d5e72630c92a27a8ee658d789
Paul Moore <pdmoore@gmail.com>
2023-02-20 18:21:58 -0800

    elixir - resistor color duo

--- elixir/resistor-color-duo/lib/resistor_color_duo.ex
+++ elixir/resistor-color-duo/lib/resistor_color_duo.ex
@@ -1,4 +1,18 @@
 defmodule ResistorColorDuo do
+  @color_value %{
+    black:  0,
+    brown:  1,
+    red:    2,
+    orange: 3,
+    yellow: 4,
+    green:  5,
+    blue:   6,
+    violet: 7,
+    grey:   8,
+    white:  9
+  }
+
+
   @doc """
   Calculate a resistance value from two colors
   """
@@ -6,22 +20,22 @@ defmodule ResistorColorDuo do
   def value(colors) do
 # [:brown, :black]
 
-    color_value = %{
-      black:  0,
-      brown:  1,
-      red:    2,
-      orange: 3,
-      yellow: 4,
-      green:  5,
-      blue:   6,
-      violet: 7,
-      grey:   8,
-      white:  9
-    }
+#    color_value = %{
+#      black:  0,
+#      brown:  1,
+#      red:    2,
+#      orange: 3,
+#      yellow: 4,
+#      green:  5,
+#      blue:   6,
+#      violet: 7,
+#      grey:   8,
+#      white:  9
+#    }
     [band_1 | tail] = colors
     [band_2 | _] = tail
-    first_digit  = color_value[band_1]
-    second_digit = color_value[band_2]
+    first_digit  = @color_value[band_1]
+    second_digit = @color_value[band_2]
     two_digits_combined_as_string = Integer.to_string(first_digit) <> Integer.to_string(second_digit)
     String.to_integer(two_digits_combined_as_string)
   end
