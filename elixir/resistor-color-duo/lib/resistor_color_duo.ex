4639b5a3c8d5aa2922a0f88ac9021b395b89a5c8
Paul Moore <pdmoore@gmail.com>
2023-02-20 18:22:21 -0800

    elixir - resistor color duo

--- elixir/resistor-color-duo/lib/resistor_color_duo.ex
+++ elixir/resistor-color-duo/lib/resistor_color_duo.ex
@@ -12,26 +12,11 @@ defmodule ResistorColorDuo do
     white:  9
   }
 
-
   @doc """
   Calculate a resistance value from two colors
   """
   @spec value(colors :: [atom]) :: integer
   def value(colors) do
-# [:brown, :black]
-
-#    color_value = %{
-#      black:  0,
-#      brown:  1,
-#      red:    2,
-#      orange: 3,
-#      yellow: 4,
-#      green:  5,
-#      blue:   6,
-#      violet: 7,
-#      grey:   8,
-#      white:  9
-#    }
     [band_1 | tail] = colors
     [band_2 | _] = tail
     first_digit  = @color_value[band_1]
