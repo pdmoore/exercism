94705b9d907e51e8df0ca07facdd29a405a9792f
Paul Moore <pdmoore@gmail.com>
2023-02-21 15:45:03 -0800

    elixir - resistor color trio

--- elixir/resistor-color-trio/lib/resistor_color_trio.ex
+++ elixir/resistor-color-trio/lib/resistor_color_trio.ex
@@ -1,8 +1,25 @@
 defmodule ResistorColorTrio do
+  @color_value %{
+    black:  0,
+    brown:  1,
+    red:    2,
+    orange: 3,
+    yellow: 4,
+    green:  5,
+    blue:   6,
+    violet: 7,
+    grey:   8,
+    white:  9
+  }
+
   @doc """
   Calculate the resistance value in ohms from resistor colors
   """
   @spec label(colors :: [atom]) :: {number, :ohms | :kiloohms | :megaohms | :gigaohms}
-  def label(colors) do
+  def label([band_1, band_2, band_3 | _]) do
+    tens = @color_value[band_1] * 10
+    ones = @color_value[band_2]
+    total = (tens + ones) * trunc(:math.pow(10, @color_value[band_3]))
+    {total, :ohms}
   end
 end
