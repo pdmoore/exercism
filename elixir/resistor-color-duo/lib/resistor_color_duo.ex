2233380760ddf687d859b6b80e0aa86a71627bd5
Paul Moore <pdmoore@gmail.com>
2023-02-20 18:17:51 -0800

    elixir - resistor color duo

--- elixir/resistor-color-duo/lib/resistor_color_duo.ex
+++ elixir/resistor-color-duo/lib/resistor_color_duo.ex
@@ -18,12 +18,11 @@ defmodule ResistorColorDuo do
       grey:   8,
       white:  9
     }
-    # if param is a list, access head | tail?
-    [head1 | tail] = colors
-    [head2 | _] = tail
-#    first = color_value[List.first(colors)]
-    first = color_value[head1]
-    second = color_value[head2]
-    String.to_integer(Integer.to_string(first) <> Integer.to_string(second))
+    [band_1 | tail] = colors
+    [band_2 | _] = tail
+    first_digit  = color_value[band_1]
+    second_digit = color_value[band_2]
+    two_digits_combined_as_string = Integer.to_string(first_digit) <> Integer.to_string(second_digit)
+    String.to_integer(two_digits_combined_as_string)
   end
 end
