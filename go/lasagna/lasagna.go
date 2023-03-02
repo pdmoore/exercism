fcf6bdbaa0fb0ed33c8266829b40f7db125fd4b8
Paul Moore <pdmoore@gmail.com>
2023-03-02 11:12:57 -0800

    go - lasagna

--- go/lasagna/lasagna.go
+++ go/lasagna/lasagna.go
@@ -15,5 +15,5 @@ func PreparationTime(numberOfLayers int) int {
 
 // ElapsedTime calculates the time elapsed cooking the lasagna. This time includes the preparation time and the time the lasagna is baking in the oven.
 func ElapsedTime(numberOfLayers, actualMinutesInOven int) int {
-	panic("ElapsedTime not implemented")
+	return PreparationTime(numberOfLayers) + actualMinutesInOven
 }
