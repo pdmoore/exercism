e549bb24317b8b1834fb41f1ada43e8c989da9a4
Paul Moore <pdmoore@gmail.com>
2023-03-05 12:28:47 -0800

    go - pangram

--- go/pangram/cases_test.go
+++ go/pangram/cases_test.go
@@ -1,4 +1,4 @@
-tpackage pangram
+package pangram
 
 // This is an auto-generated file. Do not change it manually. Run the generator to update the file.
 // See https://github.com/exercism/go#synchronizing-tests-and-instructions
