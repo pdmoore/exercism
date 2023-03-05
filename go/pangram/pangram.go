8ae4c983c79cbed82366e7f20acf1ec0fb4763c4
Paul Moore <pdmoore@gmail.com>
2023-03-05 12:18:06 -0800

    go - pangram

--- go/pangram/pangram.go
+++ go/pangram/pangram.go
@@ -1,10 +1,13 @@
 package pangram
 
+import "strings"
+
 func IsPangram(input string) bool {
 	uniqueCharacters := make(map[byte]struct{})
+	allLower := strings.ToLower(input)
 
-	for i := 0; i < len(input); i++ {
-		thisChar := input[i]
+	for i := 0; i < len(allLower); i++ {
+		thisChar := allLower[i]
 		if thisChar >= 'a' && thisChar <= 'z' {
 			_, isPresent := uniqueCharacters[thisChar]
 			if !isPresent {
