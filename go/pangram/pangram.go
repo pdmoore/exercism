221a008ef646dd54394a46846d1623a935369418
Paul Moore <pdmoore@gmail.com>
2023-03-05 12:15:05 -0800

    go - pangram

--- go/pangram/pangram.go
+++ go/pangram/pangram.go
@@ -1,13 +1,29 @@
 package pangram
 
+import "fmt"
+
 func IsPangram(input string) bool {
-	m := make(map[byte]bool)
+	m := make(map[byte]struct{})
 
 	for i := 0; i < len(input); i++ {
-		if m[input[i]] != true {
-			m[input[i]] = true
+		// check for a to z!
+
+		thisChar := input[i]
+		if thisChar >= 'a' && thisChar <= 'z' {
+			_, isPresent := m[thisChar]
+			if !isPresent {
+				m[thisChar] = struct{}{}
+			}
 		}
 	}
-
+	fmt.Println(m)
 	return len(m) == 26
+
+	//unique_chars := []byte{}
+	//
+	//for i := 0; i < len(input); i++ {
+	//	unique_chars = append(unique_chars, input[i])
+	//}
+	//
+	//return len(unique_chars) == 26
 }
