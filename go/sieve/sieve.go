be53ff594038d7ee193cc75ae1d7235eeef6e356
Paul Moore <pdmoore@gmail.com>
2023-03-08 13:40:37 -0800

    go - sieve

--- go/sieve/sieve.go
+++ go/sieve/sieve.go
@@ -1,5 +1,34 @@
 package sieve
 
 func Sieve(limit int) []int {
-	return []int{}
+	if limit == 1 {
+		return []int{}
+	}
+
+	//// loop through that and remove multiples of current index
+	//candidates := make([]int, limit)
+	//for i := 2; i < limit; i++ {
+	//	candidates[i-2] = i
+	//}
+
+	//return candidates
+
+	//var primes []int
+	//for j := range candidates {
+	//	primes = append(primes, candidates[j])
+	//}
+	//
+	//return primes
+
+	var primes = []int{}
+
+	candidates := []int{limit}
+
+	for i := range candidates {
+		//fmt.Println(candidates[i])
+		primes = append(primes, candidates[i])
+	}
+
+	return primes
+
 }
