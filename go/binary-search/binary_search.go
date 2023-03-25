7225f37e30fdbf80fdf29027d4420c15b6f1b0b3
Paul Moore <pdmoore@gmail.com>
2023-03-25 13:22:38 -0700

    go - binary search

--- go/binary-search/binary_search.go
+++ go/binary-search/binary_search.go
@@ -3,14 +3,15 @@ package binarysearch
 const KeyNotFound = -1
 
 func SearchInts(list []int, key int) int {
-	var leftBoundary = 0
-	var rightBoundary = len(list) - 1
+	var leftBoundary, rightBoundary = 0, len(list) - 1
 
 	for leftBoundary <= rightBoundary {
 		var middleIndex = ((rightBoundary - leftBoundary) / 2) + leftBoundary
 		if list[middleIndex] == key {
 			return middleIndex
-		} else if key < list[middleIndex] {
+		}
+
+		if key < list[middleIndex] {
 			rightBoundary = middleIndex - 1
 		} else {
 			leftBoundary = middleIndex + 1
