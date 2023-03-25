431337a5a945abf6f04980acdd53711c819e5211
Paul Moore <pdmoore@gmail.com>
2023-03-24 18:58:29 -0700

    go - binary search

--- go/binary-search/binary_search.go
+++ go/binary-search/binary_search.go
@@ -6,9 +6,22 @@ func SearchInts(list []int, key int) int {
 		return -1
 	}
 
-	var middleIndex = len(list) / 2
-	if list[middleIndex] == key {
-		return middleIndex
+	var left = 0
+	var right = len(list)
+
+	if left == right && list[left] == key {
+		return left
+	}
+
+	for left < right {
+		var middleIndex = (right - left) / 2
+		if list[middleIndex] == key {
+			return middleIndex
+		} else if key < list[middleIndex] {
+			right = middleIndex - 1
+		} else { // to the right
+			left = middleIndex + 1
+		}
 	}
 
 	return 0
