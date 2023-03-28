1d30b91fd4446f973a111d25debbb791dc4f5669
Paul Moore <pdmoore@gmail.com>
2023-03-28 10:19:02 -0700

    go - secret handshake

--- go/secret-handshake/secret_handshake.go
+++ go/secret-handshake/secret_handshake.go
@@ -4,8 +4,7 @@ func Handshake(code uint) []string {
 
 	var actions []string
 
-	var firstBitSet = code & (1 << uint(1-1))
-	if firstBitSet != 0 {
+	if code&(1<<uint(1-1)) != 0 {
 		actions = append(actions, "wink")
 	}
 
