8cc2108c8927ecf3db74e1ba5a459dd771d478d9
Paul Moore <pdmoore@gmail.com>
2023-05-06 16:49:02 -0700

    prolog - triangle

--- prolog/triangle/triangle.pl
+++ prolog/triangle/triangle.pl
@@ -5,7 +5,7 @@ triangle(Side1, Side2, Side3, _) :- Side2 + Side3 < Side1, !, fail.
 triangle(Side1, Side2, Side3, _) :- Side3 + Side1 < Side2, !, fail.
 
 triangle(Side1, Side2, Side3, "equilateral") :- Side1 = Side2, Side2 = Side3, !.
-triangle(Side1, Side2, Side3, Type) :- Type = "scalene", Side1 \= Side2, Side2 \= Side3, Side2 \= Side1, !.
+triangle(Side1, Side2, Side3, "scalene") :- Side1 \= Side2, Side2 \= Side3, Side2 \= Side1, !.
 triangle(Side, Side, _, Type) :- Type = "isosceles", !.
 triangle(Side, _, Side, Type) :- Type = "isosceles", !.
 triangle(_, Side, Side, Type) :- Type = "isosceles", !.
