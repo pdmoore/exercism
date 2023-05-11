4172a1fd995f30a68bf2f6b6a1deabfbd21e9939
Paul Moore <pdmoore@gmail.com>
2023-05-11 13:51:43 -0700

    prolog - raindrops

--- prolog/raindrops/raindrops.pl
+++ prolog/raindrops/raindrops.pl
@@ -5,11 +5,10 @@ convert(N, Sounds) :- divBy3(N), divBy5(N), divBy7(N) -> string_concat("PlingPla
 convert(N, Sounds) :- divBy3(N), divBy5(N) -> string_concat("PlingPlang", "", Sounds) ; number_string(N, Sounds).
 convert(N, Sounds) :- divBy3(N), divBy7(N) -> string_concat("PlingPlong", "", Sounds) ; number_string(N, Sounds).
 convert(N, Sounds) :- divBy5(N), divBy7(N) -> string_concat("PlangPlong", "", Sounds) ; number_string(N, Sounds).
-%convert(N, Sounds) :- divBy3(N) -> string_concat("Pling", "", Sounds) ; number_string(N, Sounds).
-convert(N, Sounds) :- divBy5(N) -> string_concat("Plang", "", Sounds) ; number_string(N, Sounds).
-convert(N, Sounds) :- divBy7(N) -> string_concat("Plong", "", Sounds) ; number_string(N, Sounds).
 divBy3(N) :- 0 is mod(N,3).
 divBy5(N) :- 0 is mod(N,5).
 divBy7(N) :- 0 is mod(N,7).
-convert(N, "Pling") :- divBy3(N), !.
+convert(N, "Pling") :- 0 is mod(N, 3), !.
+convert(N, "Plang") :- 0 is mod(N, 5), !.
+convert(N, "Plong") :- 0 is mod(N, 7), !.
 convert(N, Sounds) :- number_string(N, Sounds).
