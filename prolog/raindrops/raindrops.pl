2625c3e6d042c516584b77613314ea78365e400e
Paul Moore <pdmoore@gmail.com>
2023-05-11 13:09:50 -0700

    prolog - raindrops

--- prolog/raindrops/raindrops.pl
+++ prolog/raindrops/raindrops.pl
@@ -2,5 +2,5 @@
 % Extract the string_concat
 % Add mod(N,5) to the chain
 % Add mod(N,7) to the chain
-convert(N, Sounds) :- number_string(N, Sounds).
+% convert(N, Sounds) :- number_string(N, Sounds).
 convert(N, Sounds) :- 0 is mod(N,3) -> string_concat("Pling", "", Sounds) ; number_string(N, Sounds).
