40eefc10231bde7f897b4460b9163ed051869db4
Paul Moore <pdmoore@gmail.com>
2023-05-11 10:21:11 -0700

    prolog - raindrops, all Pling pass

--- prolog/raindrops/raindrops.pl
+++ prolog/raindrops/raindrops.pl
@@ -1,3 +1,6 @@
+% Extract the 0 is mod(N,3)
+% Extract the string_concat
+% Add mod(N,5) to the chain
+% Add mod(N,7) to the chain
 convert(N, Sounds) :- number_string(N, Sounds).
 convert(N, Sounds) :- 0 is mod(N,3) -> string_concat("Pling", "", Sounds) ; number_string(N, Sounds).
-PlingPlangPlong(N, Sounds) :- N = 3, string_concat("Pling", "", Sounds).
