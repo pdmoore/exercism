04171fd7930b535ab70bae8efbe1de0f0e2ef4cd
Paul Moore <pdmoore@gmail.com>
2023-05-11 14:18:35 -0700

    prolog - raindrops

--- prolog/raindrops/raindrops.pl
+++ prolog/raindrops/raindrops.pl
@@ -1,9 +1,9 @@
 convert(N, Sounds) :- 
-    DivBy3 is mod(N, 3),
-    DivBy5 is mod(N, 5), 
-    DivBy7 is mod(N, 7), 
-    (DivBy3 =:= 0 -> Pling = "Pling" ; Pling = ""),
-    (DivBy5 =:= 0 -> Plang = "Plang" ; Plang = ""),
-    (DivBy7 =:= 0 -> Plong = "Plong" ; Plong = ""),
+    ModuloOf3 is mod(N, 3),
+    ModuloOf5 is mod(N, 5), 
+    ModuloOf7 is mod(N, 7), 
+    (ModuloOf3 =:= 0 -> Pling = "Pling" ; Pling = ""),
+    (ModuloOf5 =:= 0 -> Plang = "Plang" ; Plang = ""),
+    (ModuloOf7 =:= 0 -> Plong = "Plong" ; Plong = ""),
     atomics_to_string([Pling, Plang, Plong], Result),
     (string_length(Result, 0) -> number_string(N, Sounds) ; Sounds = Result).
