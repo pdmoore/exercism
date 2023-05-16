f8a0dbeeb2b868d10e5ad5c56325ed1adb316a84
Paul Moore <pdmoore@gmail.com>
2023-05-15 18:45:07 -0700

    prolog - space age

--- prolog/space-age/space_age.pl
+++ prolog/space-age/space_age.pl
@@ -2,4 +2,3 @@ space_age("Earth", AgeSec, Years) :- Years = AgeSec / 31557600.
 space_age("Mercury", AgeSec, Years) :- Years = (AgeSec / 31557600) / 0.2408467. 
 space_age("Venus", AgeSec, Years) :- Years = (AgeSec / 31557600) / 0.61519726.
 space_age("Mars", AgeSec, Years) :- Years = (AgeSec / 31557600) / 1.8808158.
-space_age(Planet, AgeSec, Years).
