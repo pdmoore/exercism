cb2fb47c1facb784bbcc6f9e2e8b0a82488208a7
Paul Moore <pdmoore@gmail.com>
2023-05-15 19:07:22 -0700

    prolog - space age

--- prolog/space-age/space_age.pl
+++ prolog/space-age/space_age.pl
@@ -1,3 +1,4 @@
+earth_year_in_seconds(31557600).
 orbital_period("Mercury", 0.2408467).
 orbital_period("Venus", 0.61519726).
 orbital_period("Earth", 1.0).
@@ -10,4 +11,3 @@ space_age(Planet, AgeSec, Years) :-
     earth_year_in_seconds(S),
     orbital_period(Planet, OP),
     Years = (AgeSec / OP / S).
-earth_year_in_seconds(31557600).
