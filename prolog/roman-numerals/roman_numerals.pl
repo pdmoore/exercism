84d75dd5798f36e18d789e3b48bbef987d1d320c
Paul Moore <pdmoore@gmail.com>
2023-05-21 16:50:03 -0700

    prolog - roman numeral

--- prolog/roman-numerals/roman_numerals.pl
+++ prolog/roman-numerals/roman_numerals.pl
@@ -1,5 +1,5 @@
 ones(N, Numeral) :- N = 1, string_concat("I", "", Numeral).
-%ones(N, Numeral) :- N = 2, string_concat("I", "I", Numeral).
+ones(N, Numeral) :- N = 2, string_concat("I", "I", Numeral).
 convert(1, Numeral) :- ones(1, Numeral).
-convert(2, Numeral) :- string_concat("I", "I", Numeral).
+convert(2, Numeral) :- ones(2, Numeral).
 convert(3, Numeral) :- Two = "II", string_concat("I", Two, Numeral).
