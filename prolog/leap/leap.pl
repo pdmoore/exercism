1dfe1cd54655f65f2aca02384dcedec0fda700da
Paul Moore <pdmoore@gmail.com>
2023-05-28 14:00:26 -0700

    prolog - leap

--- prolog/leap/leap.pl
+++ prolog/leap/leap.pl
@@ -1,6 +1,2 @@
-%leap(0).
-leap(Year) :- 0 is mod(Year, 400), true.
+leap(Year) :- 0 is mod(Year, 400), true, !.
 leap(Year) :- 0 is mod(Year, 4), 0 =\= mod(Year, 100), true.
-%leap(1996) :- true.
-%leap(2000) :- true.
-%leap(2400) :- true.
