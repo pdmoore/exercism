38dcbbecc3cdeba62df4025d40a83972e5508ea5
Paul Moore <pdmoore@gmail.com>
2023-05-28 21:31:27 -0700

    prolog - acronym

--- prolog/acronym/acronym.pl
+++ prolog/acronym/acronym.pl
@@ -1,7 +1,8 @@
-abbreviate("Portable Network Graphics", Acronym) :- Acronym = "PNG".
+abbreviate("Portable Network Graphics", Acronym) :- Acronym = "PNG", !.
 abbreviate(Sentence, Acronym) :- 
-    split_string(Sentence, Words),
+    % split_string(Sentence," ", "",  Words),
     % need to [H | T] which should give me the first char
     % and do that for each item in Words, and collate each first char
     % into the return value
+    % look up maplist(getFirstChar, Words) as a possible mechanism
 abbreviate(Sentence, Acronym).
