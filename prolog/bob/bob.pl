67214c013fba63fc8308203b6435033f4f8d5e41
Paul Moore <pdmoore@gmail.com>
2023-05-31 14:11:36 -0700

    prolog - bob

--- prolog/bob/bob.pl
+++ prolog/bob/bob.pl
@@ -1,10 +1,11 @@
 hey(Sentence, Response) :- 
     normalize_space(string(Trimmed), Sentence), 
+    IsEmpty = string_length(Trimmed, 0),
     string_upper(Trimmed, Upper),
     IsQuestion = sub_string(Trimmed, _, 1, 0, "?"),
     IsYelling = (Trimmed = Upper),
     (
-    Trimmed = "", Response = "Fine. Be that way!";
+    IsEmpty, Response = "Fine. Be that way!";
     IsQuestion, IsYelling, Reponse = "Calm down, I know what I'm doing!";
     IsQuestion, Response = "Sure.";
     IsYelling, Response = "Whoa, chill out!"; 
