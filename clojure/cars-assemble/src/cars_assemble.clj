28a6f744e9323f16bd94ae4762ea160ddad9307f
Paul Moore <pdmoore@gmail.com>
2023-06-11 14:19:54 -0700

    clojure - cars assemble

---
+++ clojure/cars-assemble/src/cars_assemble.clj
@@ -0,0 +1,13 @@
+(ns cars-assemble)
+
+(defn production-rate
+  "Returns the assembly line's production rate per hour,
+   taking into account its success rate"
+  [speed]
+  (* speed 221.0)
+  )
+
+(defn working-items
+  "Calculates how many working cars are produced per minute"
+  [speed]
+  )
