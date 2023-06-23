0296b3e8a9d9954a66b715aeea85c7ce330af9dc
Paul Moore <pdmoore@gmail.com>
2023-06-23 13:00:17 -0700

    clojure - log levels

--- clojure/log-levels/src/log_levels.clj
+++ clojure/log-levels/src/log_levels.clj
@@ -15,16 +15,15 @@
    and returns its level in lower-case."
   [s]
   (def right-bracket (str/index-of s "]"))
-  (def word (subs s 1 right-bracket))
-  (def level word)
+  (def level (subs s 1 right-bracket))
   (str/lower-case level))
 
 (defn reformat
   "Takes a string representing a log line and formats it
    with the message first and the log level in parentheses."
   [s]
-  (def right-bracket (+ (str/index-of s "]") 3) )
-  (def message-body (str/trim (subs s right-bracket)) )
+  (def message-starts-at (+ (str/index-of s "]") 3) )
+  (def message-body (str/trim (subs s message-starts-at)) )
 
   ;(def message-body "in progress")
   (str message-body " (" (log-level s) ")")
