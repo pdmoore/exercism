69e03f2cff63e1d8a47d57e829a96ef2621733cc
Paul Moore <pdmoore@gmail.com>
2023-06-24 10:08:25 -0700

    clojure - leap

--- clojure/leap/src/leap.clj
+++ clojure/leap/src/leap.clj
@@ -1,9 +1,6 @@
 (ns leap)
 
 (defn leap-year? [year] ;; <- argslist goes here
-  (and
-    (zero? (mod year 4))
-    (or
-      (not (zero? (mod year 100)))
-      (zero? (mod year 400))))
-)
+  (and (zero? (mod year 4))
+       (or (not (zero? (mod year 100)))
+           (zero? (mod year 400)))))
