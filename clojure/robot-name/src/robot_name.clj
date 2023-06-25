a57c76d76cc3565988e82a52ca089b4ccb328afc
Paul Moore <pdmoore@gmail.com>
2023-06-25 09:51:34 -0700

    clojure - robot name

--- clojure/robot-name/src/robot_name.clj
+++ clojure/robot-name/src/robot_name.clj
@@ -1,7 +1,10 @@
 (ns robot-name)
 
+
+(def random-upper-char (char (+ (rand 26) 65)))
+
 (defn robot []
-  (def thing (format "%S%S%d%d%d" (char (+ (rand 26) 65)) (char (+ (rand 26) 65)) (rand-int 9) (rand-int 9) (rand-int 9)))
+  (def thing (format "%S%S%d%d%d" random-upper-char random-upper-char (rand-int 9) (rand-int 9) (rand-int 9)))
   )
 
 (defn robot-name [robot]
@@ -9,5 +12,5 @@
 )
 
 (defn reset-name [robot]
-  (def thing (format "%S%S%d%d%d" (char (+ (rand 26) 65)) (char (+ (rand 26) 65)) (rand-int 9) (rand-int 9) (rand-int 9)))
+  (def thing (format "%S%S%d%d%d" random-upper-char random-upper-char (rand-int 9) (rand-int 9) (rand-int 9)))
 )
