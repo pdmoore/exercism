5a25b052c0a3e172c5ee0fee49ab6211cb123c4d
Paul Moore <pdmoore@gmail.com>
2023-06-25 09:47:58 -0700

    clojure - robot name

--- clojure/robot-name/src/robot_name.clj
+++ clojure/robot-name/src/robot_name.clj
@@ -1,9 +1,7 @@
 (ns robot-name)
 
 (defn robot []
-  (defn char-range [lo hi]
-    range (int lo) (inc (int hi)))
-  (def thing (format "%S%S%d%d%d" "R" "X" (rand-int 9) (rand-int 9) (rand-int 9)))
+  (def thing (format "%S%S%d%d%d" (char (+ (rand 26) 65)) (char (+ (rand 26) 65)) (rand-int 9) (rand-int 9) (rand-int 9)))
   )
 
 (defn robot-name [robot] ;; <- arglist goes here
