b6a46ccdf347ebea603c3a8d706beb1ba5316f91
Paul Moore <pdmoore@gmail.com>
2023-06-25 13:29:34 -0700

    clojure - robot name

--- clojure/robot-name/src/robot_name.clj
+++ clojure/robot-name/src/robot_name.clj
@@ -1,5 +1,11 @@
 (ns robot-name)
 
+
+; This passes all tests but feels wrong
+; robot param isn't referenced in either robot-name or reset-name
+; some mild duplication in generate-name - could generate two random characters together
+; IntelliJ reports 5 Problems but doesn't offer useful fixes
+
 (defn random-char [] (char (+ (rand 26) 65)))
 (defn random-digits [] (rand-int 1000))
 
