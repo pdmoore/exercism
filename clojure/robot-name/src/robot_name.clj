06b22251dee57f86d54d6e677e659f5c40c2b2df
Paul Moore <pdmoore@gmail.com>
2023-06-25 09:36:40 -0700

    clojure - robot name

---
+++ clojure/robot-name/src/robot_name.clj
@@ -0,0 +1,13 @@
+(ns robot-name)
+
+(defn robot []
+
+  )
+
+(defn robot-name [robot] ;; <- arglist goes here
+  "RX837"
+)
+
+(defn reset-name [robot] ;; <- arglist goes here
+  ;; your code goes here
+)
