79856bc305f073b4db1eca39a0c929d2d3e1cffc
Paul Moore <pdmoore@gmail.com>
2023-06-27 12:39:05 -0700

    clojure - difference of squares

---
+++ clojure/difference-of-squares/src/difference_of_squares.clj
@@ -0,0 +1,14 @@
+(ns difference-of-squares)
+
+(defn difference [] ;; <- arglist goes here
+  ;; your code goes here
+)
+
+(defn sum-of-squares [] ;; <- arglist goes here
+  ;; your code goes here
+)
+
+(defn square-of-sum [N] ;; <- arglist goes here
+  ;; your code goes here
+  225
+)
