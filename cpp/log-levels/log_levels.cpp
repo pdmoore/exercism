9c8902ebd2618c35a42eb8932585ede29f2751a2
Paul Moore <pdmoore@gmail.com>
2023-07-21 09:13:49 -0700

    cpp - log levels

--- cpp/log-levels/log_levels.cpp
+++ cpp/log-levels/log_levels.cpp
@@ -2,8 +2,8 @@
 
 namespace log_line {
 
-    const std::string message(const char *string) {
-
-        return string;
+    const std::string message(const char *message_contents) {
+        std::string foo = message_contents;
+        return foo.substr(9);
     };
 } // namespace log_line
