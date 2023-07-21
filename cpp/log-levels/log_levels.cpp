a5c0f6720b016a994d4d127de899558f9413e9f9
Paul Moore <pdmoore@gmail.com>
2023-07-21 09:27:15 -0700

    cpp - log levels

--- cpp/log-levels/log_levels.cpp
+++ cpp/log-levels/log_levels.cpp
@@ -2,18 +2,18 @@
 
 namespace log_line {
 
-    const std::string message(std::string message_contents) {
+    std::string message(std::string message_contents) {
         int colon_index = message_contents.find(":");
         int start_of_description = colon_index + 2;
         return message_contents.substr(start_of_description);
     }
 
-    const std::string log_level(std::string message_contents) {
+    std::string log_level(std::string message_contents) {
         int end_of_level_index = message_contents.find("]") - 1;
         return message_contents.substr(1, end_of_level_index);
     }
 
-    const std::string reformat(std::string message_contents) {
-        return log_line::message(message_contents) + " (" + log_line::log_level(message_contents) + ")";
+    std::string reformat(std::string message_contents) {
+        return message(message_contents) + " (" + log_level(message_contents) + ")";
     }
 } // namespace log_line
