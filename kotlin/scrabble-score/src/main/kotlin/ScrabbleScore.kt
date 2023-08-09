dc3c17ca2c46ade070542b00c105cb5c6ab1210e
Paul Moore <pdmoore@gmail.com>
2023-08-09 13:04:48 -0700

    kotlin - scrabble score

--- kotlin/scrabble-score/src/main/kotlin/ScrabbleScore.kt
+++ kotlin/scrabble-score/src/main/kotlin/ScrabbleScore.kt
@@ -1,27 +1,27 @@
 object ScrabbleScore {
+    const val onePointers = "AEIOULNRST"
+    const val twoPointers = "DG"
+    const val threePointers = "BCMP"
+    const val fourPointers = "FHVWY"
+    const val fivePointers = "K"
+    const val eightPointers = "JX"
+    const val tenPointers = "QZ"
+    val scores = mapOf(
+        onePointers to 1,
+        twoPointers to 2,
+        threePointers to 3,
+        fourPointers to 4,
+        fivePointers to 5,
+        eightPointers to 8,
+        tenPointers to 10
+    )
 
     fun scoreLetter(c: Char): Int {
-        val onePointers = "AEIOULNRST"
-        val twoPointers = "DG"
-        val threePointers = "BCMP"
-        val fourPointers = "FHVWY"
-        val fivePointers = "K"
-        val eightPointers = "JX"
-        val tenPointers = "QZ"
-        val scores = mapOf(onePointers to 1,
-                           twoPointers to 2,
-                           threePointers to 3,
-                           fourPointers to 4,
-                           fivePointers to 5,
-                           eightPointers to 8,
-                           tenPointers to 10)
         val upperCasedCharacter = c.uppercase()
 
         for (key in scores.keys) {
             if (key.contains(upperCasedCharacter)) {
-                // TODO fix this to handle null better
-                val points = scores.get(key)
-                if (points != null) return points
+                return scores.get(key) ?: 0
             }
         }
 
@@ -31,7 +31,7 @@ object ScrabbleScore {
     fun scoreWord(word: String): Int {
         var sum = 0;
         for (character in word.iterator()) {
-            sum += scoreLetter(character);
+            sum += scoreLetter(character)
         }
         return sum;
     }
