4859799be7c70fafd4ba500c72a294ce26df0fd0
Paul Moore <pdmoore@gmail.com>
2023-08-09 12:47:27 -0700

    kotlin - scrabble score

--- kotlin/scrabble-score/src/main/kotlin/ScrabbleScore.kt
+++ kotlin/scrabble-score/src/main/kotlin/ScrabbleScore.kt
@@ -1,32 +1,34 @@
 object ScrabbleScore {
 
     fun scoreLetter(c: Char): Int {
-        val onePointLetters = listOf<String>("A", "E", "I", "O", "U", "L", "N", "R", "S", "T");
+        val onePointers = "AEIOULNRST"
+        val twoPointers = "DG"
         val twoPointLetters = listOf<String>("D", "G");
         val threePointLetters = listOf<String>("B", "C", "M", "P");
         val fourPointLetters = listOf<String>("F", "H", "V", "W", "Y");
         val fivePointLetters = listOf<String>("K");
         val eightPointLetters = listOf<String>("J", "X");
         val tenPointLetters = listOf<String>("Q", "Z");
-        if (onePointLetters.contains(c.uppercase())) {
+        val upperCasedCharacter = c.uppercase()
+        if (onePointers.contains(upperCasedCharacter)) {
             return 1
         }
-        if (twoPointLetters.contains(c.uppercase())) {
+        if (twoPointers.contains(upperCasedCharacter)) {
             return 2
         }
-        if (threePointLetters.contains(c.uppercase())) {
+        if (threePointLetters.contains(upperCasedCharacter)) {
             return 3
         }
-        if (fourPointLetters.contains(c.uppercase())) {
+        if (fourPointLetters.contains(upperCasedCharacter)) {
             return 4
         }
-        if (fivePointLetters.contains(c.uppercase())) {
+        if (fivePointLetters.contains(upperCasedCharacter)) {
             return 5
         }
-        if (eightPointLetters.contains(c.uppercase())) {
+        if (eightPointLetters.contains(upperCasedCharacter)) {
             return 8
         }
-        if (tenPointLetters.contains(c.uppercase())) {
+        if (tenPointLetters.contains(upperCasedCharacter)) {
             return 10
         }
         return 0;
