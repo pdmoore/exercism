bde8d6686f830391e25552f5a88001e632c893be
Paul Moore <pdmoore@gmail.com>
2023-08-09 12:44:15 -0700

    kotlin - scrabble score

--- kotlin/scrabble-score/src/main/kotlin/ScrabbleScore.kt
+++ kotlin/scrabble-score/src/main/kotlin/ScrabbleScore.kt
@@ -1,11 +1,32 @@
 object ScrabbleScore {
 
     fun scoreLetter(c: Char): Int {
-        val onePointLetters = listOf<String>("a", "o");
-        if (onePointLetters.contains(c.lowercase())) {
+        val onePointLetters = listOf<String>("A", "E", "I", "O", "U", "L", "N", "R", "S", "T");
+        val twoPointLetters = listOf<String>("D", "G");
+        val threePointLetters = listOf<String>("B", "C", "M", "P");
+        val fourPointLetters = listOf<String>("F", "H", "V", "W", "Y");
+        val fivePointLetters = listOf<String>("K");
+        val eightPointLetters = listOf<String>("J", "X");
+        val tenPointLetters = listOf<String>("Q", "Z");
+        if (onePointLetters.contains(c.uppercase())) {
             return 1
         }
-        if (c.lowercase().equals("z")) {
+        if (twoPointLetters.contains(c.uppercase())) {
+            return 2
+        }
+        if (threePointLetters.contains(c.uppercase())) {
+            return 3
+        }
+        if (fourPointLetters.contains(c.uppercase())) {
+            return 4
+        }
+        if (fivePointLetters.contains(c.uppercase())) {
+            return 5
+        }
+        if (eightPointLetters.contains(c.uppercase())) {
+            return 8
+        }
+        if (tenPointLetters.contains(c.uppercase())) {
             return 10
         }
         return 0;
