513c1ce5dc87e29780824f10a5d5d23c450061ef
Paul Moore <pdmoore@gmail.com>
2023-08-14 12:38:38 -0700

    kotlin - scrabble score

--- kotlin/scrabble-score/src/main/kotlin/ScrabbleScore.kt
+++ kotlin/scrabble-score/src/main/kotlin/ScrabbleScore.kt
@@ -1,27 +1,17 @@
 object ScrabbleScore {
-    val letterToPointValue = mapOf(
-            "AEIOULNRST" to 1,
-            "DG"    to 2,
-            "BCMP"  to 3,
-            "FHVWY" to 4,
-            "K"     to 5,
-            "JX"    to 8,
-            "QZ"    to 10
-    )
-
-    private fun scoreLetter(c: Char): Int {
-        val upperCasedCharacter = c.uppercase()
-
-        for (key in letterToPointValue.keys) {
-            if (key.contains(upperCasedCharacter)) {
-                return letterToPointValue[key] ?: 0
+    private fun scoreLetter(c: Char): Int =
+            when (c.uppercase()) {
+                in "AEIOULNRST" -> 1
+                in "DG" -> 2
+                in "BCMP" -> 3
+                in "FHVWY" -> 4
+                in "K" -> 5
+                in "JX" -> 8
+                in "QZ" -> 10
+                else -> 0
             }
-        }
-
-        return 0;
-    }
 
     fun scoreWord(word: String): Int {
-        return  word.map { c -> scoreLetter(c) }.sum();
+        return word.map { c -> scoreLetter(c) }.sum();
     }
 }
