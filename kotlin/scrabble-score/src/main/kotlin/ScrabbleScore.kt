3df9fdefd2575cc9e7816b578c0980fec9bc527d
Paul Moore <pdmoore@gmail.com>
2023-08-14 12:40:57 -0700

    kotlin - scrabble score

--- kotlin/scrabble-score/src/main/kotlin/ScrabbleScore.kt
+++ kotlin/scrabble-score/src/main/kotlin/ScrabbleScore.kt
@@ -1,14 +1,13 @@
 object ScrabbleScore {
     private fun scoreLetter(c: Char): Int =
             when (c.uppercase()) {
-                in "AEIOULNRST" -> 1
                 in "DG" -> 2
                 in "BCMP" -> 3
                 in "FHVWY" -> 4
                 in "K" -> 5
                 in "JX" -> 8
                 in "QZ" -> 10
-                else -> 0
+                else -> 1   // "AEIOULNRST"
             }
 
     fun scoreWord(word: String): Int =
