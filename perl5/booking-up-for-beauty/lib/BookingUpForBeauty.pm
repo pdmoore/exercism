019f16670d55b643ff3c4cf426bf48396d913f13
Paul Moore <pdmoore@gmail.com>
2023-09-24 15:12:40 -0700

    perl - beauty up for booking

--- perl5/booking-up-for-beauty/lib/BookingUpForBeauty.pm
+++ perl5/booking-up-for-beauty/lib/BookingUpForBeauty.pm
@@ -2,42 +2,22 @@ package BookingUpForBeauty;
 
 use v5.38;
 
-# Suggested datetime modules you can use:
 use Time::Piece;
-#use DateTime::Tiny;
-#use DateTime::Format::Strptime qw( );
-
-# Recommended, commented out for portability.
-#use Readonly;
 
 use Exporter ('import');
 our @EXPORT_OK = ('appointment_has_passed', 'is_afternoon_appointment', 'describe_appointment');
 
 my $STRPTIME_FORMAT = '%Y-%m-%d' . 'T' . '%H:%M:%S';
-#Readonly::Scalar $STRPTIME_FORMAT => $STRPTIME_FORMAT;
 
 # Private subroutines conventionally start with an underscore.
 # It isn't necessary, but provided for convenience.
 sub _parse_datetime ($date_string) {
-#    return DateTime::Tiny->from_string(Time::Piece->strptime($date_string, $STRPTIME_FORMAT));
-    #return DateTime::Tiny->from_string($date_string, $STRPTIME_FORMAT);
-    #return $date_string;
     return Time::Piece->strptime($date_string, $STRPTIME_FORMAT);
-#    my $format = DateTime::Format::Strptime->new($STRPTIME_FORMAT);
-#    return $format->parse_datetime($date_string);
 }
 
 sub appointment_has_passed ($date_string) {
     my $time = _parse_datetime($date_string);
-    #my $now  = DateTime::Tiny->now;
-    #return DateTime->compare($time, $now);
-    # parens are necessary, why?
-    return ($time < gmtime);
-    #return $time < gmtime;
-
-#    my $now = DateTime::Tiny->now;
-#    my $diff = $now - $time;
-#    return $diff;
+    return $time < gmtime;
 }
 
 sub is_afternoon_appointment ($date_string) {
