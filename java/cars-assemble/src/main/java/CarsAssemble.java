ba410c04a0604b9dd3139264f305fb3357b42c96
Paul Moore <pdmoore@gmail.com>
2023-10-05 09:55:21 -0700

    java - cars assemble

---
+++ java/cars-assemble/src/main/java/CarsAssemble.java
@@ -0,0 +1,12 @@
+public class CarsAssemble {
+
+    private final int BASE_PRODUCTION_RATE = 221;
+
+    public double productionRatePerHour(int speed) {
+        return BASE_PRODUCTION_RATE * speed;
+    }
+
+    public int workingItemsPerMinute(int speed) {
+        throw new UnsupportedOperationException("Please implement the CarsAssemble.workingItemsPerMinute() method");
+    }
+}
