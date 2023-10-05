84563445c6a7a670921568785a5057646cab8a17
Paul Moore <pdmoore@gmail.com>
2023-10-05 09:58:15 -0700

    java - cars assemble

--- java/cars-assemble/src/main/java/CarsAssemble.java
+++ java/cars-assemble/src/main/java/CarsAssemble.java
@@ -7,8 +7,13 @@ public class CarsAssemble {
     }
 
     public int workingItemsPerMinute(int speed) {
-        return (int) productionRatePerHour(speed);
-
+        return (int) (productionRatePerHour(speed) / 60);
+/*
+1 to 4: 100% success rate.
+5 to 8: 90% success rate.
+9: 80% success rate.
+10: 77% success rate.
+ */
 
     }
 }
