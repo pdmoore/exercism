60b15a1b74903a1b2506a6f38aca1cf0b397107d
Paul Moore <pdmoore@gmail.com>
2023-11-19 09:19:01 -0800

    java - clock

---
+++ java/clock/src/main/java/Clock.java
@@ -0,0 +1,25 @@
+class Clock {
+
+    private final int hours;
+    private final int minutes;
+
+    Clock(int hours, int minutes) {
+        this.hours = hours;
+        this.minutes = minutes;
+    }
+
+    void add(int minutes) {
+        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+    }
+
+    @Override
+    public String toString() {
+        return String.format("%02d:%02d", hours, minutes);
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+    }
+
+}
