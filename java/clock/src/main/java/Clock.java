ac8a22bb4edfea4eee99f5f873de0cf357632828
Paul Moore <pdmoore@gmail.com>
2023-11-19 11:22:05 -0800

    java - clock

--- java/clock/src/main/java/Clock.java
+++ java/clock/src/main/java/Clock.java
@@ -1,10 +1,27 @@
 class Clock {
 
-    private int hours;
-    private int minutes;
+    private int totalMinutes;
 
     Clock(int hours, int minutes) {
-        int totalMinutes = hours * 60 + minutes;
+        int initialMinutes = hours * 60 + minutes;
+        int hoursPortion = (initialMinutes / 60) % 24;
+        int minutesPortion = initialMinutes % 60;
+        if (minutesPortion < 0) {
+            minutesPortion = 60 + minutesPortion;
+            hoursPortion -= 1;
+        }
+        if (hoursPortion < 0) {
+            hoursPortion = 24 + hoursPortion;
+        }
+        totalMinutes = hoursPortion * 60 + minutesPortion;
+    }
+
+    void add(int minutes) {
+        totalMinutes += minutes;
+    }
+
+    @Override
+    public String toString() {
         int hoursPortion = (totalMinutes / 60) % 24;
         int minutesPortion = totalMinutes % 60;
         if (minutesPortion < 0) {
@@ -14,26 +31,12 @@ class Clock {
         if (hoursPortion < 0) {
             hoursPortion = 24 + hoursPortion;
         }
-        this.hours = hoursPortion;
-        this.minutes = minutesPortion;
-    }
-
-    void add(int minutes) {
-        int totalMinutes = hours * 60 + this.minutes + minutes;
-        Clock tempClock = new Clock(0, totalMinutes);
-        this.hours = tempClock.hours;
-        this.minutes = tempClock.minutes;
-    }
-
-    @Override
-    public String toString() {
-        return String.format("%02d:%02d", hours, minutes);
+        return String.format("%02d:%02d", hoursPortion, minutesPortion);
     }
 
     @Override
     public boolean equals(Object obj) {
         // No tests for checking if obj is null, not a Clock, or this same Clock
-        Clock other = (Clock)obj;
-        return this.hours == other.hours && this.minutes == other.minutes;
+        return this.totalMinutes == ((Clock)obj).totalMinutes;
     }
 }
