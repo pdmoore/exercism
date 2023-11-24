57f80c16db09bac14b74c4adc5600e41e6c33bb6
Paul Moore <pdmoore@gmail.com>
2023-11-24 08:42:26 -0800

    clojure - matching brackets

--- clojure/matching-brackets/src/matching_brackets.clj
+++ clojure/matching-brackets/src/matching_brackets.clj
@@ -4,9 +4,13 @@
       (cond
             (empty? s) (empty? brackets)
             (= (first s) \() (newthing (rest s) (conj brackets \())
-            (= (first s) \[) (newthing (rest s) (conj brackets \[))
             (= (first s) \)) (and (= (peek brackets) \() (newthing (rest s) (pop brackets)))
+
+            (= (first s) \[) (newthing (rest s) (conj brackets \[))
             (= (first s) \]) (and (= (peek brackets) \[) (newthing (rest s) (pop brackets)))
+
+            (= (first s) \{) (newthing (rest s) (conj brackets \{))
+            (= (first s) \}) (and (= (peek brackets) \{) (newthing (rest s) (pop brackets)))
             :else (newthing (rest s) brackets)
             ))
 
