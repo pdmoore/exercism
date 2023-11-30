cfda8cf84ce6b8a4fac058a97fd2005d2850203f
Paul Moore <pdmoore@gmail.com>
2023-11-30 09:39:04 -0800

    c++ - armstrong numbers

--- cpp/armstrong-numbers/armstrong_numbers.cpp
+++ cpp/armstrong-numbers/armstrong_numbers.cpp
@@ -5,16 +5,10 @@ namespace armstrong_numbers {
 
     bool is_armstrong_number(int candidate) {
         int num_digits = floor(log10(candidate) + 1);
-        int digits[10];
-        int number = candidate;
-        for (int i = num_digits; i >= 0; i--) {
-            digits[i] = number % 10;
-            number /= 10;
-        }
 
         int sum_of_powers = 0;
-        for (int i = 1; i <= num_digits; ++i) {
-            sum_of_powers += pow(digits[i], num_digits);
+        for (int i = candidate; i > 0;  i /= 10) {
+            sum_of_powers += pow(i % 10, num_digits);
         }
 
         return sum_of_powers == candidate;
