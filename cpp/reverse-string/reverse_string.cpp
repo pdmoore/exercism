dfb6ee738976487f2dba5b2c740f04470fea0e03
Paul Moore <pdmoore@gmail.com>
2023-12-04 13:05:47 -0800

    c++ - reverse string

--- cpp/reverse-string/reverse_string.cpp
+++ cpp/reverse-string/reverse_string.cpp
@@ -4,11 +4,10 @@
 
 namespace reverse_string {
     std::string reverse_string(const std::string forwards) {
-
         std::string result = "";
 
-        for (int i = forwards.length(); i > 0; i--) {
-            result += forwards[i - 1];
+        for (int i = forwards.length() - 1; i >= 0; i--) {
+            result += forwards[i];
         }
 
         return result;
