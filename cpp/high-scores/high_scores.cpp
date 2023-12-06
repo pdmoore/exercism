aa41d826453874acf03538b29dbde34a837ef757
Paul Moore <pdmoore@gmail.com>
2023-12-06 10:31:28 -0800

    cpp - high scores

--- cpp/high-scores/high_scores.cpp
+++ cpp/high-scores/high_scores.cpp
@@ -21,26 +21,11 @@ namespace arcade {
     }
 
     std::vector<int> HighScores::top_three() {
-
         std::vector<int> sorted_copy(scores.begin(), scores.end());
-
         std::sort(sorted_copy.rbegin(), sorted_copy.rend());
 
-        // hmmm std::max didn't work
-        int num_to_copy = scores.size();
-        if (num_to_copy > 3) {
-            num_to_copy = 3;
-        }
-
+        int num_to_copy = std::min(std::size(scores), size_t(3));
         std::vector<int> three(sorted_copy.begin(), sorted_copy.begin() + num_to_copy);
-
-//        if (scores.size() == 2) {
-//            three.pop_back();
-//        }
-//        if (scores.size() == 1) {
-//            three.pop_back();
-//            three.pop_back();
-//        }
 
         return three;
     }
