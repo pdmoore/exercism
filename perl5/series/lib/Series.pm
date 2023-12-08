b664471e25a2dc65730202ee3427d6fddc68a5a6
Paul Moore <pdmoore@gmail.com>
2023-12-08 13:21:35 -0800

    perl - series

---
+++ perl5/series/lib/Series.pm
@@ -0,0 +1,22 @@
+package Series;
+
+use v5.38;
+
+use Exporter qw<import>;
+our @EXPORT_OK = qw<slices>;
+
+sub slices ( $series, $slice_length ) {
+
+    die "series cannot be empty" if (length($series) == 0);
+
+    die "slice length cannot be zero" if ($slice_length == 0);
+    die "slice length cannot be negative" if ($slice_length < 0);
+    die "slice length cannot be greater than series length" if ($slice_length > length($series));
+
+    my @slices;
+    for (my $i = 0; $i < length($series) - $slice_length + 1; $i++) {
+        push(@slices, substr($series, $i, $slice_length))
+    }
+
+    return \@slices;
+}
