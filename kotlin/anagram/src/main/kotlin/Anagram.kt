3af380b2b78bf1f9cc4481850edbb6225570a151
Paul Moore <pdmoore@gmail.com>
2023-12-10 14:34:21 -0800

    kotlin - anagram

---
+++ kotlin/anagram/src/main/kotlin/Anagram.kt
@@ -0,0 +1,12 @@
+import java.util.*
+
+class Anagram(source: String) {
+    val source = source;
+
+
+    fun match(anagrams: Collection<String>): Set<String> {
+        // TODO("Implement the function to complete the task")
+//        if (source.equals(anagrams.iterator().next())) Collections.emptyList<String>()
+        return Collections.emptyList<String>().toSet()
+    }
+}
