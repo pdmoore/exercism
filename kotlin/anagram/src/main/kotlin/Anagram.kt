7bf6f332e72170c35e08247b775b581712e726a1
Paul Moore <pdmoore@gmail.com>
2023-12-10 14:58:03 -0800

    kotlin - anagram

--- kotlin/anagram/src/main/kotlin/Anagram.kt
+++ kotlin/anagram/src/main/kotlin/Anagram.kt
@@ -4,14 +4,11 @@ import kotlin.collections.HashSet
 class Anagram(source: String) {
     val source = source.lowercase();
 
-
     fun match(anagrams: Collection<String>): Set<String> {
         val result = HashSet<String>()
         anagrams.forEach {
 
             if (!source.equals(it.lowercase()) &&
-                it.length == source.length &&
-//                source.toList().containsAll(it.lowercase().toList())) {
                 source.lowercase().toCharArray().sorted() == it.lowercase().toCharArray().sorted()) {
                 result.add(it);
             }
