c6d1f7b63e8732fd41813386bc8f28cd1b249759
Paul Moore <pdmoore@gmail.com>
2023-12-12 12:04:10 -0800

    kotlin - triangle

--- kotlin/triangle/src/main/kotlin/Triangle.kt
+++ kotlin/triangle/src/main/kotlin/Triangle.kt
@@ -1,7 +1,6 @@
 class Triangle<out T : Number>(val a: T, val b: T, val c: T) {
     private var _sideLengths: Set<T>
     init {
-        if (a == 0 || b == 0 || c == 0) throw IllegalArgumentException()
         val sidesAsNumbers = listOf(a.toDouble(), b.toDouble(), c.toDouble())
         require(sidesAsNumbers[0] + sidesAsNumbers[1] > sidesAsNumbers[2])
         require(sidesAsNumbers[0] + sidesAsNumbers[2] > sidesAsNumbers[1])
