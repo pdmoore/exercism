ce87a08b62313c77df5ffe7e5bdb38f73702f7d9
Paul Moore <pdmoore@gmail.com>
2023-12-12 15:18:33 -0800

    kotlin - phone number

--- kotlin/phone-number/src/main/kotlin/PhoneNumber.kt
+++ kotlin/phone-number/src/main/kotlin/PhoneNumber.kt
@@ -1,18 +1,17 @@
 class PhoneNumber(var raw_number: String) {
 
     private val clean_number: String?
-    private val clean_number2: String?
     init {
-        clean_number = raw_number.filter(Char::isDigit)
-        require(arrayOf(10, 11).contains(clean_number.length))
-        if (clean_number.length == 11) require(clean_number.startsWith("1"))
-        clean_number2 = clean_number.takeLast(10);
-        require(validAreadCode(clean_number.takeLast(10)))
-        require(validExchangeCode(clean_number.takeLast(10)))
+        val justDigits = raw_number.filter(Char::isDigit)
+        require(arrayOf(10, 11).contains(justDigits.length))
+        if (justDigits.length == 11) require(justDigits.startsWith("1"))
+        clean_number = justDigits.takeLast(10);
+        require(validAreadCode(justDigits.takeLast(10)))
+        require(validExchangeCode(justDigits.takeLast(10)))
     }
 
     private fun validAreadCode(takeLast: String) = (('2'..'9').contains(takeLast[0]))
     private fun validExchangeCode(takeLast: String) = (('2'..'9').contains(takeLast[3]))
 
-    val number: String? = clean_number2
+    val number: String? = clean_number
 }
