2de4daa1195d69eb3494a9af49832a23d023b52c
Paul Moore <pdmoore@gmail.com>
2023-12-12 15:08:17 -0800

    kotlin - phone number

--- kotlin/phone-number/src/main/kotlin/PhoneNumber.kt
+++ kotlin/phone-number/src/main/kotlin/PhoneNumber.kt
@@ -2,15 +2,14 @@ class PhoneNumber(var raw_number: String) {
 
     private var clean_number: String?
     init {
-        val pattern = "[-.\\(\\)\\s+]".toRegex()
-        clean_number = raw_number.replace(pattern, "")
+        clean_number = raw_number.filter(Char::isDigit)
+        if (clean_number!!.length == 11) require(clean_number!!.startsWith("1"))
         if (clean_number!!.length == 11 && clean_number!!.startsWith("1")) {
             clean_number = clean_number!!.substring(1)
         }
         require(clean_number!!.length == 10)
         require(validAreadCode())
         require(validExchangeCode())
-        require(clean_number!!.matches("[0-9]+".toRegex()))
     }
 
     private fun validAreadCode() = (('2'..'9').contains(clean_number?.get(0) ?: true))
