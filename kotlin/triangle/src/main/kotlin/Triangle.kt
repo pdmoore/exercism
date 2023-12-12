febdb9a75429805cad3fee4171d63181a20e1463
Paul Moore <pdmoore@gmail.com>
2023-12-12 11:43:34 -0800

    kotlin - triangle

---
+++ kotlin/triangle/src/main/kotlin/Triangle.kt
@@ -0,0 +1,8 @@
+class Triangle<out T : Number>(val a: T, val b: T, val c: T) {
+
+    // TODO: Implement proper constructor
+
+    val isEquilateral: Boolean = true
+    val isIsosceles: Boolean = true
+    val isScalene: Boolean = true
+}
