be41c0cdf87d71d7cfd6cde9731d50a69204c40f
Paul Moore <pdmoore@gmail.com>
2023-12-11 18:44:48 -0800

    kotlin - rna transcription

--- kotlin/rna-transcription/src/main/kotlin/RnaTranscription.kt
+++ kotlin/rna-transcription/src/main/kotlin/RnaTranscription.kt
@@ -1,1 +1,11 @@
-fun transcribeToRna(dna: String): String = ""
+fun transcribeToRna(dna: String): String {
+    if (dna.isEmpty()) return ""
+
+    when (dna) {
+        "C" -> return "G"
+        "G" -> return "C"
+        "T" -> return "A"
+        "A" -> return "U"
+    }
+    return "oops"
+}
