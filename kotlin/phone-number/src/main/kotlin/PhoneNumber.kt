07501f219727ecfcdcbfe2754eaa3fed8e56e993
Paul Moore <pdmoore@gmail.com>
2023-12-12 15:00:40 -0800

    kotlin - phone number

--- kotlin/phone-number/src/main/kotlin/PhoneNumber.kt
+++ kotlin/phone-number/src/main/kotlin/PhoneNumber.kt
@@ -8,13 +8,14 @@ class PhoneNumber(var raw_number: String) {
             clean_number = clean_number!!.substring(1)
         }
         require(clean_number!!.length == 10)
-        require(!clean_number!!.startsWith("0"))
-        require(!clean_number!!.startsWith("1"))
-        require(clean_number!!.get(3) != '0')
-        require(clean_number!!.get(3) != '1')
+        require(validAreadCode())
+        require(validExchangeCode())
         require(clean_number!!.matches("[0-9]+".toRegex()))
     }
 
+
+    private fun validAreadCode() = (!clean_number!!.startsWith("0") && !clean_number!!.startsWith("1"))
+    private fun validExchangeCode() = (clean_number!!.get(3) != '0' &&  clean_number!!.get(3) != '1')
 
     val number: String? = clean_number
 }
