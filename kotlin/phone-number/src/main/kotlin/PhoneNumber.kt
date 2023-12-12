34f1608fcca2020287ada801b4bd120f63a35d9e
Paul Moore <pdmoore@gmail.com>
2023-12-12 14:44:04 -0800

    kotlin - phone number

--- kotlin/phone-number/src/main/kotlin/PhoneNumber.kt
+++ kotlin/phone-number/src/main/kotlin/PhoneNumber.kt
@@ -2,11 +2,9 @@ class PhoneNumber(var raw_number: String) {
 
     private val clean_number: String?
     init {
-        val pattern = "\\s+".toRegex()
+        val pattern = "[-.\\s+]".toRegex()
         clean_number = raw_number.replace("(", "")
             .replace(")", "")
-            .replace("-", "")
-            .replace(".", "")
             .replace(pattern, "")
     }
 
