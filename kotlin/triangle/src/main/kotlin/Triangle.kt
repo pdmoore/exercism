d12c49e01ea7c9be395dda46e1d757d44b864ec6
Paul Moore <pdmoore@gmail.com>
2023-12-12 12:07:18 -0800

    kotlin - triangle

--- kotlin/triangle/src/main/kotlin/Triangle.kt
+++ kotlin/triangle/src/main/kotlin/Triangle.kt
@@ -2,9 +2,9 @@ class Triangle<out T : Number>(val a: T, val b: T, val c: T) {
     private var _sideLengths: Set<Double>
     init {
         val sidesAsNumbers = listOf(a.toDouble(), b.toDouble(), c.toDouble())
-        require(sidesAsNumbers[0] + sidesAsNumbers[1] > sidesAsNumbers[2])
-        require(sidesAsNumbers[0] + sidesAsNumbers[2] > sidesAsNumbers[1])
-        require(sidesAsNumbers[1] + sidesAsNumbers[2] > sidesAsNumbers[0])
+        require(sidesAsNumbers[0] + sidesAsNumbers[1] > sidesAsNumbers[2] &&
+                sidesAsNumbers[0] + sidesAsNumbers[2] > sidesAsNumbers[1] &&
+                sidesAsNumbers[1] + sidesAsNumbers[2] > sidesAsNumbers[0])
 
         sidesAsNumbers.toSet().also { _sideLengths = it }
     }
