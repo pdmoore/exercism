bed2b7ce32d6f6e003f06476b22ff673cfea0d93
Paul Moore <pdmoore@gmail.com>
2023-12-16 18:19:47 -0800

    groovy - queen attack

--- groovy/queen-attack/src/main/groovy/QueenAttack.groovy
+++ groovy/queen-attack/src/main/groovy/QueenAttack.groovy
@@ -1,7 +1,16 @@
 class QueenAttack {
 
     static canAttack(Queen q1, Queen q2) {
-        return q1.row == q2.row ||
-               q2.column == q2.column;
+        if (q1.row == q2.row) {
+            return true;
+        }
+        if (q1.column == q2.column) {
+            return true;
+        }
+
+        boolean onRightDiagonal = Math.abs(q1.row - q1.column) == Math.abs(q2.row - q2.column);
+        boolean onLeftDiagonal =  (q1.row + q1.column) == (q2.row + q2.column);
+//        boolean diag = Math.abs(q1.row - q2.row) != Math.abs(q1.column - q2.column);
+        return onRightDiagonal || onLeftDiagonal;
     }
 }
