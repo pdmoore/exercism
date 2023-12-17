79f3e51f1732592179695a07ff8f853c5eb370b8
Paul Moore <pdmoore@gmail.com>
2023-12-16 18:06:21 -0800

    groovy - queen attack

--- groovy/queen-attack/src/main/groovy/Queen.groovy
+++ groovy/queen-attack/src/main/groovy/Queen.groovy
@@ -6,6 +6,9 @@ class Queen {
         if (row < 0 || column < 0) {
             throw new Exception("position must be positive");
         }
+        if (row >= 8 || column >= 8) {
+            throw new Exception("poisiton must be on board [0..8]");
+        }
         this.row = row;
         this.column = column;
     }
