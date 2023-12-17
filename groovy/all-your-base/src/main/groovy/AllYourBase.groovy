8c008b8c3cfeeea92ae71b6ee59fed110474c603
Paul Moore <pdmoore@gmail.com>
2023-12-17 11:10:12 -0800

    groovy - all your base

--- groovy/all-your-base/src/main/groovy/AllYourBase.groovy
+++ groovy/all-your-base/src/main/groovy/AllYourBase.groovy
@@ -1,30 +1,16 @@
 class AllYourBase {
 
     int inputBase;
-    def digits;
     int value;
 
     AllYourBase(inputBase, digits) {
         this.inputBase = inputBase;
-        this.digits = digits;
-        if (inputBase == 10) {
-            def size = digits.size;
-            def sum = 0;
-            for (int i = 0; i < size; i++) {
-                sum += (10**(size - i - 1)) * digits[i];
-            }
-
-            this.value = sum;
+        def sum = 0;
+        for (int i = 0; i < digits.size; i++) {
+            sum += (inputBase**(digits.size - i - 1)) * digits[i];
         }
-        if (inputBase == 2) {
-            def size = digits.size;
-            def sum = 0;
-            for (int i = 0; i < size; i++) {
-                sum += (2**(size - i - 1)) * digits[i];
-            }
 
-            this.value = sum;
-        }
+        this.value = sum;
     }
 
     def rebase(outputBase) {
