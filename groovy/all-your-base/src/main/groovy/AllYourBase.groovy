df35a37be3b324a2fad57c166f92a5821e7428c7
Paul Moore <pdmoore@gmail.com>
2023-12-17 11:08:42 -0800

    groovy - all your base

--- groovy/all-your-base/src/main/groovy/AllYourBase.groovy
+++ groovy/all-your-base/src/main/groovy/AllYourBase.groovy
@@ -11,7 +11,7 @@ class AllYourBase {
             def size = digits.size;
             def sum = 0;
             for (int i = 0; i < size; i++) {
-                sum += (10**(size-i-1)) * digits[i];
+                sum += (10**(size - i - 1)) * digits[i];
             }
 
             this.value = sum;
@@ -20,7 +20,7 @@ class AllYourBase {
             def size = digits.size;
             def sum = 0;
             for (int i = 0; i < size; i++) {
-                sum += (2**(size-i-1)) * digits[i];
+                sum += (2**(size - i - 1)) * digits[i];
             }
 
             this.value = sum;
@@ -28,30 +28,14 @@ class AllYourBase {
     }
 
     def rebase(outputBase) {
-        if (outputBase == 10) {
-
-            def result = [];
-            int num = this.value;
-            while (num > 0) {
-                int remainder = num % 10;
-                result.add(remainder);
-                num = num / 10;
-            }
-
-            return result.reverse();
+        def result = [];
+        int num = this.value;
+        while (num > 0) {
+            int remainder = num % outputBase;
+            result.add(remainder);
+            num = num / outputBase;
         }
 
-        if (outputBase == 2) {
-            def result = [];
-            int num = this.value;
-            while (num > 0) {
-                int remainder = num % 2;
-                result.add(remainder);
-                num = num / 2;
-            }
-            return result.reverse();
-        }
-
-
+        return result.reverse();
     }
 }
