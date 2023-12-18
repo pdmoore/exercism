f93d5aa9273f139b4ebc141d246fea85fd8a58d2
Paul Moore <pdmoore@gmail.com>
2023-12-18 12:51:31 -0800

    groovy - d&d character

--- groovy/dnd-character/src/main/groovy/DndCharacter.groovy
+++ groovy/dnd-character/src/main/groovy/DndCharacter.groovy
@@ -1,19 +1,10 @@
 class DndCharacter {
 
     int modifier(int constitution) {
-        switch (constitution) {
-            case 3: return (constitution - 11) / 2;
-            case 4:
-            case 5: return (constitution - 11) / 2;
-            case 6:
-                case 7: return (constitution - 11) / 2;
-            case 8:
-                case 9: return (constitution - 11) / 2;
-
+        if (constitution <= 11) {
+            return (constitution - 11) / 2;
         }
 
-        int ;
-
-        return 0;
+        return 18 - constitution;
     }
 }
