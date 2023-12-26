482d8fcf374401f9c7502b20dbf16e59abcb0195
Paul Moore <pdmoore@gmail.com>
2023-12-26 09:25:20 -0800

    prolog - isogram

--- prolog/isogram/isogram.pl
+++ prolog/isogram/isogram.pl
@@ -1,6 +1,5 @@
 isogram(Phrase) :- 
-  normalize_space(string(Trimmed), Phrase),
-  string_upper(Trimmed, Upper),
+  string_upper(Phrase, Upper),
   string_chars(Upper, Chars),
   include(is_alpha, Chars, JustLetters),
   is_set(JustLetters).
