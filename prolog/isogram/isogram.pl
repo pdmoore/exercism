d05bd3a99e44c5a904a25005d33ebcd4c9428a91
Paul Moore <pdmoore@gmail.com>
2023-12-26 09:19:49 -0800

    prolog - isogram

--- prolog/isogram/isogram.pl
+++ prolog/isogram/isogram.pl
@@ -2,23 +2,6 @@
 isogram(false).
 isogram(Phrase) :- 
   normalize_space(string(Trimmed), Phrase),
-  IsEmpty = string_length(Trimmed, 0),
   string_upper(Trimmed, Upper),
-  string_length(Upper, Length),
-  atom_chars(Upper, Chars),
-  char_count(Chars, 0, [], Count),
-  NoDuplicates = (Length, Count),
-  (
-      IsEmpty, true;
-      NoDuplicates
-  ).
-
-char_count([], _, C, C).
-char_count([C|T], Index, Counted, Result) :-
-  select(c(C, Positions), Counted, MoreCounted),
-  succ(Index, Index1),
-  char_count(T, Index1, [c(C,[Index[Positions])|MoreCounted\, Result).
-char_count([C|T], Index, Counted, Result) :-
-  \+ member(c(C,_), Counted),
-  succ(Index, Index1),
-  char_count(T, Index1, [c(C,[Index])|Counted], Result).
+  string_chars(Upper, Chars),
+  is_set(Chars).
