f8f3fb780df9fb48f3bed30fb35fcfb26086d86c
Paul Moore <pdmoore@gmail.com>
2023-12-28 08:46:21 -0800

    prolog - roman numerals

--- prolog/roman-numerals/roman_numerals.pl
+++ prolog/roman-numerals/roman_numerals.pl
@@ -1,12 +1,8 @@
-roman_char("V", 5).
-roman_char("IV", 4).
-roman_char("I", 1).
-
-roman(0) --> "".
-roman(N) --> { roman_char(Roman, Value), N >= Value }, Roman, { N1 is N - Value }, roman(N1).
-
 convert(N, Numeral) :- 
-    phrase(roman(N), Numerals), 
-    string_chars(Numerals, Chars),
-    atomic_list_concat(Chars, Atom),
-    atom_string(Atom, Numeral), !.
+  roman(N, Digits),
+  atomics_to_string(Digits, Numeral).
+
+roman(0, []) :- !.
+roman(N, ["I" | T])  :- N < 4, M is N - 1, roman(M, T), !.
+roman(N, ["IV" | T]) :- N < 5, M is N - 4, roman(M, T), !.
+roman(N, ["V"  | T]) :- N < 9, M is N - 5, roman(M, T), !.
