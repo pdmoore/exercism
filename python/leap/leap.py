1822a532b1793b4efaaeb21c5c5b8c6ff5671c8d
Paul Moore <pdmoore@gmail.com>
2024-01-15 20:15:25 -0800

    python - leap year

--- python/leap/leap.py
+++ python/leap/leap.py
@@ -1,6 +1,2 @@
 def leap_year(year):
-    if year % 400 == 0:
-        return True
-    if year % 100 == 0:
-        return False
-    return True if year % 4 == 0 else False
+    return (year % 400 == 0) or ((year % 4 == 0) and not (year % 100 == 0))
