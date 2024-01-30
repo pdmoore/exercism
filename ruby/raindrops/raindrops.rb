d7499db5e583fd829f1255fdb2d0cb0f09a24e4a
Paul Moore <pdmoore@gmail.com>
2024-01-30 09:00:24 -0800

    ruby - raindrops

---
+++ ruby/raindrops/raindrops.rb
@@ -0,0 +1,12 @@
+=begin
+Write your code for the 'Raindrops' exercise in this file. Make the tests in
+`raindrops_test.rb` pass.
+
+To get started with TDD, see the `README.md` file in your
+`ruby/raindrops` directory.
+=end
+class Raindrops
+  def self.convert(number)
+    return number.to_s
+  end
+end
