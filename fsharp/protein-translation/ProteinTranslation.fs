92e842b7d59ce8fb649171d8b68911aacbd70aaf
Paul Moore <pdmoore@gmail.com>
2024-02-15 09:35:40 -0800

    f# - protein translation

--- fsharp/protein-translation/ProteinTranslation.fs
+++ fsharp/protein-translation/ProteinTranslation.fs
@@ -1,7 +1,15 @@
 module ProteinTranslation
 
+let translateCodon(rna: string, proteins: list<string>) =
+    if "AUG".Equals(rna) then proteins @ ["Methionine"] else proteins
+
 let proteins (rna: string) =
     if rna.Length.Equals(0) then [] else
     
-    if "AUG".Equals(rna) then ["Methionine"] else [""]
+    // lists are immutable - need a new list concated with previous
+    // something like pass in codon and list
+    // return new list of translated codon
+    // BUT how to deal with STOP
+    let result = []
+    translateCodon(rna[0..2], result)
     
