7c953d9c45af097b56f4003384b53bca39f39b15
Paul Moore <pdmoore@gmail.com>
2024-02-19 11:01:57 -0800

    f# - protein translation

--- fsharp/protein-translation/ProteinTranslation.fs
+++ fsharp/protein-translation/ProteinTranslation.fs
@@ -4,7 +4,13 @@ let proteins (rna: string): string list =
     let doProteins (rna: string): (string * string) option =
         match rna[0..2] with
         | "AUG" -> Some ("Methionine",  rna[3..])
-        | "" -> None
+        | "UCA" | "UCC" | "UCG" | "UCU" -> Some ("Serine", rna[3..])
+        | "UAC" | "UAU" -> Some ("Tyrosine", rna[3..])
+        | "UGC" | "UGU" -> Some ("Cysteine", rna[3..])
+        | "UGG" -> Some("Tryptophan", rna[3..])
+        | "UUA" | "UUG" -> Some ("Leucine", rna[3..])
+        | "UUU" | "UUC" -> Some ("Phenylalanine", rna[3..])
+        | "UAA" | "UAG" | "UGA" | "" -> None
         | _ -> failwith "Unknown coding"
         
     List.unfold doProteins rna
