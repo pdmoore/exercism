36b279a4a350f9f59e07677d2973022791d046bb
Paul Moore <pdmoore@gmail.com>
2024-02-25 16:22:41 -0800

    c# - list ops

--- csharp/list-ops/ListOps.cs
+++ csharp/list-ops/ListOps.cs
@@ -11,15 +11,8 @@ public static class ListOps
         throw new NotImplementedException("You need to implement this function.");
     }
 
-    public static List<TOut> Map<TIn, TOut>(List<TIn> input, Func<TIn, TOut> map)
-    {
-        var result = new List<TOut>();
-        foreach (var item in input)
-        {
-            result.Add(map(item));
-        }
-        return result;
-    }
+    public static List<TOut> Map<TIn, TOut>(List<TIn> input, Func<TIn, TOut> map) => 
+        input.Select(map).ToList();
 
     public static List<T> Filter<T>(List<T> input, Func<T, bool> predicate) => 
         input.Where(predicate).ToList();
