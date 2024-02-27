4eedd69ed4a703018a0b92a32c3375881b9d8beb
Paul Moore <pdmoore@gmail.com>
2024-02-26 16:35:54 -0800

    c# - list ops

--- csharp/list-ops/ListOps.cs
+++ csharp/list-ops/ListOps.cs
@@ -6,10 +6,7 @@ public static class ListOps
 {
     public static int Length<T>(List<T> input) => input.Count;
 
-    public static List<T> Reverse<T>(List<T> input)
-    {
-        return Enumerable.Reverse(input).ToList();
-    }
+    public static List<T> Reverse<T>(List<T> input) => Enumerable.Reverse(input).ToList();
 
     public static List<TOut> Map<TIn, TOut>(List<TIn> input, Func<TIn, TOut> map) =>
         input.Select(map).ToList();
@@ -21,7 +18,7 @@ public static class ListOps
         input.Aggregate(start, func);
 
     public static TOut Foldr<TIn, TOut>(List<TIn> input, TOut start, Func<TIn, TOut, TOut> func) =>
-        input.OrderDescending().Aggregate(start, (current, @in) => func(@in, current));
+        Enumerable.Reverse(input).Aggregate(start, (current, @in) => func(@in, current));
 
     public static List<T> Concat<T>(List<List<T>> input)
     {
