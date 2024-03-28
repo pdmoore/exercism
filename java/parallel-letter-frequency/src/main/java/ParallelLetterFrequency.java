8b4c8d2bc2814a1b413fb278dd9ba376b4c831e3
Paul Moore <pdmoore@gmail.com>
2024-03-28 12:48:01 -0700

    java - parallel letter frequency

--- java/parallel-letter-frequency/src/main/java/ParallelLetterFrequency.java
+++ java/parallel-letter-frequency/src/main/java/ParallelLetterFrequency.java
@@ -1,27 +1,17 @@
 import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
 
 class ParallelLetterFrequency {
-    Map<Character, Integer> charCount = new HashMap<>();
+    Map<Character, Integer> charCount = new ConcurrentHashMap<>();
 
 
     ParallelLetterFrequency(String[] texts) {
 
-        Arrays.stream(texts).
-                sequential().
+        Arrays.stream(texts).parallel().
                 forEach(line -> line.replaceAll("[^\\p{L}]", "").chars().
                         forEach(ch -> charCount.merge(Character.toLowerCase((char) ch), 1, Integer::sum)));
-
-
-        // texts is an array of lines
-//        for (int i = 0; i < texts.length; i++) {
-//            // strip out white space
-//            texts[i].chars().forEach(ch -> charCount.merge(Character.toLowerCase((char) ch), 1, Integer::sum));
-//
-//        }
-
-
     }
 
     Map<Character, Integer> countLetters() {
