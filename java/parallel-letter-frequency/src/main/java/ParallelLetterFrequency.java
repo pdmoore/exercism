d79dac785b287823c17e7e6bb8c8ce16f760d3d3
Paul Moore <pdmoore@gmail.com>
2024-03-28 10:08:55 -0700

    java - parallel letter frequency

---
+++ java/parallel-letter-frequency/src/main/java/ParallelLetterFrequency.java
@@ -0,0 +1,15 @@
+import java.util.HashMap;
+import java.util.Map;
+
+class ParallelLetterFrequency {
+
+    ParallelLetterFrequency(String[] texts) {
+
+    }
+
+    Map<Character, Integer> countLetters() {
+        Map<Character, Integer> expectedOutput = new HashMap<>();
+        return expectedOutput;
+    }
+
+}
