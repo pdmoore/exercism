7c3d83ec1c0b3870037b77c71dc468da4bca9630
Paul Moore <pdmoore@gmail.com>
2024-04-04 09:46:54 -0700

    bash - sieve

---
+++ bash/sieve/sieve.sh
@@ -0,0 +1,33 @@
+#!/usr/bin/env bash
+
+# The following comments should help you get started:
+# - Bash is flexible. You may use functions or write a "raw" script.
+#
+# - Complex code can be made easier to read by breaking it up
+#   into functions, however this is sometimes overkill in bash.
+#
+# - You can find links about good style and other resources
+#   for Bash in './README.md'. It came with this exercise.
+#
+#   Example:
+#   # other functions here
+#   # ...
+#   # ...
+#
+#   main () {
+#     # your main function code here
+#   }
+#
+#   # call main with all of the positional arguments
+#   main "$@"
+#
+# *** PLEASE REMOVE THESE COMMENTS BEFORE SUBMITTING YOUR SOLUTION ***
+(( $# == 1))  || exit 0
+if [ $1 -ge 2 ]; 
+then
+    declare -a primes
+    primes+=( $1 )
+    echo "${primes[@]}"
+else
+    echo ""
+fi
