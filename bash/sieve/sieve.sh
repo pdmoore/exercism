fdc4d39f06e86fb487a18b5e92c00b8a68f09b75
Paul Moore <pdmoore@gmail.com>
2024-04-04 10:05:10 -0700

    bash - sieve

--- bash/sieve/sieve.sh
+++ bash/sieve/sieve.sh
@@ -1,37 +1,14 @@
-#!/usr/bin/env bash
-
-# The following comments should help you get started:
-# - Bash is flexible. You may use functions or write a "raw" script.
-#
-# - Complex code can be made easier to read by breaking it up
-#   into functions, however this is sometimes overkill in bash.
-#
-# - You can find links about good style and other resources
-#   for Bash in './README.md'. It came with this exercise.
-#
-#   Example:
-#   # other functions here
-#   # ...
-#   # ...
-#
-#   main () {
-#     # your main function code here
-#   }
-#
-#   # call main with all of the positional arguments
-#   main "$@"
-#
-# *** PLEASE REMOVE THESE COMMENTS BEFORE SUBMITTING YOUR SOLUTION ***
 declare -a primes
 declare -a sieve 
+limit=$1
     
 # mark all as prime up to limit
-for ((i=2; i<=$1; i++)); do sieve[i]=1; done
+for ((i=2; i<=$limit; i++)); do sieve[i]=1; done
        
-for ((i=2; i<=$1; i++)); do
+for ((i=2; i<=$limit; i++)); do
     (( sieve[i] )) || continue
     primes+=( $i )
-    for ((j=2*i; j<=$1; j+=i)) do sieve[j]=0; done
+    for ((j=2*i; j<=$limit; j+=i)) do sieve[j]=0; done
 done
 
 echo "${primes[@]}"
