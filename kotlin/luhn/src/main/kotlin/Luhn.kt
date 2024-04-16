75093bb64a17c29a33cf6ffb5381e69e1e9da543
Paul Moore <pdmoore@gmail.com>
2024-04-16 16:28:08 -0700

    kotlin - luhn

--- kotlin/luhn/src/main/kotlin/Luhn.kt
+++ kotlin/luhn/src/main/kotlin/Luhn.kt
@@ -1,18 +1,17 @@
 object Luhn {
 
     fun isValid(candidate: String): Boolean {
-        if (!candidate.matches("[0-9 ]+".toRegex())) return false;
+        if (!candidate.matches("[0-9 ]+".toRegex())) return false
 
-        var onlyDigits = candidate.filter { it.isDigit() }
+        val reversedDigits = candidate.filter { it.isDigit() }.reversed()
 
-        if (onlyDigits.length <= 1) {
+        if (reversedDigits.length <= 1) {
             return false
         }
 
-        var reversed = onlyDigits.reversed();
         var doubleThis = false
         var sum = 0
-        for (char in reversed) {
+        for (char in reversedDigits) {
             if (doubleThis) {
                 var digit = char.toString().toInt() * 2
                 if (digit > 9) digit -= 9
