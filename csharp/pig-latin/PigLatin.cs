27f57fd6fa1ef39ef305e201823e71fbe90341a3
Paul Moore <pdmoore@gmail.com>
2024-07-01 16:17:56 -0700

    c# - pig latin

--- csharp/pig-latin/PigLatin.cs
+++ csharp/pig-latin/PigLatin.cs
@@ -2,8 +2,8 @@ using System;
 
 public static class PigLatin
 {
-    private static readonly string Vowels = "aeiou";
-    
+    private const string Vowels = "aeiou";
+
     public static string Translate(string word)
     {
         if (StartsWithVowel(word))
@@ -11,7 +11,10 @@ public static class PigLatin
             return word + "ay";
         }
 
-        return "igpay";
+        var firstChar = word.ToCharArray()[0];
+        var restOfWord = word.Substring(1);
+
+        return restOfWord + firstChar + "ay";
     }
 
     private static bool StartsWithVowel(string word) => Vowels.Contains(word.ToCharArray()[0]);
