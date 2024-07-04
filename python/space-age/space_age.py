3b6d18563c4937e553ad074d0ea44c439f3482b6
Paul Moore <pdmoore@gmail.com>
2024-07-04 14:55:14 -0700

    python - space age

--- python/space-age/space_age.py
+++ python/space-age/space_age.py
@@ -1,34 +1,31 @@
-EARTH_SECONDS_IN_YEAR = 60 * 60 *24 * 365.25
+"""seconds per minute per hour per day per year"""
+EARTH_SECONDS_IN_YEAR = 60 * 60 * 24 * 365.25
 
 class SpaceAge:
     def __init__(self, seconds):
-        self.seconds = seconds
-
-    def earth_years(self):
-        return self.seconds / EARTH_SECONDS_IN_YEAR
+        self.earth_age = seconds / EARTH_SECONDS_IN_YEAR
 
     def on_earth(self):
-        return round(self.earth_years(), 2)
+        return round(self.earth_age, 2)
 
     def on_mercury(self):
-        return round(self.earth_years() / 0.2408467, 2)
+        return round(self.earth_age / 0.2408467, 2)
 
     def on_venus(self):
-        return round(self.earth_years() / 0.61519726, 2)
+        return round(self.earth_age / 0.61519726, 2)
 
     def on_mars(self):
-        return round(self.earth_years() / 1.8808158, 2)
+        return round(self.earth_age / 1.8808158, 2)
 
     def on_jupiter(self):
-        return round(self.earth_years() / 11.862615, 2)
-
+        return round(self.earth_age / 11.862615, 2)
 
     def on_saturn(self):
-        return round(self.earth_years() / 29.447498, 2)
+        return round(self.earth_age / 29.447498, 2)
 
 
     def on_uranus(self):
-        return round(self.earth_years() / 84.016846, 2)
+        return round(self.earth_age / 84.016846, 2)
 
     def on_neptune(self):
-        return round(self.earth_years() / 164.79132, 2)
+        return round(self.earth_age / 164.79132, 2)
