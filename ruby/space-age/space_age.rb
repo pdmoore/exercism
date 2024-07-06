b4785c1f6b0be71516b7caf2d4366aa30e2cd49b
Paul Moore <pdmoore@gmail.com>
2024-07-06 16:27:39 -0700

    ruby - space age

--- ruby/space-age/space_age.rb
+++ ruby/space-age/space_age.rb
@@ -1,38 +1,21 @@
 class SpaceAge
   EARTH_YEAR_IN_SECONDS = 60 * 60 * 24 * 365.25
   def initialize(seconds)
-    @earth_age = seconds / EARTH_YEAR_IN_SECONDS
+    @seconds = seconds
   end
 
-  def on_earth
-    @earth_age
-  end
-
-  def on_mercury
-    @earth_age / 0.2408467
-  end
-
-  def on_venus
-    @earth_age / 0.61519726# code here
-  end
-
-  def on_mars
-    @earth_age / 1.8808158 # code here
-  end
-
-  def on_saturn
-    @earth_age / 29.447498# code here
-  end
-
-  def on_jupiter
-    @earth_age / 11.862615# code here
-  end
-
-  def on_uranus
-    @earth_age / 84.016846# code here
-  end
-
-  def on_neptune
-    @earth_age / 164.79132# code here
+  YEAR_DURATION_RELATIVE_TO_EARTH = {
+    mercury:   0.2408467,
+    venus:     0.61519726,
+    earth:     1.0,
+    mars:      1.8808158,
+    jupiter:  11.862615,
+    saturn:   29.447498,
+    uranus:   84.016846,
+    neptune: 164.79132
+  }.each do |planet, earth_years|
+    define_method :"on_#{planet}" do
+      @seconds.fdiv(EARTH_YEAR_IN_SECONDS * earth_years).round(2)
+    end
   end
 end
