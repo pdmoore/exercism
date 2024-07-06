281827dd42ad017f11398c9e2f2e48c687e81333
Paul Moore <pdmoore@gmail.com>
2024-07-06 16:18:54 -0700

    ruby - space age

--- ruby/space-age/space_age.rb
+++ ruby/space-age/space_age.rb
@@ -1,12 +1,4 @@
-=begin
-Write your code for the 'Space Age' exercise in this file. Make the tests in
-`space_age_test.rb` pass.
-
-To get started with TDD, see the `README.md` file in your
-`ruby/space-age` directory.
-=end
 class SpaceAge
-
   def initialize(seconds)
     @earth_age = seconds / 31_557_600.0
   end
