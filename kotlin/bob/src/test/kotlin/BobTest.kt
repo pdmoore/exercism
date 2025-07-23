3eb4fafb69aea9e8649ee2e3922becaabe892235
Paul Moore <pdmoore@gmail.com>
2025-07-23 09:45:30 -0700

    kotlin - bob

--- kotlin/bob/src/test/kotlin/BobTest.kt
+++ kotlin/bob/src/test/kotlin/BobTest.kt
@@ -34,13 +34,11 @@ class BobTest {
         assertEquals("Sure.", Bob.hey("fffbbcbeab?"))
     }
 
-    @Ignore
     @Test
     fun talkingForcefully() {
         assertEquals("Whatever.", Bob.hey("Let's go make out behind the gym!"))
     }
 
-    @Ignore
     @Test
     fun usingAcronymsInRegularSpeech() {
         assertEquals("Whatever.", Bob.hey("It's OK if you don't want to go to the DMV."))
