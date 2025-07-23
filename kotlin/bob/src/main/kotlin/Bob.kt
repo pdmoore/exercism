31e538af5df4831d816297f9f193f46cc0426db5
Paul Moore <pdmoore@gmail.com>
2025-07-23 09:56:39 -0700

    kotlin - bob

--- kotlin/bob/src/main/kotlin/Bob.kt
+++ kotlin/bob/src/main/kotlin/Bob.kt
@@ -3,16 +3,16 @@ object Bob {
         val trimmedInput = input.trim()
         if (trimmedInput.isEmpty()) return "Fine. Be that way!"
 
-        val containsLetters = input.any { it.isLetter() }
+        val containsLetters = trimmedInput.any { it.isLetter() }
         val isQuestion = trimmedInput.endsWith('?')
 
         if (containsLetters) {
-            val isYelling = input.equals(input.uppercase())
+            val isYelling = trimmedInput == trimmedInput.uppercase()
 
             if (isYelling && isQuestion) return "Calm down, I know what I'm doing!"
             if (isYelling) return "Whoa, chill out!"
         }
-            if (isQuestion) return "Sure."
+        if (isQuestion) return "Sure."
 
         return "Whatever."
     }
