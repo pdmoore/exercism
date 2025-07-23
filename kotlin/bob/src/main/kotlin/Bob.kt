4deefe422d5c6d81e419f522443ce299d15eb145
Paul Moore <pdmoore@gmail.com>
2025-07-23 10:10:32 -0700

    kotlin - bob

--- kotlin/bob/src/main/kotlin/Bob.kt
+++ kotlin/bob/src/main/kotlin/Bob.kt
@@ -3,22 +3,21 @@ object Bob {
 
         if (input.isSilence()) return "Fine. Be that way!"
 
-        val containsLetters = input.trim().any { it.isLetter() }
-
-        if (containsLetters) {
             val isYelling = input.isYelling()
 
             if (isYelling && input.isQuestion()) return "Calm down, I know what I'm doing!"
             if (isYelling) return "Whoa, chill out!"
-        }
+
         if (input.isQuestion()) return "Sure."
 
         return "Whatever."
     }
 }
 
-private fun String.isQuestion(): Boolean = this.trim().endsWith('?')
+private fun String.isQuestion(): Boolean = !this.isSilence() && this.isNotEmpty() && this.trim().last() == '?'
+private fun String.isYelling() = this.stripLastChar().any { it.isLetter() } && this.stripLastChar().filter { it.isLetter() }.all { it.isUpperCase() }
 
-private fun String.isYelling() = this.trim() == this.trim().uppercase()
-
-private fun String.isSilence(): Boolean = this.trim().isEmpty()
+private val PUNCTUATION_REGEX = "[!\"#\$%&'()*+,-./:;<=>?@\\[\\]^_`{|}~]".toRegex();
+private fun String.stripLastChar(): String = this.slice(0..this.length - 2)
+private fun String.isPunctuations(): Boolean = PUNCTUATION_REGEX.containsMatchIn( this.stripLastChar().filter { !it.isWhitespace() } )
+private fun String.isSilence(): Boolean = this.none { it.isLetterOrDigit() } && !this.isPunctuations()
