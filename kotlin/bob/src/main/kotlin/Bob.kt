a7b5840e039dfadf7de7cbcb0ad565e3ee94b5cf
Paul Moore <pdmoore@gmail.com>
2025-07-23 10:07:24 -0700

    kotlin - bob

--- kotlin/bob/src/main/kotlin/Bob.kt
+++ kotlin/bob/src/main/kotlin/Bob.kt
@@ -3,22 +3,22 @@ object Bob {
 
         if (input.isSilence()) return "Fine. Be that way!"
 
-
-        val trimmedInput = input.trim()
-
-        val containsLetters = trimmedInput.any { it.isLetter() }
-        val isQuestion = trimmedInput.endsWith('?')
+        val containsLetters = input.trim().any { it.isLetter() }
 
         if (containsLetters) {
-            val isYelling = trimmedInput == trimmedInput.uppercase()
+            val isYelling = input.isYelling()
 
-            if (isYelling && isQuestion) return "Calm down, I know what I'm doing!"
+            if (isYelling && input.isQuestion()) return "Calm down, I know what I'm doing!"
             if (isYelling) return "Whoa, chill out!"
         }
-        if (isQuestion) return "Sure."
+        if (input.isQuestion()) return "Sure."
 
         return "Whatever."
     }
 }
+
+private fun String.isQuestion(): Boolean = this.trim().endsWith('?')
+
+private fun String.isYelling() = this.trim() == this.trim().uppercase()
 
 private fun String.isSilence(): Boolean = this.trim().isEmpty()
