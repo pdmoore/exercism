438bcd622af7389e9dffbece5534371d82a3d790
Paul Moore <pdmoore@gmail.com>
2025-07-23 09:43:28 -0700

    kotlin - bob

--- kotlin/bob/src/main/kotlin/Bob.kt
+++ kotlin/bob/src/main/kotlin/Bob.kt
@@ -1,5 +1,7 @@
 object Bob {
     fun hey(input: String): String {
+        if (input.equals(input.uppercase())) return "Whoa, chill out!"
+
         return "Whatever."
     }
 }
--- kotlin/bob/src/test/kotlin/BobTest.kt
+++ kotlin/bob/src/test/kotlin/BobTest.kt
@@ -9,7 +9,6 @@ class BobTest {
         assertEquals("Whatever.", Bob.hey("Tom-ay-to, tom-aaaah-to."))
     }
 
-    @Ignore
     @Test
     fun shouting() {
         assertEquals("Whoa, chill out!", Bob.hey("WATCH OUT!"))
