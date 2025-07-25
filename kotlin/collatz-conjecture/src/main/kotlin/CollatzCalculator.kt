e887662bd4659aa115c4ca7de8dbae31f9150087
Paul Moore <pdmoore@gmail.com>
2025-07-25 09:39:45 -0700

    kotlin - collatz

--- kotlin/collatz-conjecture/src/main/kotlin/CollatzCalculator.kt
+++ kotlin/collatz-conjecture/src/main/kotlin/CollatzCalculator.kt
@@ -8,10 +8,10 @@ object CollatzCalculator {
     }
 
     private fun countSteps(start: Int, count: Int): Int {
-        if (start == 1) return count
-
-        if (start % 2 == 0) return countSteps(start / 2, count + 1)
-
-        return countSteps(start * 3 + 1, count + 1)
+        when {
+            start == 1 -> return count
+            start % 2 == 0 -> return countSteps(start / 2, count + 1)
+            else -> return countSteps(start * 3 + 1, count + 1)
+        }
     }
 }
