f956abcf1e12592a493b1b71983f598bb9fa8b42
Paul Moore <pdmoore@gmail.com>
2025-07-25 09:43:28 -0700

    kotlin - collatz

--- kotlin/collatz-conjecture/src/main/kotlin/CollatzCalculator.kt
+++ kotlin/collatz-conjecture/src/main/kotlin/CollatzCalculator.kt
@@ -7,9 +7,9 @@ object CollatzCalculator {
 
     private fun countSteps(n: Int, stepNumber: Int): Int {
         when {
-            n == 1 -> return stepNumber
+            n == 1     -> return stepNumber
             n.IsEven() -> return countSteps(n / 2, stepNumber + 1)
-            else -> return countSteps(n * 3 + 1, stepNumber + 1)
+            else       -> return countSteps(n * 3 + 1, stepNumber + 1)
         }
     }
 }
