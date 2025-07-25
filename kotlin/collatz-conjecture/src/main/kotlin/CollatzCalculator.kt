1f834f0d7eafaccb4893a16f61109efa7bdf3969
Paul Moore <pdmoore@gmail.com>
2025-07-25 09:41:55 -0700

    kotlin - collatz

--- kotlin/collatz-conjecture/src/main/kotlin/CollatzCalculator.kt
+++ kotlin/collatz-conjecture/src/main/kotlin/CollatzCalculator.kt
@@ -1,17 +1,15 @@
-import javax.print.attribute.standard.QueuedJobCount
-
 object CollatzCalculator {
     fun computeStepCount(start: Int): Int {
-        if (start <= 0) throw IllegalArgumentException("start")
+        require(start > 0) { "Only positive integers allowed" }
 
         return countSteps(start, 0)
     }
 
-    private fun countSteps(start: Int, count: Int): Int {
+    private fun countSteps(n: Int, stepNumber: Int): Int {
         when {
-            start == 1 -> return count
-            start % 2 == 0 -> return countSteps(start / 2, count + 1)
-            else -> return countSteps(start * 3 + 1, count + 1)
+            n == 1 -> return stepNumber
+            n % 2 == 0 -> return countSteps(n / 2, stepNumber + 1)
+            else -> return countSteps(n * 3 + 1, stepNumber + 1)
         }
     }
 }
