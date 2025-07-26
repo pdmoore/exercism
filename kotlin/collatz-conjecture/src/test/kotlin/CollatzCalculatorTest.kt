0ce68c52581c6ad84dccd1031da21f5dec9a9b6c
Paul Moore <pdmoore@gmail.com>
2025-07-26 15:29:44 -0700

    kotlin - collatz

--- kotlin/collatz-conjecture/src/test/kotlin/CollatzCalculatorTest.kt
+++ kotlin/collatz-conjecture/src/test/kotlin/CollatzCalculatorTest.kt
@@ -1,7 +1,4 @@
-import org.junit.Ignore
-import org.junit.Rule
 import org.junit.Test
-import org.junit.rules.ExpectedException
 import kotlin.test.assertEquals
 
 class CollatzCalculatorTest {
@@ -28,6 +25,17 @@ class CollatzCalculatorTest {
         steps(-15)
     }
 
+    @Test
+    fun `generateSequence behavior`() {
+        // on its own will never terminate
+        // println(generateSequence(1) { n -> if (n % 2 == 0) n / 2 else n * 3 + 1 }
+
+        // take(n) allows it to run until n elements are generated
+        println(generateSequence(1) { n -> if (n % 2 == 0) n / 2 else n * 3 + 1 }.take(n = 10).toList());
+
+        // indexOf allows it to run until the element is found (and returns a non -1 value)
+        println(generateSequence(16) { n -> if (n % 2 == 0) n / 2 else n * 3 + 1 }.indexOf(1) )
+    }
 }
 
 private fun assertStepsEqual(input: Int, steps: Int) = assertEquals(steps, steps(input))
