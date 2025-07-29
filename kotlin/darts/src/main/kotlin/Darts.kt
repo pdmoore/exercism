079a8062917a4238dd6c3a993bda78205c13b091
Paul Moore <pdmoore@gmail.com>
2025-07-29 12:47:00 -0700

    kotlin - darts

--- kotlin/darts/src/main/kotlin/Darts.kt
+++ kotlin/darts/src/main/kotlin/Darts.kt
@@ -6,22 +6,17 @@ object Darts {
     val MIDDLE_CIRCLE = 1.0..5.0
     val OUTER_CIRCLE = 5.0..10.0
 
-    fun score(x: Int, y: Int): Int {
-        return score(x.toDouble(), y.toDouble())
-    }
+    fun score(x: Int, y: Int): Int = score(x.toDouble(), y.toDouble())
+    fun score(x: Double, y: Int): Int = score(x, y.toDouble())
 
     fun score(x: Double, y: Double): Int {
-        val distance = sqrt((x * x) + (y * y))
+        val distanceFromCenter = sqrt((x * x) + (y * y))
 
         when {
-            distance in INNER_CIRCLE -> return 10
-            distance in MIDDLE_CIRCLE -> return 5
-            distance in OUTER_CIRCLE -> return 1
+            distanceFromCenter in INNER_CIRCLE -> return 10
+            distanceFromCenter in MIDDLE_CIRCLE -> return 5
+            distanceFromCenter in OUTER_CIRCLE -> return 1
             else -> return 0
         }
-    }
-
-    fun score(x: Double, y: Int): Int {
-        return score(x, y.toDouble())
     }
 }
