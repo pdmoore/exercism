d30a3337888a9876dc1e11c1f2f50800c48f8d8d
Paul Moore <pdmoore@gmail.com>
2025-07-29 12:45:38 -0700

    kotlin - darts

--- kotlin/darts/src/main/kotlin/Darts.kt
+++ kotlin/darts/src/main/kotlin/Darts.kt
@@ -1,6 +1,11 @@
 import kotlin.math.sqrt
 
 object Darts {
+
+    val INNER_CIRCLE = 0.0..1.0
+    val MIDDLE_CIRCLE = 1.0..5.0
+    val OUTER_CIRCLE = 5.0..10.0
+
     fun score(x: Int, y: Int): Int {
         return score(x.toDouble(), y.toDouble())
     }
@@ -9,9 +14,9 @@ object Darts {
         val distance = sqrt((x * x) + (y * y))
 
         when {
-            distance in 0.0..1.0 -> return 10
-            distance in 1.0..5.0 -> return 5
-            distance <= 10.0 -> return 1
+            distance in INNER_CIRCLE -> return 10
+            distance in MIDDLE_CIRCLE -> return 5
+            distance in OUTER_CIRCLE -> return 1
             else -> return 0
         }
     }
